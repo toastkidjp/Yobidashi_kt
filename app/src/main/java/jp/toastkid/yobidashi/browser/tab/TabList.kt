0207e584c59ea2124665b46e69fd84f2d4f1f79c
Toast Kid <super_q1m@yahoo.co.jp>
2017-08-19 10:11:09 +0900

    Modify saving tab to individual json files.

--- app/src/main/java/jp/toastkid/yobidashi/browser/tab/TabList.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/tab/TabList.kt
@@ -1,15 +1,13 @@
 package jp.toastkid.yobidashi.browser.tab
 
 import android.content.Context
-
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-
+import jp.toastkid.yobidashi.libs.Logger
+import okio.Okio
 import java.io.File
 import java.io.IOException
-import java.util.ArrayList
-
-import okio.Okio
+import java.util.*
 
 /**
  * First collection of [Tab].
@@ -18,7 +16,7 @@ import okio.Okio
  */
 class TabList private constructor() {
 
-    private val tabs: MutableList<Tab>
+    @Transient private val tabs: MutableList<Tab>
 
     private var index: Int = 0
 
@@ -48,8 +46,15 @@ class TabList private constructor() {
     internal fun save() {
         try {
             initJsonAdapterIfNeed()
-            val json = jsonAdapter!!.toJson(this)
-            Okio.buffer(Okio.sink(tabsFile!!)).write(json.toByteArray(charset("UTF-8"))).flush()
+            val json = jsonAdapter?.toJson(this)
+            Okio.buffer(Okio.sink(tabsFile)).write(json?.toByteArray(charset("UTF-8"))).flush()
+            itemsDir?.list()?.map { File(itemsDir, it) }?.forEach { it.delete() }
+            (0..tabs.size - 1).forEach {
+                Logger.i("save " + File(itemsDir, "$it.json").absoluteFile)
+                Okio.buffer(Okio.sink(File(itemsDir, "$it.json")))
+                    .write(tabJsonAdapter?.toJson(tabs.get(it))?.toByteArray(charset("UTF-8")))
+                    .flush()
+            }
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -78,16 +83,23 @@ class TabList private constructor() {
     internal fun clear() {
         tabs.clear()
         index = -1
-        tabsFile!!.delete()
+        tabsFile?.delete()
+        itemsDir?.delete()
     }
 
     companion object {
 
         private val TABS_DIR = "tabs"
 
+        private val TABS_ITEM_DIR = TABS_DIR + "/items"
+
         private var tabsFile: File? = null
 
+        private var tabJsonAdapter: JsonAdapter<Tab>? = null
+
         private var jsonAdapter: JsonAdapter<TabList>? = null
+
+        private var itemsDir: File? = null
 
         internal fun loadOrInit(context: Context): TabList {
             initTabsFile(context)
@@ -97,7 +109,18 @@ class TabList private constructor() {
 
             try {
                 initJsonAdapterIfNeed()
-                return jsonAdapter!!.fromJson(Okio.buffer(Okio.source(tabsFile!!)))!!
+                initTabJsonAdapterIfNeed()
+
+                val fromJson: TabList?
+                        = jsonAdapter?.fromJson(Okio.buffer(Okio.source(tabsFile as File)))
+
+                Logger.i("size = ${itemsDir?.list()?.size}")
+
+                itemsDir?.list()
+                        ?.map{ tabJsonAdapter?.fromJson(Okio.buffer(Okio.source(File(itemsDir, it))))}
+                        ?.forEach { fromJson?.add(it as Tab) }
+
+                return fromJson as TabList
             } catch (e: IOException) {
                 e.printStackTrace()
             }
@@ -111,6 +134,18 @@ class TabList private constructor() {
                 storeDir.mkdirs()
             }
             tabsFile = File(storeDir, "tabs.json")
+
+            itemsDir = File(context.filesDir, TABS_ITEM_DIR)
+            if (itemsDir != null && !(itemsDir as File).exists()) {
+                itemsDir?.mkdirs()
+            }
+        }
+
+        private fun initTabJsonAdapterIfNeed() {
+            if (tabJsonAdapter != null) {
+                return
+            }
+            tabJsonAdapter = Moshi.Builder().build().adapter(Tab::class.java)
         }
 
         private fun initJsonAdapterIfNeed() {
