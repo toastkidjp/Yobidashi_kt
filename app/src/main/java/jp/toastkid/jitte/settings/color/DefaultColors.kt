151cb8e9c9f87846c7095134c4c1978de28e49ad
Toast Kid <super_q1m@yahoo.co.jp>
2017-08-23 18:57:16 +0900

    Add default colors.

--- app/src/main/java/jp/toastkid/jitte/settings/color/DefaultColors.kt
+++ app/src/main/java/jp/toastkid/jitte/settings/color/DefaultColors.kt
@@ -1,12 +1,10 @@
 package jp.toastkid.jitte.settings.color
 
 import android.content.Context
-import android.graphics.Color
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
 import android.support.v4.content.ContextCompat
 import android.support.v4.util.ArraySet
-
 import jp.toastkid.jitte.R
 
 /**
@@ -19,29 +17,40 @@ internal object DefaultColors {
     fun make(context: Context): ArraySet<SavedColor> {
         val models = ArraySet<SavedColor>()
         models.add(SavedColors.makeSavedColor(
-                fromRes(context, R.color.colorPrimary), fromRes(context, R.color.textPrimary)))
-        models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.colorPrimaryDark), fromRes(context, R.color.textPrimary)))
+        models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.colorPrimaryDark), fromRes(context, R.color.deep_yellow)))
         models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.crimson_bg), fromRes(context, R.color.crimson_font)))
         models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.cyan_bg), fromRes(context, R.color.white)))
         models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.red_200_dd), fromRes(context, R.color.black)))
+        models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.yellow_200_dd), fromRes(context, R.color.black)))
+        models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.black), fromRes(context, R.color.textPrimary)))
+        models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.indigo_200_dd), fromRes(context, R.color.black)))
         models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.white), fromRes(context, R.color.black)))
         models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.pinky), fromRes(context, R.color.white)))
         models.add(SavedColors.makeSavedColor(
-                fromRes(context, R.color.lime_bg), fromRes(context, R.color.filter_white)))
+                fromRes(context, R.color.lime_bg), fromRes(context, R.color.white)))
         models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.purple_bg), fromRes(context, R.color.white)))
         models.add(SavedColors.makeSavedColor(
-                fromRes(context, R.color.darkgray_scale), fromRes(context, R.color.filter_white)))
-        models.add(SavedColors.makeSavedColor(
                 fromRes(context, R.color.wa_bg), fromRes(context, R.color.wa_font)))
         models.add(SavedColors.makeSavedColor(
-                Color.TRANSPARENT, Color.BLACK))
+                fromRes(context, R.color.light_blue_200_dd), fromRes(context, R.color.white)))
+        models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.teal_500_dd), fromRes(context, R.color.white)))
+        models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.gray_500_dd), fromRes(context, R.color.white)))
+        models.add(SavedColors.makeSavedColor(
+                fromRes(context, R.color.deep_orange_500_dd), fromRes(context, R.color.white)))
+
         return models
     }
 
