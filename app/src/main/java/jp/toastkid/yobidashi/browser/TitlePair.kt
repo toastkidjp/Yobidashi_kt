bb722a480ceb670d5daffa6cac0e69dec2c9cd2c
Toast Kid <super_q1m@yahoo.co.jp>
2017-09-28 19:53:18 +0900

    Clean up TitlePair's code.

--- app/src/main/java/jp/toastkid/yobidashi/browser/TitlePair.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/TitlePair.kt
@@ -7,18 +7,12 @@ package jp.toastkid.yobidashi.browser
  */
 class TitlePair private constructor(private val title: String, private val subtitle: String) {
 
-    fun title(): String {
-        return title
-    }
+    fun title(): String = title
 
-    fun subtitle(): String {
-        return subtitle
-    }
+    fun subtitle(): String = subtitle
 
     companion object {
 
-        fun make(title: String, subtitle: String): TitlePair {
-            return TitlePair(title, subtitle)
-        }
+        fun make(title: String, subtitle: String): TitlePair = TitlePair(title, subtitle)
     }
 }
