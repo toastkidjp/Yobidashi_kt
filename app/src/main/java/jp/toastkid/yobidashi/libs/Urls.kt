4b531118e4b8659aa88825c8fdd870f2e6a2471a
Toast Kid <super_q1m@yahoo.co.jp>
2017-10-17 21:11:24 +0900

    Modify Urls and test case.

--- app/src/main/java/jp/toastkid/yobidashi/libs/Urls.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/Urls.kt
@@ -5,35 +5,28 @@ import android.webkit.URLUtil
 
 /**
  * URL utilities.
-
+  TODO Clean up code
  * @author toastkidjp
  */
-class Urls private constructor() {
+object Urls {
 
-    init {
-        throw IllegalStateException()
+    /**
+     * Return passed url is invalid.
+     * @param url
+     *
+     * @return
+     */
+    fun isInvalidUrl(url: String): Boolean {
+        return TextUtils.isEmpty(url) || !URLUtil.isHttpUrl(url) && !URLUtil.isHttpsUrl(url)
     }
 
-    companion object {
-
-        /**
-         * Return passed url is invalid.
-         * @param url
-         *
-         * @return
-         */
-        fun isInvalidUrl(url: String): Boolean {
-            return TextUtils.isEmpty(url) || !URLUtil.isHttpUrl(url) && !URLUtil.isHttpsUrl(url)
-        }
-
-        /**
-         * Return passed url is valid.
-         * @param url
-         *
-         * @return
-         */
-        fun isValidUrl(url: String): Boolean {
-            return !isInvalidUrl(url)
-        }
+    /**
+     * Return passed url is valid.
+     * @param url
+     *
+     * @return
+     */
+    fun isValidUrl(url: String): Boolean {
+        return !isInvalidUrl(url)
     }
 }
