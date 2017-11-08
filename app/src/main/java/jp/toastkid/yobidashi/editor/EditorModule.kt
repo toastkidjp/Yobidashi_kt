9bd36ca2d2549de94835e73824c52f1cbc8518d1
Toast Kid <super_q1m@yahoo.co.jp>
2017-11-08 22:44:00 +0900

    Modify editor's layout.

--- app/src/main/java/jp/toastkid/yobidashi/editor/EditorModule.kt
+++ app/src/main/java/jp/toastkid/yobidashi/editor/EditorModule.kt
@@ -9,7 +9,9 @@ import android.net.Uri
 import android.os.Environment
 import android.support.annotation.StringRes
 import android.support.v7.app.AlertDialog
+import android.text.Editable
 import android.text.Html
+import android.text.TextWatcher
 import android.view.View
 import jp.toastkid.yobidashi.R
 import jp.toastkid.yobidashi.databinding.ModuleEditorBinding
@@ -89,6 +91,7 @@ class EditorModule(
                     })
                     .show()
         }
+
         val colorPair = preferenceApplier.colorPair()
         Colors.setBgAndText(binding.save, colorPair)
         Colors.setBgAndText(binding.load, colorPair)
@@ -100,6 +103,27 @@ class EditorModule(
         Colors.setBgAndText(binding.toTop, colorPair)
         Colors.setBgAndText(binding.toBottom, colorPair)
         Colors.setBgAndText(binding.clear, colorPair)
+
+        binding.editorInput.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(contentEditable: Editable?) {
+                setContentTextLengthCount(context)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+        })
+    }
+
+    /**
+     * Set content text length count to binding.counter.
+     *
+     * @param context Context
+     */
+    private fun setContentTextLengthCount(context: Context) {
+        binding.counter.text =
+                context.getString(R.string.message_character_count, content().length)
     }
 
     /**
@@ -140,6 +164,7 @@ class EditorModule(
         val context = binding.root.context
         val inputLayout = TextInputs.make(context)
         inputLayout.editText?.setText(DEFAULT_FILE_NAME)
+
         AlertDialog.Builder(context)
                 .setTitle(context.getString(R.string.title_dialog_input_file_name))
                 .setView(inputLayout)
@@ -281,7 +306,15 @@ class EditorModule(
      * Clear input text.
      */
     private inline fun clearInput() {
-        binding.editorInput.setText("")
+        setContentText("")
+    }
+
+    /**
+     * Set content string and set text length.
+     */
+    private fun setContentText(contentStr: String) {
+        binding.editorInput.setText(contentStr)
+        setContentTextLengthCount(binding.root.context)
     }
 
     /**
