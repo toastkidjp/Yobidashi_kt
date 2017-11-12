d4e2faa003eee8cd4356ac0aa376ad071a6bc12c
Toast Kid <super_q1m@yahoo.co.jp>
2017-11-12 19:46:43 +0900

    Convert to object.

--- app/src/main/java/jp/toastkid/yobidashi/libs/TextInputs.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/TextInputs.kt
@@ -5,51 +5,43 @@ import android.support.design.widget.TextInputLayout
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
-
 import jp.toastkid.yobidashi.R
 
 /**
  * @author toastkidjp
  */
-class TextInputs private constructor() {
+object TextInputs  {
 
-    init {
-        throw IllegalStateException()
-    }
+    fun setEmptyAlert(inputLayout: TextInputLayout): EditText {
+        val input = inputLayout.editText
+        if (input == null) {
+            return input!!
+        }
 
-    companion object {
+        input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
-        fun setEmptyAlert(inputLayout: TextInputLayout): EditText {
-            val input = inputLayout.editText
-            if (input == null) {
-                return input!!
             }
 
-            input.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
 
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.toString().isEmpty()) {
+                    inputLayout.error = inputLayout.context
+                            .getString(R.string.favorite_search_addition_dialog_empty_message)
+                    return
                 }
+                inputLayout.isErrorEnabled = false
+            }
+        })
+        return input
+    }
 
-                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-
-                }
-
-                override fun afterTextChanged(s: Editable) {
-                    if (s.toString().isEmpty()) {
-                        inputLayout.error = inputLayout.context
-                                .getString(R.string.favorite_search_addition_dialog_empty_message)
-                        return
-                    }
-                    inputLayout.isErrorEnabled = false
-                }
-            })
-            return input
-        }
-
-        fun make(context: Context): TextInputLayout {
-            val inputLayout = TextInputLayout(context)
-            inputLayout.addView(EditText(context))
-            return inputLayout
-        }
+    fun make(context: Context): TextInputLayout {
+        val inputLayout = TextInputLayout(context)
+        inputLayout.addView(EditText(context))
+        return inputLayout
     }
 }
