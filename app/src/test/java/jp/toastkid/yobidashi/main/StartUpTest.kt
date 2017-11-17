c0d89205606ab189d1ac27c31bd1a6dfb46e2d95
Toast Kid <super_q1m@yahoo.co.jp>
2017-11-17 23:41:00 +0900

    Implement StartUpTest.

---
+++ app/src/test/java/jp/toastkid/yobidashi/main/StartUpTest.kt
@@ -0,0 +1,30 @@
+package jp.toastkid.yobidashi.main
+
+import jp.toastkid.yobidashi.R
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * [StartUp]'s test cases.
+ *
+ * @author toastkidjp
+ */
+class StartUpTest {
+
+    /**
+     * Check of [StartUp#find].
+     */
+    @Test
+    fun getTitleId() {
+        assertEquals(StartUp.BROWSER, StartUp.find("BROWSER"))
+    }
+
+    /**
+     * Check of [StartUp#findById].
+     */
+    @Test
+    fun getRadioButtonId() {
+        assertEquals(StartUp.BROWSER, StartUp.findById(R.id.start_up_browser))
+    }
+
+}
