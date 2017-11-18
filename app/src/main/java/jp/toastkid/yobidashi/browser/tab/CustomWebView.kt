e2bc415058a9de7a385857d769bb9144aac4abc7
Toast Kid <super_q1m@yahoo.co.jp>
2017-11-18 20:08:48 +0900

    Modify Pull to refresh.

--- app/src/main/java/jp/toastkid/yobidashi/browser/tab/CustomWebView.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/tab/CustomWebView.kt
@@ -1,6 +1,7 @@
 package jp.toastkid.yobidashi.browser.tab
 
 import android.content.Context
+import android.view.MotionEvent
 import android.webkit.WebView
 
 /**
@@ -14,13 +15,23 @@ internal class CustomWebView(context: Context) : WebView(context) {
 
     var scrollListener: (Int, Int, Int, Int) -> Unit = { _, _, _, _  -> }
 
+    var scrolling: Int = 0;
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_UP) {
+            scrolling = 0;
+        }
+        return super.onTouchEvent(event)
+    }
+
     override fun onOverScrolled(scrollX: Int, scrollY: Int, clampedX: Boolean, clampedY: Boolean) {
         super.onOverScrolled(scrollX, scrollY, clampedX, clampedY)
-        enablePullToRefresh = true
+        enablePullToRefresh = scrolling <= 0
     }
 
     override fun onScrollChanged(horizontal: Int, vertical: Int, oldHorizontal: Int, oldVertical: Int) {
         super.onScrollChanged(horizontal, vertical, oldHorizontal, oldVertical)
+        scrolling += vertical
         scrollListener(horizontal, vertical, oldHorizontal, oldVertical)
     }
 }
