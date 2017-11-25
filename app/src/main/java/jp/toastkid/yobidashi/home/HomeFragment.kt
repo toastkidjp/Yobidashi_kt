1aa537aa203176a6c6e5a1dc6103c8c471a30448
Toast Kid <super_q1m@yahoo.co.jp>
2017-11-25 17:06:15 +0900

    Clean up HomeFragment's code.

--- app/src/main/java/jp/toastkid/yobidashi/home/HomeFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.ColorInt
+import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.ColorUtils
 import android.support.v7.widget.LinearLayoutManager
@@ -19,7 +20,6 @@ import io.reactivex.functions.Consumer
 import jp.toastkid.yobidashi.BaseFragment
 import jp.toastkid.yobidashi.R
 import jp.toastkid.yobidashi.barcode.BarcodeReaderActivity
-import jp.toastkid.yobidashi.barcode.InstantBarcodeGenerator
 import jp.toastkid.yobidashi.databinding.FragmentHomeBinding
 import jp.toastkid.yobidashi.launcher.LauncherActivity
 import jp.toastkid.yobidashi.libs.intent.SettingsIntentFactory
@@ -30,22 +30,30 @@ import jp.toastkid.yobidashi.settings.background.BackgroundSettingActivity
 import jp.toastkid.yobidashi.settings.color.ColorSettingActivity
 
 /**
- * Speed dial.
+ * Home fragment.
  *
  * @author toastkidjp
  */
 class HomeFragment : BaseFragment() {
 
-    /** Data binding object.  */
+    /**
+     * Data binding object.
+     */
     private lateinit var binding: FragmentHomeBinding
 
-    /** Callback.  */
+    /**
+     * Callback.
+     */
     private var action: FragmentReplaceAction? = null
 
-    /** ModuleAdapter.  */
+    /**
+     * ModuleAdapter.
+     */
     private var adapter: Adapter? = null
 
-    /** Disposables.  */
+    /**
+     * Disposables.
+     */
     private val disposables: CompositeDisposable = CompositeDisposable()
 
     override fun onAttach(context: Context) {
@@ -59,7 +67,7 @@ class HomeFragment : BaseFragment() {
             savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater!!, LAYOUT_ID, container, false)
+        binding = DataBindingUtil.inflate(inflater!!, LAYOUT_ID, container, false)
         binding.fragment = this
 
         initMenus()
@@ -87,14 +95,11 @@ class HomeFragment : BaseFragment() {
             Menu.CODE_READER -> {
                 startActivity(BarcodeReaderActivity.makeIntent(activity))
             }
-            Menu.SHARE_BARCODE -> {
-                InstantBarcodeGenerator(activity).invoke()
-            }
             Menu.LAUNCHER -> {
                 startActivity(LauncherActivity.makeIntent(activity))
             }
             Menu.BROWSER -> {
-                action!!.action(Command.OPEN_BROWSER)
+                action?.action(Command.OPEN_BROWSER)
             }
             Menu.PLANNING_POKER -> {
                 startActivity(PlanningPokerActivity.makeIntent(activity))
@@ -126,13 +131,16 @@ class HomeFragment : BaseFragment() {
                 else
                     ContextCompat.getColor(activity, R.color.darkgray_scale)
         )
+
         val colorPair = colorPair()
-        @ColorInt val bgColor:   Int = colorPair.bgColor()
         @ColorInt val fontColor: Int = colorPair.fontColor()
+
         binding.mainTitle.setTextColor(colorPair().fontColor())
+        binding.searchAction.setTextColor(fontColor)
+
+        @ColorInt val bgColor:   Int = colorPair.bgColor()
         binding.searchInput.setTextColor(bgColor)
         binding.searchActionBackground.setBackgroundColor(ColorUtils.setAlphaComponent(bgColor, 128))
-        binding.searchAction.setTextColor(fontColor)
         binding.searchIcon.setColorFilter(bgColor)
         binding.voiceSearch.setColorFilter(bgColor)
         binding.searchInputBorder.setBackgroundColor(bgColor)
@@ -140,9 +148,9 @@ class HomeFragment : BaseFragment() {
 
     /**
      * Open search.
-     * @param v
+     * @param ignored
      */
-    fun search(v: View) {
+    fun search(ignored: View) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             binding.searchBar.transitionName = "share"
@@ -153,9 +161,9 @@ class HomeFragment : BaseFragment() {
 
     /**
      * Open voice search.
-     * @param v
+     * @param ignored
      */
-    fun voiceSearch(v: View) {
+    fun voiceSearch(ignored: View) {
         startActivityForResult(VoiceSearch.makeIntent(activity), REQUEST_CODE_VOICE_SEARCH)
     }
 
@@ -178,13 +186,19 @@ class HomeFragment : BaseFragment() {
         disposables.dispose()
     }
 
+    @StringRes
     override fun titleId(): Int = R.string.title_home
 
     companion object {
 
-        /** Layout ID.  */
-        private val LAYOUT_ID = R.layout.fragment_home
+        /**
+         * Layout ID.
+         */
+        private const val LAYOUT_ID: Int = R.layout.fragment_home
 
-        private val REQUEST_CODE_VOICE_SEARCH = 2
+        /**
+         * Request code.
+         */
+        private const val REQUEST_CODE_VOICE_SEARCH: Int = 2
     }
 }
