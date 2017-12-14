2196d5e90c9fd1173033a03c3e061b67200ec59b
Toast Kid <super_q1m@yahoo.co.jp>
2017-12-14 23:37:57 +0900

    Implement saving preview image to file.

--- app/src/main/java/jp/toastkid/yobidashi/libs/ImagePreviewActivity.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/ImagePreviewActivity.kt
@@ -1,13 +1,19 @@
 package jp.toastkid.yobidashi.libs
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.annotation.LayoutRes
+import android.view.MenuItem
 import jp.toastkid.yobidashi.BaseActivity
 import jp.toastkid.yobidashi.R
 import jp.toastkid.yobidashi.databinding.ActivityImagePreviewBinding
+import jp.toastkid.yobidashi.libs.intent.IntentFactory
 import java.io.File
 
 /**
@@ -17,8 +23,12 @@ class ImagePreviewActivity(): BaseActivity() {
 
     var path: String? = null
 
+    var imagePath: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        overridePendingTransition(0, 0)
 
         if (!intent.hasExtra(EXTRA_KEY_IMAGE_PATH)) {
             Toaster.tShort(this, R.string.message_illegal_image_path)
@@ -29,9 +39,43 @@ class ImagePreviewActivity(): BaseActivity() {
         setContentView(LAYOUT_ID)
         val binding: ActivityImagePreviewBinding =
                 DataBindingUtil.setContentView(this, LAYOUT_ID)
-        val imagePath = intent.getStringExtra(EXTRA_KEY_IMAGE_PATH)
+        imagePath = intent.getStringExtra(EXTRA_KEY_IMAGE_PATH)
         ImageLoader.setImageToImageView(binding.image, imagePath)
-        binding.close.setOnClickListener { finish() }
+
+        binding.toolbar.setNavigationIcon(R.drawable.ic_back)
+        binding.toolbar.setNavigationOnClickListener { finish() }
+        binding.toolbar.inflateMenu(R.menu.activity_image_preview)
+        binding.toolbar.setOnMenuItemClickListener{ this.clickMenu(it) }
+    }
+
+    override fun clickMenu(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.close -> {
+                finish()
+            }
+            R.id.save  -> {
+                startActivityForResult(
+                        IntentFactory.makeDocumentOnStorage(STORE_FILE_TYPE, DEFAULT_STORE_FILE_NAME),
+                        1
+                )
+            }
+        }
+        return super.clickMenu(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        intent?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                val takeFlags: Int = intent.getFlags() and Intent.FLAG_GRANT_READ_URI_PERMISSION
+                contentResolver.takePersistableUriPermission(intent.data, takeFlags)
+            }
+            ImageLoader.loadBitmap(this, Uri.parse(File(imagePath).toURI().toString()))
+                ?.compress(Bitmap.CompressFormat.PNG, 100, contentResolver.openOutputStream(it.data))
+        }
     }
 
     override fun titleId(): Int = R.string.app_name
@@ -47,6 +91,10 @@ class ImagePreviewActivity(): BaseActivity() {
         private const val LAYOUT_ID: Int = R.layout.activity_image_preview
 
         private const val EXTRA_KEY_IMAGE_PATH: String = "image_path"
+
+        private const val STORE_FILE_TYPE: String = "image/png"
+
+        private const val DEFAULT_STORE_FILE_NAME: String = "image.png"
 
         fun makeIntent(context: Context, file: File): Intent =
                 Intent(context, ImagePreviewActivity::class.java).apply {
