f1e33a6e1ec44a0c613f8718adbf23bac2d14530
Toast Kid <super_q1m@yahoo.co.jp>
2017-12-31 20:45:25 +0900

    Clean up Activity's adapter.

--- app/src/main/java/jp/toastkid/yobidashi/search/favorite/ActivityAdapter.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/favorite/ActivityAdapter.kt
@@ -9,12 +9,11 @@ import com.github.gfx.android.orma.Relation
 import com.github.gfx.android.orma.widget.OrmaRecyclerViewAdapter
 import io.reactivex.schedulers.Schedulers
 import jp.toastkid.yobidashi.R
-import jp.toastkid.yobidashi.databinding.ItemFavoriteSearchBinding
 import jp.toastkid.yobidashi.search.BackgroundSeachAction
 import jp.toastkid.yobidashi.search.SearchCategory
 
 /**
- * TODO Clean up code.
+ * Favorite Search activity's adapter.
  *
  * @author toastkidjp
  */
@@ -25,33 +24,42 @@ internal class ActivityAdapter(
         private val toasterCallback: (Int) -> Unit
 ) : OrmaRecyclerViewAdapter<FavoriteSearch, FavoriteSearchHolder>(context, relation) {
 
+    /**
+     * Layout inflater.
+     */
     private val inflater: LayoutInflater = LayoutInflater.from(context)
 
     override fun onCreateViewHolder(
             parent: ViewGroup,
             viewType: Int
-    ): FavoriteSearchHolder {
-        return FavoriteSearchHolder(
-                DataBindingUtil
-                        .inflate<ItemFavoriteSearchBinding>(inflater, R.layout.item_favorite_search, parent, false)
-        )
-    }
+    ): FavoriteSearchHolder = FavoriteSearchHolder(
+            DataBindingUtil.inflate(inflater, R.layout.item_favorite_search, parent, false)
+    )
 
     override fun onBindViewHolder(holder: FavoriteSearchHolder, position: Int) {
         bindViews(holder, relation.get(position))
         holder.switchDividerVisibility(position != (itemCount - 1))
     }
 
-    override fun getItemCount(): Int {
-        return relation.count()
-    }
+    override fun getItemCount(): Int = relation.count()
 
+    /**
+     * Remove item at position.
+     *
+     * @param position
+     */
     fun removeAt(position: Int) {
         removeItemAsMaybe(relation.get(position))
                 .subscribeOn(Schedulers.io())
                 .subscribe()
     }
 
+    /**
+     * Bind views.
+     *
+     * @param holder
+     * @param favoriteSearch
+     */
     private fun bindViews(holder: FavoriteSearchHolder, favoriteSearch: FavoriteSearch) {
         val category = SearchCategory.findByCategory(favoriteSearch.category as String)
         holder.setImageId(category.iconId)
