a0575f9dac36dcd8d52b7d181e07a64d73a73901
Toast Kid <super_q1m@yahoo.co.jp>
2018-01-10 20:35:22 +0900

    Divide other function object.

--- app/src/main/java/jp/toastkid/yobidashi/ExtendedApplication.kt
+++ app/src/main/java/jp/toastkid/yobidashi/ExtendedApplication.kt
@@ -1,8 +1,6 @@
 package jp.toastkid.yobidashi
 
 import android.app.Application
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import com.squareup.leakcanary.LeakCanary
 import io.reactivex.Completable
 import io.reactivex.disposables.CompositeDisposable
@@ -11,12 +9,10 @@ import io.reactivex.schedulers.Schedulers
 import jp.toastkid.yobidashi.browser.bookmark.BookmarkInitializer
 import jp.toastkid.yobidashi.libs.db.DbInitter
 import jp.toastkid.yobidashi.libs.preference.PreferenceApplier
-import jp.toastkid.yobidashi.libs.storage.FilesDir
 import jp.toastkid.yobidashi.notification.widget.NotificationWidget
-import jp.toastkid.yobidashi.settings.background.BackgroundSettingActivity
+import jp.toastkid.yobidashi.settings.background.DefaultBackgroundImagePreparator
 import jp.toastkid.yobidashi.settings.color.SavedColors
 import timber.log.Timber
-import java.io.FileOutputStream
 
 /**
  * For using LeakCanary and so on...
@@ -68,20 +64,7 @@ class ExtendedApplication : Application() {
         SavedColors.insertDefaultColors(this)
         preferenceApplier.updateLastAd()
         BookmarkInitializer.invoke(this)
-
-        Completable.fromAction {
-            val filesDir = FilesDir(this, BackgroundSettingActivity.BACKGROUND_DIR)
-            val defaultFile = filesDir.assignNewFile("apricot")
-            BitmapFactory.decodeResource(resources, R.mipmap.apricot)
-                    .compress(Bitmap.CompressFormat.PNG, 100,
-                            FileOutputStream(defaultFile))
-            BitmapFactory.decodeResource(resources, R.mipmap.toast)
-                    .compress(Bitmap.CompressFormat.PNG, 100,
-                            FileOutputStream(filesDir.assignNewFile("toast")))
-            preferenceApplier.backgroundImagePath = defaultFile.absolutePath
-        }.subscribeOn(Schedulers.io())
-                .subscribe()
-                .addTo(disposables)
+        DefaultBackgroundImagePreparator(this).addTo(disposables)
     }
 
     override fun onTerminate() {
