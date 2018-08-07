f0f058a40d917479acfea826fed6c279a74bafb3
Toast Kid <super_q1m@yahoo.co.jp>
2018-08-08 00:26:26 +0900

    Rewrite ImplicitIntentInvoker to DialogFragment.

---
+++ app/src/main/java/jp/toastkid/yobidashi/libs/intent/ImplicitIntentInvokerDialogFragment.kt
@@ -0,0 +1,62 @@
+package jp.toastkid.yobidashi.libs.intent
+
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import jp.toastkid.yobidashi.R
+import jp.toastkid.yobidashi.libs.TextInputs
+import jp.toastkid.yobidashi.libs.Toaster
+import jp.toastkid.yobidashi.libs.clip.Clipboard
+import timber.log.Timber
+
+/**
+ * For invoking implicit intent.
+ *
+ * @author toastkidjp
+ */
+class ImplicitIntentInvokerDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val activityContext = context ?: return super.onCreateDialog(savedInstanceState)
+
+        val textInputLayout = TextInputs.make(activityContext)
+        TextInputs.setEmptyAlert(textInputLayout)
+
+        Clipboard.getPrimary(activityContext)?.let { textInputLayout.editText?.setText(it) }
+
+        return AlertDialog.Builder(activityContext)
+                .setTitle(R.string.title_invoker_dialog)
+                .setMessage(R.string.message_invoker_dialog)
+                .setView(textInputLayout)
+                .setNegativeButton(R.string.cancel) { d, i -> d.cancel() }
+                .setPositiveButton(R.string.invoke) { d, i ->
+                    textInputLayout.editText?.text?.let { invoke(activityContext, it.toString()) }
+                    d.dismiss()
+                }
+                .create()
+    }
+
+    /**
+     * Invoke intent with specified action name.
+     *
+     * @param context
+     * @param actionName
+     */
+    fun invoke(context: Context, actionName: String) {
+        try {
+            context.startActivity(Intent(actionName))
+            Toaster.tShort(context, context.getString(R.string.message_invoking_intent, actionName))
+        } catch (e: ActivityNotFoundException) {
+            Timber.e(e)
+            Toaster.tShort(
+                    context,
+                    context.getString(R.string.message_invoking_intent_failed, actionName)
+            )
+        }
+    }
+
+}
