68d5255463a04d319d3d3544ea79eaaaf072f6f2
Toast Kid <super_q1m@yahoo.co.jp>
2018-08-10 12:57:03 +0900

    Implement SearchQueryExtractor's test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/search/SearchQueryExtractorTest.kt
@@ -0,0 +1,27 @@
+package jp.toastkid.yobidashi.search
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * [SearchQueryExtractor]'s test cases.
+ *
+ * @author toastkidjp
+ */
+@RunWith(RobolectricTestRunner::class)
+class SearchQueryExtractorTest {
+
+    @Test
+    fun testInvoke() {
+        assertEquals(
+                "かもめ",
+                SearchQueryExtractor("https://www.google.com/search?q=%E3%81%8B%E3%82%82%E3%82%81")
+        )
+        assertEquals(
+                "かもめ",
+                SearchQueryExtractor("https://www.amazon.com/s/ref=nb_sb_noss?field-keywords=%E3%81%8B%E3%82%82%E3%82%81")
+        )
+    }
+}
