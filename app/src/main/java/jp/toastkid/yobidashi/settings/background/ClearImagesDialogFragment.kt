8ba5da8484a58fb524fce5112a6b743f18ec1efb
Toast Kid <super_q1m@yahoo.co.jp>
2018-08-18 20:04:24 +0900

    Divide clear-image-dialog to DialogFragment.

---
+++ app/src/main/java/jp/toastkid/yobidashi/settings/background/ClearImagesDialogFragment.kt
@@ -0,0 +1,37 @@
+package jp.toastkid.yobidashi.settings.background
+
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import android.text.Html
+import jp.toastkid.yobidashi.R
+
+/**
+ * @author toastkidjp
+ */
+internal class ClearImagesDialogFragment : DialogFragment() {
+
+    interface Callback {
+        fun onClickClearImages()
+    }
+
+    private var onClick: Callback? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val activityContext = context ?: return super.onCreateDialog(savedInstanceState)
+        if (activityContext is Callback) {
+            onClick = activityContext
+        }
+        return AlertDialog.Builder(activityContext)
+                .setTitle(R.string.clear_all)
+                .setMessage(Html.fromHtml(activityContext.getString(R.string.confirm_clear_all_settings)))
+                .setCancelable(true)
+                .setPositiveButton(R.string.ok) { d, _ ->
+                    onClick?.onClickClearImages()
+                    d.dismiss()
+                }
+                .setNegativeButton(R.string.cancel) { d, _ -> d.cancel() }
+                .create()
+    }
+}
