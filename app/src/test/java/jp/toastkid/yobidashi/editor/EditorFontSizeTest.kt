e45c995ab04a7d83401590e0dc21e704a65eae3c
Toast Kid <super_q1m@yahoo.co.jp>
2018-09-19 23:18:14 +0900

    Implement editor font size enum.

---
+++ app/src/test/java/jp/toastkid/yobidashi/editor/EditorFontSizeTest.kt
@@ -0,0 +1,18 @@
+package jp.toastkid.yobidashi.editor
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class EditorFontSizeTest {
+
+    @Test
+    fun testFindIndex() {
+        assertEquals(3, EditorFontSize.findIndex(11))
+        assertEquals(4, EditorFontSize.findIndex(12))
+        assertEquals(4, EditorFontSize.findIndex(13))
+        assertEquals(5, EditorFontSize.findIndex(14))
+    }
+}
