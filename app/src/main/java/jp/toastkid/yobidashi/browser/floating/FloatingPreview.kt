d1f26260ce414e7e24d47654082f92ae296c9167
Toast Kid <super_q1m@yahoo.co.jp>
2018-09-24 21:39:01 +0900

    Implement floating preview. Fix #219.

---
+++ app/src/main/java/jp/toastkid/yobidashi/browser/floating/FloatingPreview.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.browser.floating
+
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.core.net.toUri
+import jp.toastkid.yobidashi.R
+import jp.toastkid.yobidashi.main.MainActivity
+
+/**
+ * @author toastkidjp
+ */
+class FloatingPreview(private val parent: ViewGroup) {
+
+    operator fun invoke(webView: WebView, url: String) {
+        if (parent.childCount != 0) {
+            parent.removeAllViews()
+        }
+        parent.visibility = View.VISIBLE
+        val layoutParams = FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                )
+        layoutParams.height =
+                parent.context.resources.getDimensionPixelSize(R.dimen.floating_preview_height)
+        layoutParams.gravity = Gravity.BOTTOM
+        webView.layoutParams = layoutParams
+
+        webView.isEnabled = true
+        webView.onResume()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                parent.context?.let {
+                    it.startActivity(MainActivity.makeBrowserIntent(it, url.toUri()))
+                }
+                parent.visibility = View.GONE
+                parent.removeAllViews()
+                return false
+            }
+        }
+
+        parent.addView(webView)
+        webView.loadUrl(url)
+    }
+
+    fun hide(webView: WebView?) {
+        parent.visibility = View.GONE
+        webView?.isEnabled = false
+        webView?.onPause()
+    }
+}
