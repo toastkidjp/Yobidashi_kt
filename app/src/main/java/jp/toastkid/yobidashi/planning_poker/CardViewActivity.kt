b456bd1289de3b72a43be9338c92fddf03109ae2
Toast Kid <super_q1m@yahoo.co.jp>
2018-12-05 09:56:01 +0900

    refactor Remove unused type declaration in CardViewActivity.

--- app/src/main/java/jp/toastkid/yobidashi/planning_poker/CardViewActivity.kt
+++ app/src/main/java/jp/toastkid/yobidashi/planning_poker/CardViewActivity.kt
@@ -48,12 +48,12 @@ class CardViewActivity : AppCompatActivity() {
         /**
          * Card extra key.
          */
-        const val EXTRA_KEY_CARD_TEXT: String = "card_text"
+        const val EXTRA_KEY_CARD_TEXT = "card_text"
 
         /**
          * Layout ID.
          */
-        private const val LAYOUT_ID: Int = R.layout.activity_transparent
+        private const val LAYOUT_ID = R.layout.activity_transparent
 
         /**
          * Make [CardViewActivity]'s intent.
@@ -65,7 +65,7 @@ class CardViewActivity : AppCompatActivity() {
         fun makeIntent(
                 context: Context,
                 text: String
-        ): Intent = Intent(context, CardViewActivity::class.java)
+        ) = Intent(context, CardViewActivity::class.java)
                 .also { it.putExtra(EXTRA_KEY_CARD_TEXT, text) }
     }
 }
