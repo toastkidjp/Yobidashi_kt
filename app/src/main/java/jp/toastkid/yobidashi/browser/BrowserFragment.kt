706c0750ee390d09e00d30ed4a2113cdcbf1dded
Toast Kid <super_q1m@yahoo.co.jp>
2018-12-05 09:48:18 +0900

    bugfix Fix bug of whiteout CycleMenu when open other activity. Fix #256.

--- app/src/main/java/jp/toastkid/yobidashi/browser/BrowserFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/BrowserFragment.kt
@@ -927,6 +927,7 @@ class BrowserFragment : BaseFragment(),
     override fun onStop() {
         super.onStop()
         tabs.saveTabList()
+        binding?.cycleMenu?.close(true)
     }
 
     override fun onDestroy() {
