d174d33117537cd6602b83851bb64de593b79f3e
Toast Kid <super_q1m@yahoo.co.jp>
2019-01-19 19:17:31 +0900

    refactor #283 Replace empty method implementation with Unit in PageSearcherModule.

--- app/src/main/java/jp/toastkid/yobidashi/browser/page_search/PageSearcherModule.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/page_search/PageSearcherModule.kt
@@ -41,17 +41,13 @@ class PageSearcherModule(
         binding.sipDownward.setColorFilter(bgColor)
 
         binding.inputLayout.editText?.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-                // NOP.
-            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 tabs.find(s.toString())
             }
 
-            override fun afterTextChanged(s: Editable) {
-                // NOP.
-            }
+            override fun afterTextChanged(s: Editable) = Unit
         })
         editText = binding.inputLayout.editText as EditText
     }
