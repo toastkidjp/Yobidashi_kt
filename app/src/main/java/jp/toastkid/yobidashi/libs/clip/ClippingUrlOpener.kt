fd728a24578390cb3a2a827bd6b499fd83e191d6
Toast Kid <super_q1m@yahoo.co.jp>
2019-01-27 15:09:31 +0900

    refactor #299 Add comments in ClippingUrlOpener.

--- app/src/main/java/jp/toastkid/yobidashi/libs/clip/ClippingUrlOpener.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/clip/ClippingUrlOpener.kt
@@ -9,10 +9,18 @@ import jp.toastkid.yobidashi.libs.Urls
 import jp.toastkid.yobidashi.libs.preference.PreferenceApplier
 
 /**
+ * Clipping URL opener, this class invoked containing URL in clipboard.
+ *
  * @author toastkidjp
  */
 object ClippingUrlOpener {
 
+    /**
+     * Invoke action.
+     *
+     * @param view [View](Nullable)
+     * @param onClick callback
+     */
     operator fun invoke(view: View?, onClick: (Uri) -> Unit) {
         if (view == null) {
             return
