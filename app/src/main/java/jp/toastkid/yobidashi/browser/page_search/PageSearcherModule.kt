ee7a1951555d4c6c97110dfdbbc16e5190c25cae
Toast Kid <super_q1m@yahoo.co.jp>
2019-04-28 22:39:38 +0900

    feature #354 Modify after edit action.

--- app/src/main/java/jp/toastkid/yobidashi/browser/page_search/PageSearcherModule.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/page_search/PageSearcherModule.kt
@@ -68,7 +68,7 @@ class PageSearcherModule(
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                view.find(s.toString())
+                view.findDown(s.toString())
             }
 
             override fun afterTextChanged(s: Editable) = Unit
