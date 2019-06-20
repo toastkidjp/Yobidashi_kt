11fafe6eb9340186e3a5ee1a4b7a76ff02d2a015
Toast Kid <super_q1m@yahoo.co.jp>
2019-06-20 23:07:41 +0900

    refactor #307 Add comments to pdf.ViewHolder.

--- app/src/main/java/jp/toastkid/yobidashi/pdf/ViewHolder.kt
+++ app/src/main/java/jp/toastkid/yobidashi/pdf/ViewHolder.kt
@@ -6,18 +6,37 @@ import android.view.View
 import jp.toastkid.yobidashi.databinding.ItemPdfContentBinding
 
 /**
+ * PDF viewer's view holder
+ *
+ * @param binding [ItemPdfContentBinding]
  * @author toastkidjp
  */
 class ViewHolder(val binding: ItemPdfContentBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    /**
+     * Set image bitmap.
+     *
+     * @param image [Bitmap]
+     */
     fun setImage(image: Bitmap) {
         binding.image.setImageBitmap(image)
     }
 
+    /**
+     * Set indicator.
+     *
+     * @param current [Int]
+     * @param max [Int]
+     */
     fun setIndicator(current: Int, max: Int) {
         binding.pageIndicator.text = "$current / $max"
     }
 
+    /**
+     * Set long tap action.
+     *
+     * @param onLongClickListener [View.OnLongClickListener]
+     */
     fun setOnLongTap(onLongClickListener: View.OnLongClickListener) {
         binding.image.setOnLongClickListener(onLongClickListener)
     }
