eea4b05d399e440b3f03e33e000944f667e06079
Toast Kid <super_q1m@yahoo.co.jp>
2019-07-10 23:37:24 +0900

    refactor #373 Add EditText import.

--- app/src/main/java/jp/toastkid/yobidashi/editor/EditorModule.kt
+++ app/src/main/java/jp/toastkid/yobidashi/editor/EditorModule.kt
@@ -14,6 +14,7 @@ import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
 import android.view.animation.Animation
+import android.widget.EditText
 import android.widget.TextView
 import androidx.annotation.Dimension
 import androidx.annotation.MainThread
