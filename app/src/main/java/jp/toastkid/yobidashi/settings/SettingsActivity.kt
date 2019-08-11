e162316f132a69236705563197c4098fcd91ef6e
Toast Kid <super_q1m@yahoo.co.jp>
2019-08-11 20:13:27 +0900

    refactor #402 Remove BaseActivity from SettingsActivity.

--- app/src/main/java/jp/toastkid/yobidashi/settings/SettingsActivity.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/SettingsActivity.kt
@@ -5,32 +5,46 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.MenuItem
 import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import jp.toastkid.yobidashi.BaseActivity
 import jp.toastkid.yobidashi.R
 import jp.toastkid.yobidashi.color_filter.ColorFilter
 import jp.toastkid.yobidashi.databinding.ActivitySettingsBinding
 import jp.toastkid.yobidashi.libs.Toaster
+import jp.toastkid.yobidashi.libs.preference.PreferenceApplier
+import jp.toastkid.yobidashi.libs.view.ToolbarColorApplier
 
 /**
  * Settings activity.
  *
  * @author toastkidjp
  */
-class SettingsActivity : BaseActivity() {
+class SettingsActivity : AppCompatActivity() {
 
     /**
      * DataBinding object.
      */
     private lateinit var binding: ActivitySettingsBinding
 
+    private lateinit var preferenceApplier: PreferenceApplier
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(LAYOUT_ID)
+
+        preferenceApplier = PreferenceApplier(this)
+
         binding = DataBindingUtil.setContentView<ActivitySettingsBinding>(this, LAYOUT_ID)
         binding.activity = this
-        initToolbar(binding.toolbar)
+        binding.toolbar.also { toolbar ->
+            toolbar.setNavigationIcon(R.drawable.ic_back)
+            toolbar.setNavigationOnClickListener { finish() }
+            toolbar.setTitle(titleId())
+            toolbar.inflateMenu(R.menu.settings_toolbar_menu)
+            toolbar.setOnMenuItemClickListener{ clickMenu(it) }
+        }
 
         supportFragmentManager?.let {
             binding.container.adapter = PagerAdapter(it, { getString(it) })
@@ -40,10 +54,22 @@ class SettingsActivity : BaseActivity() {
 
     override fun onResume() {
         super.onResume()
-        applyColorToToolbar(binding.toolbar)
+        ToolbarColorApplier()(window, binding.toolbar, preferenceApplier.colorPair())
     }
 
-    @StringRes override fun titleId(): Int = R.string.title_settings
+    private fun clickMenu(item: MenuItem) = when (item.itemId) {
+        R.id.menu_exit -> {
+            moveTaskToBack(true)
+            true
+        }
+        R.id.menu_close -> {
+            finish()
+            true
+        }
+        else -> true
+    }
+
+    @StringRes private fun titleId(): Int = R.string.title_settings
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -52,7 +78,7 @@ class SettingsActivity : BaseActivity() {
                 Toaster.snackShort(
                         binding.root,
                         R.string.message_cannot_draw_overlay,
-                        colorPair()
+                        preferenceApplier.colorPair()
                 )
                 return
             }
