464bdc358532e9281fde1d40622d68b62af1530b
Toast Kid <super_q1m@yahoo.co.jp>
2019-10-11 00:24:29 +0900

    bugfix #339 Fix BitmapScaling's bug.

--- app/src/test/java/jp/toastkid/yobidashi/libs/BitmapScalingTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/libs/BitmapScalingTest.kt
@@ -13,10 +13,18 @@ import org.robolectric.RobolectricTestRunner
 class BitmapScalingTest {
 
     @Test
-    fun test_invoke() {
+    fun test_bigger() {
         val bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ALPHA_8)
         val scaled = BitmapScaling(bitmap, 300.0, 300.0)
-        assertEquals(300, scaled.width)
-        assertEquals(300, scaled.height)
+        assertEquals(200, scaled.width)
+        assertEquals(200, scaled.height)
+    }
+
+    @Test
+    fun test_smaller() {
+        val bitmap = Bitmap.createBitmap(300, 300, Bitmap.Config.ALPHA_8)
+        val scaled = BitmapScaling(bitmap, 200.0, 200.0)
+        assertEquals(200, scaled.width)
+        assertEquals(200, scaled.height)
     }
 }
