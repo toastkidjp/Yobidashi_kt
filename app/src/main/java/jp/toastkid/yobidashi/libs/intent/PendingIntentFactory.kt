73a680385ed8d499e84cd6c5b4ed651347b6dc71
Toast Kid <super_q1m@yahoo.co.jp>
2019-11-30 20:31:18 +0900

    refactor #442 Clean up PendingIntentFactory.

--- app/src/main/java/jp/toastkid/yobidashi/libs/intent/PendingIntentFactory.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/intent/PendingIntentFactory.kt
@@ -20,75 +20,89 @@ class PendingIntentFactory {
 
     /**
      * Make launch search intent.
-     * @param context
      *
+     * @param context
      * @return [SearchActivity]'s pending intent
      */
-    fun makeSearchLauncher(context: Context): PendingIntent {
-        val intent = SearchActivity.makeIntent(context)
-        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
+    fun makeSearchLauncher(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    0,
+                    SearchActivity.makeIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
     /**
      * Make launch main activity.
-     * @param context
      *
+     * @param context
      * @return
      */
-    fun main(context: Context): PendingIntent {
-        return PendingIntent.getActivity(
-                context,
-                3,
-                MainActivity.makeIntent(context),
-                PendingIntent.FLAG_UPDATE_CURRENT
-        )
-    }
+    fun main(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    3,
+                    MainActivity.makeIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
     /**
      * Return Launcher Intent of Launcher Activity.
-
-     * @param context
      *
+     * @param context
      * @return
      */
-    fun launcher(context: Context): PendingIntent {
-        return PendingIntent.getActivity(
-                context,
-                6,
-                LauncherActivity.makeIntent(context),
-                PendingIntent.FLAG_UPDATE_CURRENT
-        )
-    }
+    fun launcher(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    6,
+                    LauncherActivity.makeIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
     /**
      * Make barcode reader intent.
-     * @param context
      *
+     * @param context
      * @return
      */
-    fun barcode(context: Context): PendingIntent {
-        val intent = BarcodeReaderActivity.makeIntent(context)
-        return PendingIntent.getActivity(context, 8, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
+    fun barcode(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    8,
+                    BarcodeReaderActivity.makeIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
-    fun browser(context: Context): PendingIntent {
-        val intent =
-                MainActivity.makeBrowserIntent(context, PreferenceApplier(context).homeUrl.toUri())
-        return PendingIntent.getActivity(context, 9, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
+    fun browser(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    9,
+                    MainActivity.makeBrowserIntent(context, PreferenceApplier(context).homeUrl.toUri()),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
-    fun bookmark(context: Context): PendingIntent {
-        val intent = BookmarkActivity.makeIntent(context)
-        return PendingIntent.getActivity(context, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
+    fun bookmark(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    10,
+                    BookmarkActivity.makeIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
-    fun randomWikipedia(context: Context): PendingIntent {
-        val intent = MainActivity.makeRandomWikipediaIntent(context)
-        return PendingIntent.getActivity(context, 11, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
+    fun randomWikipedia(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    11,
+                    MainActivity.makeRandomWikipediaIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
-    fun setting(context: Context): PendingIntent {
-        val intent = SettingsActivity.makeIntent(context)
-        return PendingIntent.getActivity(context, 12, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
+    fun setting(context: Context): PendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    12,
+                    SettingsActivity.makeIntent(context),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
 }
