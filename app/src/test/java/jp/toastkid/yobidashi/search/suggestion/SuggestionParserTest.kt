328e71d1f1b281ff000fd0aff0a94c73fb7ec39e
Toast Kid <super_q1m@yahoo.co.jp>
2019-12-22 20:18:11 +0900

    refactor #442 Extract to constant.

--- app/src/test/java/jp/toastkid/yobidashi/search/suggestion/SuggestionParserTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/search/suggestion/SuggestionParserTest.kt
@@ -17,10 +17,13 @@ class SuggestionParserTest {
      */
     @Test
     fun test_parse() {
-        val file = File(javaClass.classLoader?.getResource("suggestion/google.xml")?.toURI())
+        val file = File(javaClass.classLoader?.getResource(PATH_TO_RESOURCE)?.toURI())
         val xml = Okio.buffer(Okio.source(file)).use { it.readUtf8() }
         val suggestions = SuggestionParser()(xml)
         assertEquals(10, suggestions.size)
     }
 
+    companion object {
+        private const val PATH_TO_RESOURCE = "suggestion/google.xml"
+    }
 }
