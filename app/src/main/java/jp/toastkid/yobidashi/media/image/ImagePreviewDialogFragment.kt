f57d9840b61d3f09be055e139524a80b86612435
Toast Kid <super_q1m@yahoo.co.jp>
2020-02-08 10:22:14 +0900

    feature #506 Implement image preview DialogFragment.

---
+++ app/src/main/java/jp/toastkid/yobidashi/media/image/ImagePreviewDialogFragment.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.media.image
+
+import android.app.Dialog
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Matrix
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import io.reactivex.Maybe
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import jp.toastkid.yobidashi.R
+import jp.toastkid.yobidashi.databinding.DialogImagePreviewBinding
+import jp.toastkid.yobidashi.libs.ImageLoader
+import timber.log.Timber
+import java.io.File
+
+/**
+ * @author toastkidjp
+ */
+class ImagePreviewDialogFragment  : DialogFragment() {
+
+    private lateinit var binding: DialogImagePreviewBinding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        setStyle(STYLE_NO_TITLE, R.style.FullScreenDialogStyle)
+
+        val activityContext = context
+                ?: return super.onCreateDialog(savedInstanceState)
+
+        val path = arguments?.getString("image")
+                ?: return super.onCreateDialog(savedInstanceState)
+
+        binding = DataBindingUtil.inflate(
+                LayoutInflater.from(activityContext),
+                R.layout.dialog_image_preview,
+                null,
+                false
+        )
+
+        binding.dialog = this
+
+        val displayMetrics = resources.displayMetrics
+        val layoutParams = binding.photo.layoutParams
+        layoutParams.width = displayMetrics.widthPixels
+        layoutParams.height = displayMetrics.heightPixels
+        binding.photo.layoutParams = layoutParams
+
+        Maybe.fromCallable {
+            ImageLoader.loadBitmap(
+                    activityContext,
+                    Uri.parse(File(path).toURI().toString())
+            )
+        }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            binding.photo.setImageBitmap(it)
+                        },
+                        Timber::e
+                )
+
+        binding.photo.maximumScale = 100f
+
+        return AlertDialog.Builder(activityContext)
+                .setView(binding.root)
+                .create()
+                .also {
+                    it.window?.also { window ->
+                        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT)
+                    }
+                }
+    }
+
+    private val horizontalMatrix = Matrix().also {
+        it.preScale(-1f, 1f)
+    }
+
+    fun reverse() {
+        val current = binding.photo.drawable.toBitmap()
+        val horizontalFlipped = Bitmap.createBitmap(
+                current,
+                0,
+                0,
+                current.getWidth(),
+                current.getHeight(),
+                horizontalMatrix,
+                false
+        )
+        binding.photo.setImageBitmap(horizontalFlipped)
+    }
+
+    companion object {
+
+        fun withImage(image: Image): ImagePreviewDialogFragment {
+            return ImagePreviewDialogFragment().also {
+                it.arguments = bundleOf("image" to image.path)
+            }
+        }
+    }
+
+}
