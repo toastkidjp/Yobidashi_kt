949c40bf6eea5739f1ae3f67c5cb1c582e6e4fb2
Toast Kid <super_q1m@yahoo.co.jp>
2020-02-08 10:23:44 +0900

    feature #506 Implement image loader.

---
+++ app/src/main/java/jp/toastkid/yobidashi/media/image/ImageLoader.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.media.image
+
+import android.content.ContentResolver
+import android.provider.MediaStore
+
+/**
+ * @author toastkidjp
+ */
+class ImageLoader(private val contentResolver: ContentResolver) {
+
+    operator fun invoke(bucket: String): List<Image> {
+        val cursor = MediaStore.Images.Media.query(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                arrayOf(
+                        MediaStore.Images.Media.DATA,
+                        MediaStore.Images.Media.DISPLAY_NAME,
+                        MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+                ),
+                "bucket_display_name = ?",
+                arrayOf(bucket),
+                null
+        )
+
+        val images = mutableListOf<Image>()
+
+        val dataIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+        val displayNameIndex = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+
+        while (cursor?.moveToNext() == true) {
+            images.add(
+                    Image(
+                            cursor.getString(dataIndex),
+                            cursor.getString(displayNameIndex),
+                            false
+                    )
+            )
+        }
+        return images
+    }
+}
