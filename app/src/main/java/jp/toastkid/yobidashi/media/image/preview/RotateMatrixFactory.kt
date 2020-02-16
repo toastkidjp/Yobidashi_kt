4d27bdc449a14ae533e85959cb72a54957ac0183
Toast Kid <super_q1m@yahoo.co.jp>
2020-02-16 09:28:33 +0900

    refactor #506 Move image preview classes to preview package.

--- app/src/main/java/jp/toastkid/yobidashi/media/image/preview/RotateMatrixFactory.kt
+++ app/src/main/java/jp/toastkid/yobidashi/media/image/preview/RotateMatrixFactory.kt
@@ -5,7 +5,7 @@
  * which accompany this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
  */
-package jp.toastkid.yobidashi.media.image
+package jp.toastkid.yobidashi.media.image.preview
 
 import android.graphics.Matrix
 
