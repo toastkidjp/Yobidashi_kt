2504bb48d3cca12023e5652b9ec1a1d295a9e360
Toast Kid <super_q1m@yahoo.co.jp>
2020-02-19 15:29:30 +0900

    feature #518 Implement Speechmaker.

---
+++ app/src/main/java/jp/toastkid/yobidashi/libs/speech/SpeechMaker.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.libs.speech
+
+import android.content.Context
+import android.os.Build
+import android.speech.tts.TextToSpeech
+import java.util.*
+import kotlin.math.min
+
+/**
+ * @author toastkidjp
+ */
+class SpeechMaker(context: Context) {
+
+    private var successInitialization = false
+
+    private val textToSpeech = TextToSpeech(
+            context,
+            TextToSpeech.OnInitListener { result -> successInitialization = result == TextToSpeech.SUCCESS }
+    )
+
+    operator fun invoke(message: String) {
+        if (textToSpeech.isSpeaking) {
+            stop()
+            return
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(
+                    message.substring(min(message.length, 3999)),
+                    TextToSpeech.QUEUE_FLUSH,
+                    null,
+                    UUID.randomUUID().toString()
+            )
+        }
+    }
+
+    fun stop() {
+        textToSpeech.stop()
+    }
+
+    fun dispose() {
+        stop()
+        textToSpeech.shutdown()
+    }
+}
