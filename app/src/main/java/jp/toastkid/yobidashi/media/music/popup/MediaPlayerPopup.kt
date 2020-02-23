a9dd7ca234892a9a1fcb250172e1b8112054e38f
Toast Kid <super_q1m@yahoo.co.jp>
2020-02-23 17:44:51 +0900

    refactor #496 Implement PlaybackSpeedAdapter.

--- app/src/main/java/jp/toastkid/yobidashi/media/music/popup/MediaPlayerPopup.kt
+++ app/src/main/java/jp/toastkid/yobidashi/media/music/popup/MediaPlayerPopup.kt
@@ -46,6 +46,7 @@ import jp.toastkid.yobidashi.databinding.PopupMediaPlayerBinding
 import jp.toastkid.yobidashi.libs.Toaster
 import jp.toastkid.yobidashi.libs.preference.PreferenceApplier
 import jp.toastkid.yobidashi.media.music.MediaPlayerService
+import jp.toastkid.yobidashi.media.music.popup.playback.speed.PlaybackSpeedAdapter
 import timber.log.Timber
 
 /**
@@ -176,6 +177,8 @@ class MediaPlayerPopup(private val context: Context) {
         binding.playingSpeed.isVisible = true
 
         PlayingSpeedSpinnerInitializer().invoke(binding.playingSpeed)
+        binding.playingSpeed.adapter = PlaybackSpeedAdapter(LayoutInflater.from(binding.root.context))
+        binding.playingSpeed.setSelection(PlayingSpeed.getDefault().findIndex())
         binding.playingSpeed.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) = Unit
 
---
+++ app/src/main/java/jp/toastkid/yobidashi/media/music/popup/playback/speed/PlaybackSpeedAdapter.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.media.music.popup.playback.speed
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import jp.toastkid.yobidashi.R
+import jp.toastkid.yobidashi.libs.preference.PreferenceApplier
+import jp.toastkid.yobidashi.media.music.popup.PlayingSpeed
+
+/**
+ * @author toastkidjp
+ */
+class PlaybackSpeedAdapter(private val layoutInflater: LayoutInflater) : BaseAdapter() {
+
+    private val playingSpeeds = PlayingSpeed.values()
+
+    override fun getCount(): Int = playingSpeeds.size
+
+    override fun getItem(position: Int): PlayingSpeed = playingSpeeds[position]
+
+    override fun getItemId(position: Int): Long = playingSpeeds[position].textId.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val playingSpeed = playingSpeeds[position]
+
+        val view = layoutInflater.inflate(R.layout.item_playback_speed_spinner, parent, false)
+        view.findViewById<TextView>(R.id.text).also {
+            it.setText(playingSpeed.textId)
+            val colorPair = PreferenceApplier(it.context).colorPair()
+            it.setBackgroundColor(colorPair.bgColor())
+            it.setTextColor(colorPair.fontColor())
+        }
+        return view
+    }
+}
