8d3d5e54de60be7b64575089a8cc3fbb97ffb44e
Toast Kid <super_q1m@yahoo.co.jp>
2020-02-26 00:13:47 +0900

    feature #435 Replace image loading with Glide. Fix #435.

--- app/src/main/java/jp/toastkid/yobidashi/media/image/ViewHolder.kt
+++ app/src/main/java/jp/toastkid/yobidashi/media/image/ViewHolder.kt
@@ -7,19 +7,11 @@
  */
 package jp.toastkid.yobidashi.media.image
 
-import android.text.TextUtils
-import android.widget.ImageView
+import android.graphics.drawable.Drawable
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
-import io.reactivex.Maybe
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import jp.toastkid.yobidashi.R
+import com.bumptech.glide.Glide
 import jp.toastkid.yobidashi.databinding.ItemImageThumbnailsBinding
-import jp.toastkid.yobidashi.libs.BitmapScaling
-import jp.toastkid.yobidashi.libs.ImageLoader
-import jp.toastkid.yobidashi.settings.background.RotatedImageFixing
-import timber.log.Timber
 import java.io.File
 
 /**
@@ -29,12 +21,10 @@ import java.io.File
  *
  * @author toastkidjp
  */
-internal class ViewHolder(private val binding: ItemImageThumbnailsBinding)
-    : RecyclerView.ViewHolder(binding.root) {
-
-    private val contentResolver = binding.root.context.contentResolver
-
-    private val rotatedImageFixing = RotatedImageFixing()
+internal class ViewHolder(
+        private val binding: ItemImageThumbnailsBinding,
+        private val placeholder: Drawable?
+) : RecyclerView.ViewHolder(binding.root) {
 
     /**
      * Apply file content.
@@ -42,31 +32,16 @@ internal class ViewHolder(private val binding: ItemImageThumbnailsBinding)
      * @param image image
      */
     fun applyContent(image: Image, onClick: (Image) -> Unit) {
-        setImageTo(this.binding.image, image.path)
+        Glide.with(itemView.context)
+                .load(File(image.path).toURI().toString().toUri())
+                .placeholder(placeholder)
+                .override(300)
+                .into(binding.image)
+
         this.binding.text.text = image.name
         this.binding.root.setOnClickListener {
             onClick(image)
         }
-    }
-
-    private fun setImageTo(iv: ImageView, imagePath: String) {
-        iv.setImageResource(R.drawable.ic_image_search)
-        if (TextUtils.isEmpty(imagePath)) {
-            return
-        }
-
-        val uri = File(imagePath).toURI().toString().toUri()
-        Maybe.fromCallable { ImageLoader.loadBitmap(iv.context, uri) }
-                .subscribeOn(Schedulers.io())
-                .map {
-                    val bitmap = BitmapScaling(it, 300.0, 300.0)
-                    rotatedImageFixing.invoke(contentResolver, bitmap, uri)
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        iv::setImageBitmap,
-                        Timber::e
-                )
     }
 
 }
