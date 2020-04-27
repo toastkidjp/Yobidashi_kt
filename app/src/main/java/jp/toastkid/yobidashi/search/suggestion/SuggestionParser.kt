dd5329dc4686f7c298308b02904c902e08c81b90
Toast Kid <super_q1m@yahoo.co.jp>
2020-04-28 00:14:54 +0900

    bugfix #412 Add guard.

--- app/src/main/java/jp/toastkid/yobidashi/search/suggestion/SuggestionParser.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/suggestion/SuggestionParser.kt
@@ -17,13 +17,15 @@ internal class SuggestionParser {
      * @return suggest words
      */
     operator fun invoke(response: String): List<String> {
+        // TODO rewrite implementation.
         val split = response.split("</CompleteSuggestion>").dropLastWhile { it.isEmpty() }
         val suggestions = ArrayList<String>(split.size)
 
         split.forEach {
             val matcher = PATTERN.matcher(it)
             if (matcher.find()) {
-                suggestions.add(matcher.group(1))
+                val element = matcher.group(1) ?: return@forEach
+                suggestions.add(element)
             }
         }
         return suggestions
