5db1220da21a8a6b8208741f05cce4cdd978fb47
Toast Kid <super_q1m@yahoo.co.jp>
2020-05-06 10:11:38 +0900

    refactor #442 Use ViewStub for PageSearcher.

--- app/src/main/java/jp/toastkid/yobidashi/browser/page_search/PageSearcherModule.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/page_search/PageSearcherModule.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import androidx.core.view.isVisible
+import androidx.databinding.ViewStubProxy
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -25,50 +26,103 @@ import kotlinx.coroutines.withContext
 
 /**
  * Module for find in page.
- *
+ * TODO Clean up code.
  * @author toastkidjp
  */
-class PageSearcherModule(
-        fragmentActivity: FragmentActivity,
-        private val binding: ModuleSearcherBinding
-) {
-
-    /**
-     * View context.
-     */
-    private val context = binding.root.context
+class PageSearcherModule(private val viewStubProxy: ViewStubProxy) {
 
     /**
      * This value is used by show/hide animation.
      */
-    private val height = context.resources.getDimension(R.dimen.toolbar_height)
+    private var height = 0f
 
     /**
      * Use for open software keyboard.
+     * TODO Delete it.
      */
-    private val editText: EditText
+    private lateinit var editText: EditText
 
     private val channel = Channel<String>()
 
-    init {
+    fun isVisible() = viewStubProxy.isInflated && viewStubProxy.root?.isVisible == true
+
+    fun switch() {
+        if (isVisible()) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    /**
+     * Show module with opening software keyboard.
+     *
+     * @param activity [Activity]
+     */
+    fun show() {
+        if (!viewStubProxy.isInflated) {
+            initialize()
+        }
+        viewStubProxy.root?.animate()?.let {
+            it.cancel()
+            it.translationY(0f)
+                    .setDuration(ANIMATION_DURATION)
+                    .withStartAction { switchVisibility(View.GONE, View.VISIBLE) }
+                    .withEndAction {
+                        editText.requestFocus()
+                        (editText.context as? Activity)?.also { activity ->
+                            Inputs.showKeyboard(activity, editText)
+                        }
+                    }
+                    .start()
+        }
+    }
+
+    /**
+     * Hide module.
+     */
+    fun hide() {
+        viewStubProxy.root?.animate()?.let {
+            it.cancel()
+            it.translationY(-height)
+                    .setDuration(ANIMATION_DURATION)
+                    .withStartAction { Inputs.hideKeyboard(editText) }
+                    .withEndAction { switchVisibility(View.VISIBLE, View.GONE) }
+                    .start()
+        }
+    }
+
+    private fun switchVisibility(from: Int, to: Int) {
+        CoroutineScope(Dispatchers.Main).launch {
+            if (viewStubProxy.root?.visibility == from) {
+                viewStubProxy.root?.visibility = to
+            }
+        }
+    }
+
+    fun initialize() {
+        val context = viewStubProxy.viewStub?.context ?: return
+
+        viewStubProxy.viewStub?.inflate()
+
+        val binding = viewStubProxy.binding as? ModuleSearcherBinding ?: return
+
         TextInputs.setEmptyAlert(binding.inputLayout)
 
-        val viewModel = ViewModelProviders.of(fragmentActivity).get(PageSearcherViewModel::class.java)
+        val viewModel = (context as? FragmentActivity)?.let {
+            ViewModelProviders.of(it).get(PageSearcherViewModel::class.java)
+        }
 
         binding.viewModel = viewModel
 
-        setBackgroundColor()
-
-        editText = binding.inputLayout.editText as EditText
-
-        editText.let { editText ->
-            editText.setOnEditorActionListener { input, _, _ ->
-                viewModel.findDown(input.text.toString())
-                Inputs.hideKeyboard(editText)
+        binding.inputLayout.editText?.also {
+            it.setOnEditorActionListener { input, _, _ ->
+                viewModel?.findDown(input.text.toString())
+                Inputs.hideKeyboard(it)
                 true
             }
 
-            editText.addTextChangedListener(object : TextWatcher{
+            it.addTextChangedListener(object : TextWatcher {
                 override fun afterTextChanged(p0: Editable?) = Unit
 
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
@@ -85,94 +139,37 @@ class PageSearcherModule(
                         .debounce(1000)
                         .collect {
                             withContext(Dispatchers.Main) {
-                                viewModel.find(it)
+                                viewModel?.find(it)
                             }
                         }
             }
+            editText = it
         }
 
         (context as? FragmentActivity)?.let { activity ->
-            viewModel.clear.observe(activity, Observer {
+            viewModel?.clear?.observe(activity, Observer {
                 editText.setText("")
             })
 
-            viewModel.close.observe(activity, Observer {
-                binding.root.animate().let {
-                    it.cancel()
-                    it.translationY(-height)
-                            .setDuration(ANIMATION_DURATION)
-                            .withStartAction { Inputs.hideKeyboard(editText) }
-                            .withEndAction { switchVisibility(View.VISIBLE, View.GONE) }
-                            .start()
-                }
-            })
+            viewModel?.close?.observe(activity, Observer { hide() })
         }
 
-        hide()
+        if (height == 0f) {
+            height = binding.root.context.resources.getDimension(R.dimen.toolbar_height)
+        }
+
+        setBackgroundColor(binding)
     }
 
     /**
      * Set background color to views.
      */
-    private fun setBackgroundColor() {
-        PreferenceApplier(context).colorPair().bgColor().also {
+    private fun setBackgroundColor(binding: ModuleSearcherBinding) {
+        PreferenceApplier(binding.root.context).colorPair().bgColor().also {
             binding.close.setColorFilter(it)
             binding.sipClear.setColorFilter(it)
             binding.sipUpward.setColorFilter(it)
             binding.sipDownward.setColorFilter(it)
-        }
-    }
-
-    fun isVisible() = binding.root.isVisible
-
-    fun switch() {
-        if (isVisible()) {
-            hide()
-        } else {
-            show()
-        }
-    }
-
-    /**
-     * Show module with opening software keyboard.
-     *
-     * @param activity [Activity]
-     */
-    fun show() {
-        binding.root.animate()?.let {
-            it.cancel()
-            it.translationY(0f)
-                    .setDuration(ANIMATION_DURATION)
-                    .withStartAction { switchVisibility(View.GONE, View.VISIBLE) }
-                    .withEndAction {
-                        editText.requestFocus()
-                        (context as? Activity)?.also { activity ->
-                            Inputs.showKeyboard(activity, editText)
-                        }
-                    }
-                    .start()
-        }
-    }
-
-    /**
-     * Hide module.
-     */
-    fun hide() {
-        binding.root.animate()?.let {
-            it.cancel()
-            it.translationY(-height)
-                    .setDuration(ANIMATION_DURATION)
-                    .withStartAction { Inputs.hideKeyboard(editText) }
-                    .withEndAction { switchVisibility(View.VISIBLE, View.GONE) }
-                    .start()
-        }
-    }
-
-    private fun switchVisibility(from: Int, to: Int) {
-        CoroutineScope(Dispatchers.Main).launch {
-            if (binding.root.visibility == from) {
-                binding.root.visibility = to
-            }
         }
     }
 
