a90161821e9a06fdfd2838b9f916bb31d563acb9
Toast Kid <super_q1m@yahoo.co.jp>
2020-05-13 00:16:38 +0900

    refactor #442 Modify branching.

--- app/src/main/java/jp/toastkid/yobidashi/main/LocaleWrapper.kt
+++ app/src/main/java/jp/toastkid/yobidashi/main/LocaleWrapper.kt
@@ -16,14 +16,15 @@ object LocaleWrapper {
     }
 
     fun getLocale(configuration: Configuration): String {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            val locales = configuration.locales
-            if (locales.isEmpty) {
-                return Locale.getDefault().language
-            }
-            return locales.get(0).language
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            return configuration.locale.language
         }
-        return configuration.locale.language
+
+        val locales = configuration.locales
+        if (locales.isEmpty) {
+            return Locale.getDefault().language
+        }
+        return locales.get(0).language
     }
 
 }
