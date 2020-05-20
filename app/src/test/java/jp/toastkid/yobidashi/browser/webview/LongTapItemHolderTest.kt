89cc4eb8e77a0ad70a360374988129dbe3be5630
Toast Kid <super_q1m@yahoo.co.jp>
2020-05-21 07:54:38 +0900

    refactor #559 Implement LongTapItemHolder's test case.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/webview/LongTapItemHolderTest.kt
@@ -0,0 +1,44 @@
+package jp.toastkid.yobidashi.browser.webview
+
+import android.os.Bundle
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class LongTapItemHolderTest {
+
+    private lateinit var longTapItemHolder: LongTapItemHolder
+
+    @Before
+    fun setUp() {
+        longTapItemHolder = LongTapItemHolder()
+    }
+
+    @Test
+    fun reset() {
+        longTapItemHolder.title = "title"
+        longTapItemHolder.anchor = "https://test.com"
+
+        longTapItemHolder.reset()
+
+        assertTrue(longTapItemHolder.title.isEmpty())
+        assertTrue(longTapItemHolder.anchor.isEmpty())
+    }
+
+    @Test
+    fun extract() {
+        val bundle = mockk<Bundle>()
+        every { bundle.get("title") }.returns("test_title")
+        every { bundle.get("url") }.returns("https://test.com")
+
+        longTapItemHolder.extract(bundle)
+
+        assertEquals("test_title", longTapItemHolder.title)
+        assertEquals("https://test.com", longTapItemHolder.anchor)
+    }
+}
