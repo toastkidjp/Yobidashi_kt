3fb399ba8fbb83201e20fff21b73a70119fed3f9
Toast Kid <super_q1m@yahoo.co.jp>
2020-06-26 08:09:57 +0900

    design #496 Add ellipsis end.

---
+++ app/src/test/java/jp/toastkid/yobidashi/libs/ad/AdServiceTest.kt
@@ -0,0 +1,21 @@
+package jp.toastkid.yobidashi.libs.ad
+
+import org.junit.Assert.*
+
+/**
+ * @author toastkidjp
+ */
+class AdServiceTest {
+
+    @org.junit.Test
+    fun load() {
+    }
+
+    @org.junit.Test
+    fun sendWith() {
+    }
+
+    @org.junit.Test
+    fun destroy() {
+    }
+}
