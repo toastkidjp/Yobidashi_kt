68c866f5cc1165bd802cdc97a5464d4a0b30aaac
Toast Kid <super_q1m@yahoo.co.jp>
2020-06-26 08:12:07 +0900

    refactor #575 Implement AdService's test case.

--- app/src/test/java/jp/toastkid/yobidashi/libs/ad/AdServiceTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/libs/ad/AdServiceTest.kt
@@ -1,21 +1,69 @@
 package jp.toastkid.yobidashi.libs.ad
 
-import org.junit.Assert.*
+import com.google.android.gms.ads.AdView
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
+import jp.toastkid.yobidashi.main.AppBarViewModel
+import org.junit.Before
+import org.junit.Test
 
 /**
  * @author toastkidjp
  */
 class AdServiceTest {
 
-    @org.junit.Test
+    @MockK
+    private lateinit var adViewFactory: AdViewFactory
+
+    @MockK
+    private lateinit var adView: AdView
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
     fun load() {
+        every { adView.setAdSize(any()) }.answers { Unit }
+        every { adView.loadAd(any()) }.answers { Unit }
+        every { adViewFactory.invoke(any()) }.answers { adView }
+        val service = AdService(adViewFactory, mockk())
+
+        service.load()
+
+        verify(atLeast = 1) { adView.loadAd(any()) }
     }
 
-    @org.junit.Test
+    @Test
     fun sendWith() {
+        every { adView.setAdSize(any()) }.answers { Unit }
+        every { adViewFactory.invoke(any()) }.answers { adView }
+        val service = AdService(adViewFactory, mockk())
+
+        val appBarViewModel = mockk<AppBarViewModel>()
+        every { appBarViewModel.replace(any()) }.answers { Unit }
+
+        service.sendWith(appBarViewModel)
+
+        verify(atLeast = 1) { appBarViewModel.replace(adView) }
     }
 
-    @org.junit.Test
+    @Test
     fun destroy() {
+        every { adView.setAdSize(any()) }.answers { Unit }
+        every { adView.destroy() }.answers { Unit }
+        every { adViewFactory.invoke(any()) }.answers { adView }
+        val service = AdService(adViewFactory, mockk())
+
+        val appBarViewModel = mockk<AppBarViewModel>()
+        every { appBarViewModel.replace(any()) }.answers { Unit }
+
+        service.destroy()
+
+        verify(atLeast = 1) { adView.destroy() }
     }
 }
