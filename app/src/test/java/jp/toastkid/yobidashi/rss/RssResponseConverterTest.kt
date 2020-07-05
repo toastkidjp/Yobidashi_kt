3356c30899aa77af6c38c70dd0a84f85de20e7a6
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-06 06:35:00 +0900

    refactor #559 Implement RssResponseConverter's test.

--- app/src/test/java/jp/toastkid/yobidashi/rss/RssResponseConverterTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/rss/RssResponseConverterTest.kt
@@ -1,8 +1,42 @@
 package jp.toastkid.yobidashi.rss
 
-import org.junit.Assert.*
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
+import jp.toastkid.yobidashi.rss.model.Parser
+import jp.toastkid.yobidashi.rss.model.Rss
+import okhttp3.ResponseBody
+import org.junit.Before
+import org.junit.Test
 
 /**
  * @author toastkidjp
  */
-class RssResponseConverterTest
+class RssResponseConverterTest {
+
+    @MockK
+    private lateinit var parser: Parser
+
+    @MockK
+    private lateinit var responseBody: ResponseBody
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun test() {
+        every { parser.parse(any()) }.answers { Rss() }
+
+        val converter = RssResponseConverter(parser)
+        every { responseBody.string() }.answers { "" }
+
+        converter.convert(responseBody)
+
+        verify(exactly = 1) { parser.parse(any()) }
+        verify(exactly = 1) { responseBody.string() }
+    }
+}
