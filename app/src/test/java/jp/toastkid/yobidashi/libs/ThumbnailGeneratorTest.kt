29ef9c78864edd5a53dc0c8d94ea01c2c4562cba
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-07 06:53:04 +0900

    refactor #559 Implement ThumbnailGenerator's test.

--- app/src/test/java/jp/toastkid/yobidashi/libs/ThumbnailGeneratorTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/libs/ThumbnailGeneratorTest.kt
@@ -1,8 +1,58 @@
 package jp.toastkid.yobidashi.libs
 
-import org.junit.Assert.*
+import android.view.View
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
 
 /**
  * @author toastkidjp
  */
-class ThumbnailGeneratorTest
+class ThumbnailGeneratorTest {
+
+    private lateinit var thumbnailGenerator: ThumbnailGenerator
+
+    @MockK
+    private lateinit var view: View
+
+    @Before
+    fun setUp() {
+        thumbnailGenerator = ThumbnailGenerator()
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun testNull() {
+        thumbnailGenerator.invoke(null)
+    }
+
+    @Test
+    fun test() {
+        every { view.getMeasuredWidth() }.answers { 0 }
+        every { view.getMeasuredHeight() }.answers { 0 }
+        every { view.invalidate() }.answers { Unit }
+        every { view.draw(any()) }.answers { Unit }
+
+        thumbnailGenerator.invoke(view)
+
+        verify(exactly = 0) { view.invalidate() }
+        verify(exactly = 0) { view.draw(any()) }
+    }
+
+    @Test
+    fun test1() {
+        every { view.getMeasuredWidth() }.answers { 1 }
+        every { view.getMeasuredHeight() }.answers { 1 }
+        every { view.invalidate() }.answers { Unit }
+        every { view.draw(any()) }.answers { Unit }
+
+        thumbnailGenerator.invoke(view)
+
+        verify(exactly = 1) { view.invalidate() }
+        verify(exactly = 1) { view.draw(any()) }
+    }
+
+}
