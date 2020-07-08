c457937aa0e153b592c3a3a6b1e060b7a1e2fd47
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-09 06:31:04 +0900

    refactor #559 Implement ContentHolderService's test.

--- app/src/test/java/jp/toastkid/yobidashi/editor/ContentHolderServiceTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/editor/ContentHolderServiceTest.kt
@@ -1,8 +1,27 @@
 package jp.toastkid.yobidashi.editor
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
 
 /**
  * @author toastkidjp
  */
-class ContentHolderServiceTest
+class ContentHolderServiceTest {
+
+    private lateinit var contentHolderService: ContentHolderService
+
+    @Before
+    fun setUp() {
+        contentHolderService = ContentHolderService()
+    }
+
+    @Test
+    fun test() {
+        assertTrue(contentHolderService.isBlank())
+
+        contentHolderService.setContent("tomato")
+        assertFalse(contentHolderService.isBlank())
+        assertEquals("tomato", contentHolderService.getContent())
+    }
+}
