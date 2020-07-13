ce00ae31f63130254c09ac1561aa3928413a5e84
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-14 08:26:57 +0900

    refactor #580 Optimize imports.

--- article/src/main/java/jp/toastkid/article_viewer/article/detail/subhead/SubheadDialogFragment.kt
+++ article/src/main/java/jp/toastkid/article_viewer/article/detail/subhead/SubheadDialogFragment.kt
@@ -7,18 +7,15 @@
  */
 package jp.toastkid.article_viewer.article.detail.subhead
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import jp.toastkid.article_viewer.R
 
 /**
  * @author toastkidjp
