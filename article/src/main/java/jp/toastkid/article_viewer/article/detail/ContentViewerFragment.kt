cfe0317d8014b2451409cb70ea708bb9670a8f7b
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-14 08:26:33 +0900

    refactor #580 Remove unused import.

--- article/src/main/java/jp/toastkid/article_viewer/article/detail/ContentViewerFragment.kt
+++ article/src/main/java/jp/toastkid/article_viewer/article/detail/ContentViewerFragment.kt
@@ -15,7 +15,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
