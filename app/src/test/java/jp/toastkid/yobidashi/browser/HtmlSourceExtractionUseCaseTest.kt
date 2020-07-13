961c7dc924b671059b643d6b522980c631225b1b
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-14 08:26:10 +0900

    refactor #580 Implement HtmlSourceExtractionUseCase' test cases.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/HtmlSourceExtractionUseCaseTest.kt
@@ -0,0 +1,33 @@
+package jp.toastkid.yobidashi.browser
+
+import android.webkit.WebView
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class HtmlSourceExtractionUseCaseTest {
+
+    @MockK
+    private lateinit var webView: WebView
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun test() {
+        every { webView.evaluateJavascript(any(), any()) }.answers { Unit }
+
+        HtmlSourceExtractionUseCase().invoke(webView, mockk())
+
+        verify (exactly = 1) { webView.evaluateJavascript(any(), any()) }
+    }
+}
