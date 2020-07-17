820b515e9e99e8a3118511f387b86ebbb06df0cf
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-18 07:21:59 +0900

    feature #335 Re-order menu items.

--- app/src/main/java/jp/toastkid/yobidashi/menu/Menu.kt
+++ app/src/main/java/jp/toastkid/yobidashi/menu/Menu.kt
@@ -15,27 +15,23 @@ enum class Menu(
         @param:StringRes val titleId: Int,
         @param:DrawableRes val iconId: Int
 ) {
-    WEB_SEARCH(R.string.title_search, R.drawable.ic_web_search),
+    FIND_IN_PAGE(R.string.title_find_in_page, R.drawable.ic_find_in_page),
 
     TOP(R.string.title_menu_to_top, R.drawable.ic_top),
 
     BOTTOM(R.string.title_menu_to_bottom, R.drawable.ic_bottom),
 
-    FIND_IN_PAGE(R.string.title_find_in_page, R.drawable.ic_find_in_page),
-
-    VIEW_ARCHIVE(R.string.title_archives, R.drawable.ic_view_archive),
+    WEB_SEARCH(R.string.title_search, R.drawable.ic_web_search),
 
     SHARE(R.string.section_title_share, R.drawable.ic_share),
 
-    LOAD_HOME(R.string.title_load_home, R.drawable.ic_home),
+    VIEW_ARCHIVE(R.string.title_archives, R.drawable.ic_view_archive),
 
     VIEW_HISTORY(R.string.title_view_history, R.drawable.ic_history),
 
     BOOKMARK(R.string.title_bookmark, R.drawable.ic_bookmark),
 
-    RANDOM_WIKIPEDIA(R.string.menu_random_wikipedia, R.drawable.ic_wikipedia_white),
-
-    WHAT_HAPPENED_TODAY(R.string.menu_what_happened_today, R.drawable.ic_what_happened_today),
+    LOAD_HOME(R.string.title_load_home, R.drawable.ic_home),
 
     EDITOR(R.string.title_editor, R.drawable.ic_edit),
 
@@ -45,17 +41,21 @@ enum class Menu(
 
     CODE_READER(R.string.title_code_reader, R.drawable.ic_barcode),
 
+    RANDOM_WIKIPEDIA(R.string.menu_random_wikipedia, R.drawable.ic_wikipedia_white),
+
+    WHAT_HAPPENED_TODAY(R.string.menu_what_happened_today, R.drawable.ic_what_happened_today),
+
     PLANNING_POKER(R.string.title_planning_poker, R.drawable.ic_card),
 
     APP_LAUNCHER(R.string.title_apps_launcher, R.drawable.ic_launcher),
+
+    GESTURE_MEMO(R.string.title_gesture_memo, R.drawable.ic_gesture_memo),
 
     RSS_READER(R.string.title_rss_reader, R.drawable.ic_rss_feed),
 
     AUDIO(R.string.title_audio_player, R.drawable.ic_music),
 
     IMAGE_VIEWER(R.string.title_image_viewer, R.drawable.ic_photo),
-
-    GESTURE_MEMO(R.string.title_gesture_memo, R.drawable.ic_gesture_memo),
 
     OVERLAY_COLOR_FILTER(R.string.title_color_filter, R.drawable.ic_color_filter),
 
