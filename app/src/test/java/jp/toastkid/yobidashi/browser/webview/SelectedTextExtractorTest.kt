ad4ea4c83118c13cb28517801f6f717cee10bc39
Toast Kid <super_q1m@yahoo.co.jp>
2020-07-21 07:05:22 +0900

    refactor #559 Remove RunWith Robolectric.

--- app/src/test/java/jp/toastkid/yobidashi/browser/webview/SelectedTextExtractorTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/webview/SelectedTextExtractorTest.kt
@@ -5,13 +5,10 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
 /**
  * @author toastkidjp
  */
-@RunWith(RobolectricTestRunner::class)
 class SelectedTextExtractorTest {
 
     @Test
