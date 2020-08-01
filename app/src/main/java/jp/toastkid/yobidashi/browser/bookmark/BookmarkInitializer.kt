469554320e57cd2638844bc76bf59036bf219440
Toast Kid <super_q1m@yahoo.co.jp>
2020-08-02 07:34:07 +0900

    refactor #559 Use explicitly importing.

--- app/src/main/java/jp/toastkid/yobidashi/browser/bookmark/BookmarkInitializer.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/bookmark/BookmarkInitializer.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.*
+import java.util.Locale
 
 /**
  * Bookmark initializer.
