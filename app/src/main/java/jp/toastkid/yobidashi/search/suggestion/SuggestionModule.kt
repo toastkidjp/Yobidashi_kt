e97c244887885a05419e30d1da34739cf3e8ae54
Toast Kid <super_q1m@yahoo.co.jp>
2020-08-03 06:10:57 +0900

    refactor #559 Remove unused import.

--- app/src/main/java/jp/toastkid/yobidashi/search/suggestion/SuggestionModule.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/suggestion/SuggestionModule.kt
@@ -20,7 +20,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.*
 
 /**
  * Facade of search suggestion module.
