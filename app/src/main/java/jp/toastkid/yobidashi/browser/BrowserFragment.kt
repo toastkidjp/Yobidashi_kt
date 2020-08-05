d30909abfed410d46af17b5b891ff0ac4b380f93
Toast Kid <super_q1m@yahoo.co.jp>
2020-08-06 00:36:17 +0900

    refactor #559 Remove redundant function.

--- app/src/main/java/jp/toastkid/yobidashi/browser/BrowserFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/BrowserFragment.kt
@@ -391,7 +391,7 @@ class BrowserFragment : Fragment(),
 
         switchToolbarVisibility()
 
-        val colorPair = colorPair()
+        val colorPair = preferenceApplier.colorPair()
 
         val fontColor = colorPair.fontColor()
 
@@ -455,8 +455,6 @@ class BrowserFragment : Fragment(),
                     .nextFragment(fragment)
         }
     }
-    
-    private fun colorPair() = preferenceApplier.colorPair()
 
     override fun onClickUserAgent(userAgent: UserAgent) {
         browserModule.resetUserAgent(userAgent.text())
