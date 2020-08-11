bf5871c49efa66656bb608961dd53f75699b7ff6
Toast Kid <super_q1m@yahoo.co.jp>
2020-08-12 04:19:17 +0900

    refactor #606 Add findByName's test case.

--- article/src/test/java/jp/toastkid/article_viewer/article/list/sort/SortTest.kt
+++ article/src/test/java/jp/toastkid/article_viewer/article/list/sort/SortTest.kt
@@ -1,6 +1,7 @@
 package jp.toastkid.article_viewer.article.list.sort
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertSame
 import org.junit.Test
 
 /**
@@ -20,6 +21,14 @@ class SortTest {
         assertEquals(0, Sort.findCurrentIndex("orange"))
         assertEquals(0, Sort.findCurrentIndex("name"))
         assertEquals(1, Sort.findCurrentIndex("NAME"))
+    }
+
+    @Test
+    fun testFindByName() {
+        assertSame(Sort.LAST_MODIFIED, Sort.findByName(null))
+        assertSame(Sort.LAST_MODIFIED, Sort.findByName(""))
+        assertSame(Sort.NAME, Sort.findByName("name"))
+        assertSame(Sort.LENGTH, Sort.findByName("LENGTH"))
     }
 
 }
