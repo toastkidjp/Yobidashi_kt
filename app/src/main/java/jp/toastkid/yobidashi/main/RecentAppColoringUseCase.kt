c45aef0ac20efadc6ac54d393776d9119c86ff4e
Toast Kid <super_q1m@yahoo.co.jp>
2020-08-17 00:20:40 +0900

    refactor #611 Extract applying color to use case. Fix #611.

---
+++ app/src/main/java/jp/toastkid/yobidashi/main/RecentAppColoringUseCase.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.main
+
+import android.app.ActivityManager
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.annotation.ColorInt
+import androidx.core.graphics.ColorUtils
+import jp.toastkid.yobidashi.R
+
+/**
+ * @author toastkidjp
+ */
+class RecentAppColoringUseCase(
+        private val getString: (Int) -> String,
+        private val bitmapDecoder: (Int) -> Bitmap,
+        private val setTaskDescription: (ActivityManager.TaskDescription) -> Unit
+) {
+
+    operator fun invoke(@ColorInt color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val opaqueColor = ColorUtils.setAlphaComponent(color, 255)
+            val taskDescription = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ActivityManager.TaskDescription(
+                        getString(R.string.app_name),
+                        R.mipmap.ic_launcher,
+                        opaqueColor
+                )
+            } else {
+                ActivityManager.TaskDescription(
+                        getString(R.string.app_name),
+                        bitmapDecoder(R.mipmap.ic_launcher),
+                        opaqueColor
+                )
+            }
+            setTaskDescription(taskDescription)
+        }
+    }
+}
