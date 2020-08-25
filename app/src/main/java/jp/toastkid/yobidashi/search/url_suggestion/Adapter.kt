59a60619ff96bb815096414ee6e284050a4f60b4
Toast Kid <super_q1m@yahoo.co.jp>
2020-08-26 07:35:47 +0900

    refactor #605 Remove redundant semicolon.

--- app/src/main/java/jp/toastkid/yobidashi/search/url_suggestion/Adapter.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/url_suggestion/Adapter.kt
@@ -38,7 +38,7 @@ class Adapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(DataBindingUtil.inflate(
                 layoutInflater, R.layout.item_view_history, parent, false
-        ));
+        ))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
