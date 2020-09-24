f5807fa4a8fd9f100c986375d0eab87d2da74bb8
Toast Kid <super_q1m@yahoo.co.jp>
2020-09-25 07:25:58 +0900

    refactor #620 Use function reference.

--- todo/src/main/java/jp/toastkid/todo/view/board/BoardFragment.kt
+++ todo/src/main/java/jp/toastkid/todo/view/board/BoardFragment.kt
@@ -76,7 +76,7 @@ class BoardFragment : Fragment() {
                 view.context,
                 ItemMenuPopupActionUseCase(
                         { taskAdditionDialogFragmentUseCase.invoke(it) },
-                        { removeTask(it) }
+                        ::removeTask
                 )
         )
 
