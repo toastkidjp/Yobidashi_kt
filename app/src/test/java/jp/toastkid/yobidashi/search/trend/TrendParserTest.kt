1f884b6a57a64f4083ac65f6655ae787dcc14d5c
Toast Kid <super_q1m@yahoo.co.jp>
2020-10-22 07:46:29 +0900

    refactor #412 Extract resource to variable.

--- app/src/test/java/jp/toastkid/yobidashi/search/trend/TrendParserTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/search/trend/TrendParserTest.kt
@@ -11,7 +11,8 @@ class TrendParserTest {
 
     @Test
     fun test() {
-        val xmlSource = Okio.buffer(Okio.source(javaClass.classLoader.getResourceAsStream("hot_trend/hourly.xml"))).readUtf8()
+        val resourceAsStream = javaClass.classLoader.getResourceAsStream("hot_trend/hourly.xml")
+        val xmlSource = Okio.buffer(Okio.source(resourceAsStream)).readUtf8()
         val items = TrendParser().invoke(xmlSource)
         assertEquals(20, items.size)
     }
