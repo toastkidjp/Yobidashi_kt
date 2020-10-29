967d256b58e1c7aa3735c46b3d35f0451e79e9e7
Toast Kid <super_q1m@yahoo.co.jp>
2020-10-30 07:13:56 +0900

    feature #412 Enable to show GIF animation.

--- app/src/main/java/jp/toastkid/yobidashi/media/image/preview/ViewHolder.kt
+++ app/src/main/java/jp/toastkid/yobidashi/media/image/preview/ViewHolder.kt
@@ -7,8 +7,12 @@
  */
 package jp.toastkid.yobidashi.media.image.preview
 
+import android.os.Build
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.load
 import java.io.File
 
@@ -18,7 +22,13 @@ import java.io.File
 class ViewHolder(private val view: ImageView) : RecyclerView.ViewHolder(view) {
 
     fun setImage(path: String) {
-        view.load(File(path))
+        view.load(File(path), imageLoader)
     }
+
+    private val imageLoader = ImageLoader.Builder(view.context)
+            .componentRegistry {
+                add(if (Build.VERSION.SDK_INT >= 28) ImageDecoderDecoder() else GifDecoder())
+            }
+            .build()
 
 }
