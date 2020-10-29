0b0303f4f5740f372745871de7acc97c7ef3cf46
Toast Kid <super_q1m@yahoo.co.jp>
2020-10-30 07:15:18 +0900

    refactor #412 Add safe navigation.

--- app/src/test/java/jp/toastkid/yobidashi/search/trend/TrendParserTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/search/trend/TrendParserTest.kt
@@ -11,7 +11,8 @@ class TrendParserTest {
 
     @Test
     fun test() {
-        val resourceAsStream = javaClass.classLoader.getResourceAsStream("hot_trend/hourly.xml")
+        val resourceAsStream =
+                javaClass.classLoader?.getResourceAsStream("hot_trend/hourly.xml")
         val xmlSource = Okio.buffer(Okio.source(resourceAsStream)).readUtf8()
         val items = TrendParser().invoke(xmlSource)
         assertEquals(20, items.size)
