aa3730c5cabc2b55fd925c3a03b1bf8755c438ce
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-17 09:15:07 +0900

    refactor #506 Use Exif extractor.

--- app/src/main/java/jp/toastkid/yobidashi/media/image/preview/detail/ImageDetailFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/media/image/preview/detail/ImageDetailFragment.kt
@@ -32,44 +32,10 @@ class ImageDetailFragment : BottomSheetDialogFragment() {
         val inputStream = FileInputStream(File(imageUri.toString()))
         val exifInterface = ExifInterface(inputStream)
         val lineSeparator = System.lineSeparator()
-        val stringBuilder = StringBuilder()
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_ARTIST)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_BITS_PER_SAMPLE)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_BODY_SERIAL_NUMBER)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_BRIGHTNESS_VALUE)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_CAMERA_OWNER_NAME)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_CFA_PATTERN)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_COLOR_SPACE)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_COMPONENTS_CONFIGURATION)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_COMPRESSION)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_COMPRESSED_BITS_PER_PIXEL)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_CONTRAST)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_COPYRIGHT)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_DATETIME)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_DATETIME_DIGITIZED)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_EXIF_VERSION)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_GPS_SATELLITES)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_SATURATION)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_SHARPNESS)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_SOFTWARE)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_ORIENTATION)
-        extractAttribute(stringBuilder, exifInterface, ExifInterface.TAG_IMAGE_DESCRIPTION)
-        textView.setText(
-                "gpsDateTime: ${exifInterface.gpsDateTime}$lineSeparator" +
-                        "isFlipped: ${exifInterface.isFlipped}$lineSeparator" +
-                        "gpsDateTime: ${exifInterface.gpsDateTime}$lineSeparator" +
-                        "latLong: ${exifInterface.latLong}$lineSeparator" +
-                        "isThumbnailCompressed: ${exifInterface.isThumbnailCompressed}$lineSeparator" +
-                        "rotationDegrees: ${exifInterface.rotationDegrees}$lineSeparator${stringBuilder.toString()}"
-        )
+
+        textView.setText(ExifInformationExtractorUseCase().invoke(exifInterface))
         inputStream.close()
         return textView
-    }
-
-    private fun extractAttribute(stringBuilder: StringBuilder, exifInterface: ExifInterface, tag: String) {
-        exifInterface.getAttribute(tag)?.let {
-            stringBuilder.append("$tag: $it${System.lineSeparator()}")
-        }
     }
 
     companion object {
