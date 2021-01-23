20cd95d7c37b2659f091537f1b87b24598951f86
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-23 09:09:43 +0900

    feature #412 Add search category setting section.

---
+++ app/src/main/java/jp/toastkid/yobidashi/settings/fragment/search/category/Adapter.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.settings.fragment.search.category
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import jp.toastkid.lib.preference.PreferenceApplier
+import jp.toastkid.search.SearchCategory
+import jp.toastkid.yobidashi.R
+
+class Adapter(private val preferenceApplier: PreferenceApplier) : RecyclerView.Adapter<ViewHolder>() {
+
+    private val initialDisables = preferenceApplier.readDisableSearchCategory()
+
+    private val items = SearchCategory.values()
+            .map { SearchCategorySelection(it, initialDisables?.contains(it.name)?.not() ?: true) }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.item_search_category_selection,
+                        parent,
+                        false
+                )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items.get(position)
+        holder.bind(item)
+        holder.setTapListener({
+            item.checked = item.checked.not()
+            if (item.checked) {
+                preferenceApplier.removeDisableSearchCategory(item.searchCategory.name)
+            } else {
+                preferenceApplier.addDisableSearchCategory(item.searchCategory.name)
+            }
+            return@setTapListener item.checked
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+}
---
+++ app/src/main/java/jp/toastkid/yobidashi/settings/fragment/search/category/SearchCategorySelection.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.settings.fragment.search.category
+
+import jp.toastkid.search.SearchCategory
+
+class SearchCategorySelection(
+        val searchCategory: SearchCategory,
+        var checked: Boolean = true
+) {
+}
