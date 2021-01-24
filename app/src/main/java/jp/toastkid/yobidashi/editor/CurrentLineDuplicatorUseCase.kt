51de6c6e4e3f2f8b5a52a4bdc72d8ce78bfea064
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-25 07:35:18 +0900

    refactor #511 Extract to use case.

---
+++ app/src/main/java/jp/toastkid/yobidashi/editor/CurrentLineDuplicatorUseCase.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.editor
+
+import android.widget.EditText
+
+class CurrentLineDuplicatorUseCase {
+
+    operator fun invoke(editText: EditText) {
+        val lineNumber = editText.layout.getLineForOffset(editText.selectionStart)
+        val start = editText.layout.getLineStart(lineNumber)
+        val end = editText.layout.getLineEnd(lineNumber)
+        if (start < 0 || end < 0) {
+            return
+        }
+        editText.text.insert(
+                end,
+                editText.text.substring(start, end)
+        )
+    }
+
+}
