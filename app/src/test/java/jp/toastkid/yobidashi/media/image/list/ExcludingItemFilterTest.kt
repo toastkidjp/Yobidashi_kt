e1e8714abed8e2801c51a54111eb21c0c744e533
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-27 00:08:28 +0900

    refactor #506 Implement ExcludingItemFilter's test cases.

---
+++ app/src/test/java/jp/toastkid/yobidashi/media/image/list/ExcludingItemFilterTest.kt
@@ -0,0 +1,22 @@
+package jp.toastkid.yobidashi.media.image.list
+
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class ExcludingItemFilterTest {
+
+    @Test
+    fun test() {
+        val excludingItemFilter = ExcludingItemFilter(setOf("tomato", "onion"))
+        assertTrue(excludingItemFilter(null))
+        assertTrue(excludingItemFilter(""))
+        assertTrue(excludingItemFilter("  "))
+        assertFalse(excludingItemFilter("tomato"))
+        assertFalse(excludingItemFilter("onion"))
+        assertTrue(excludingItemFilter("toast"))
+    }
+
+}
