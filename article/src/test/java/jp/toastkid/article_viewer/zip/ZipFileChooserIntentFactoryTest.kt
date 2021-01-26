15ceb9811e1ff2c718bdb63c42cd8c555c886bbb
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-27 06:04:38 +0900

    refactor #605 Add unmockkAll.

--- article/src/test/java/jp/toastkid/article_viewer/zip/ZipFileChooserIntentFactoryTest.kt
+++ article/src/test/java/jp/toastkid/article_viewer/zip/ZipFileChooserIntentFactoryTest.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
 import io.mockk.verify
+import org.junit.After
 import org.junit.Test
 
 /**
@@ -22,6 +24,11 @@ class ZipFileChooserIntentFactoryTest {
 
         verify(exactly = 1) { anyConstructed<Intent>().addCategory(Intent.CATEGORY_OPENABLE) }
         verify(exactly = 1) { anyConstructed<Intent>().setType("application/zip") }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
     }
 
 }
