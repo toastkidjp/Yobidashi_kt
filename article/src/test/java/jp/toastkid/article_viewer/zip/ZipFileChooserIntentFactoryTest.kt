df170a85bbb729a646192e47b0de5080eb42b258
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-31 08:17:56 +0900

    refactor #605 Move initializer to setUp().

--- article/src/test/java/jp/toastkid/article_viewer/zip/ZipFileChooserIntentFactoryTest.kt
+++ article/src/test/java/jp/toastkid/article_viewer/zip/ZipFileChooserIntentFactoryTest.kt
@@ -7,6 +7,7 @@ import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
 import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -14,13 +15,20 @@ import org.junit.Test
  */
 class ZipFileChooserIntentFactoryTest {
 
+    private lateinit var zipFileChooserIntentFactory: ZipFileChooserIntentFactory
+
+    @Before
+    fun setUp() {
+        zipFileChooserIntentFactory = ZipFileChooserIntentFactory()
+    }
+
     @Test
     fun test() {
         mockkConstructor(Intent::class)
         every { anyConstructed<Intent>().addCategory(any()) }.answers { mockk() }
         every { anyConstructed<Intent>().setType(any()) }.answers { mockk() }
 
-        ZipFileChooserIntentFactory().invoke()
+        zipFileChooserIntentFactory.invoke()
 
         verify(exactly = 1) { anyConstructed<Intent>().addCategory(Intent.CATEGORY_OPENABLE) }
         verify(exactly = 1) { anyConstructed<Intent>().setType("application/zip") }
