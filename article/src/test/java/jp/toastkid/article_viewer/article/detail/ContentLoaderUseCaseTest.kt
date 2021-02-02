6f7afc950dd1985097f2a01b3e8d3a70e4c399f9
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-03 06:56:40 +0900

    refactor #580 Implement ContentLoaderUseCase's test cases.

---
+++ article/src/test/java/jp/toastkid/article_viewer/article/detail/ContentLoaderUseCaseTest.kt
@@ -0,0 +1,79 @@
+package jp.toastkid.article_viewer.article.detail
+
+import android.widget.TextView
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import io.noties.markwon.Markwon
+import jp.toastkid.article_viewer.article.ArticleRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class ContentLoaderUseCaseTest {
+
+    private lateinit var contentLoaderUseCase: ContentLoaderUseCase
+
+    @MockK
+    private lateinit var repository: ArticleRepository
+
+    @MockK
+    private lateinit var markwon: Markwon
+
+    @MockK
+    private lateinit var contentView: TextView
+
+    @MockK
+    private lateinit var subheads: MutableList<String>
+
+    @MockK
+    private lateinit var linkGeneratorService: LinkGeneratorService
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        contentLoaderUseCase = ContentLoaderUseCase(
+                repository, markwon, contentView, subheads, linkGeneratorService
+        )
+        coEvery { markwon.setMarkdown(any(), any()) }.answers { Unit }
+        coEvery { linkGeneratorService.invoke(any()) }.answers { Unit }
+        Dispatchers.setMain(Dispatchers.Default)
+    }
+
+    @Test
+    fun test() {
+        coEvery { repository.findContentByTitle(any()) }.returns("test")
+
+        contentLoaderUseCase.invoke("test")
+
+        coVerify(exactly = 1) { repository.findContentByTitle(any()) }
+        coVerify(exactly = 1) { markwon.setMarkdown(any(), any()) }
+        coVerify(exactly = 1) { linkGeneratorService.invoke(any()) }
+    }
+
+    @Test
+    fun testContentIsNull() {
+        coEvery { repository.findContentByTitle(any()) }.returns(null)
+
+        contentLoaderUseCase.invoke("test")
+
+        coVerify (exactly = 1) { repository.findContentByTitle(any()) }
+        coVerify (exactly = 0) { markwon.setMarkdown(any(), any()) }
+        coVerify (exactly = 0) { linkGeneratorService.invoke(any()) }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+        Dispatchers.resetMain()
+    }
+
+}
