654f53dc3eba8cd1942cb8c2597d50ccffba9b65
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-03 06:58:02 +0900

    refactor #412 Add test case.

--- search/src/test/java/jp/toastkid/search/SearchQueryExtractorTest.kt
+++ search/src/test/java/jp/toastkid/search/SearchQueryExtractorTest.kt
@@ -63,7 +63,8 @@ class SearchQueryExtractorTest {
             "test" to "https://www.morningstar.com/search?query=test",
             "test" to "https://www.givero.com/search?q=test",
             "test" to "https://info.finance.yahoo.co.jp/search/?query=test",
-            "test" to "https://www.merriam-webster.com/dictionary/test"
+            "test" to "https://www.merriam-webster.com/dictionary/test",
+            "test" to "https://hn.algolia.com/?q=test"
     )
 
     @Before
