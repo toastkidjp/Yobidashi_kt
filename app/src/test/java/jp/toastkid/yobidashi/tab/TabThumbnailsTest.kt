61ac7fec3270ed65d946319c5aaec0604d215529
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-07 08:18:12 +0900

    refactor #523 Implement TabThumbnails' unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/tab/TabThumbnailsTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.tab
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.lib.storage.FilesDir
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.File
+
+class TabThumbnailsTest {
+
+    @InjectMockKs
+    private lateinit var tabThumbnails: TabThumbnails
+
+    @MockK
+    private lateinit var filesDir: FilesDir
+
+    @RelaxedMockK
+    private lateinit var file: File
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { filesDir.assignNewFile(any<String>()) }.returns(file)
+
+        every { file.exists() }.returns(false)
+        every { file.delete() }.returns(false)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun testDeleteWithNullInput() {
+        tabThumbnails.delete(null)
+
+        verify(exactly = 0) { filesDir.assignNewFile(any<String>()) }
+        verify(exactly = 0) { file.exists() }
+        verify(exactly = 0) { file.delete() }
+    }
+
+    @Test
+    fun testDeleteWithBlankInput() {
+        tabThumbnails.delete(" ")
+
+        verify(exactly = 0) { filesDir.assignNewFile(any<String>()) }
+        verify(exactly = 0) { file.exists() }
+        verify(exactly = 0) { file.delete() }
+    }
+
+}
