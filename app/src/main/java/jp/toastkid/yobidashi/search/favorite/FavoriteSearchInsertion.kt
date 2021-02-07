97a2bd12d170843ef97db8ece574a8f0d650fc2c
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-08 07:34:20 +0900

    refactor #412 Use factory function.

--- app/src/main/java/jp/toastkid/yobidashi/search/favorite/FavoriteSearchInsertion.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/favorite/FavoriteSearchInsertion.kt
@@ -27,26 +27,13 @@ class FavoriteSearchInsertion(
             withContext(Dispatchers.IO) {
                 val repository =
                         DatabaseFinder().invoke(context).favoriteSearchRepository()
-                repository.insert(makeFavoriteSearch(category, query))
+                repository.insert(FavoriteSearch.with(category, query))
             }
 
             Toaster.tShort(
                     context,
                     context.getString(R.string.format_message_done_adding_favorite_search, query)
             )
-        }
-    }
-
-    /**
-     * Make object.
-     *
-     * @param c Category string
-     * @param q Query
-     */
-    private fun makeFavoriteSearch(c: String, q: String): FavoriteSearch {
-        return FavoriteSearch().also {
-            it.category = c
-            it.query = q
         }
     }
 
