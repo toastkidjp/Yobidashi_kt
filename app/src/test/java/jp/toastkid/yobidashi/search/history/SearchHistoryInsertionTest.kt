26e9b0965bec5e95b6a832b0c9417cc063bdc9ad
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-11 07:18:14 +0900

    refactor #412 Implement SearchHistoryInsertion's unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/search/history/SearchHistoryInsertionTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.search.history
+
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi.libs.db.AppDatabase
+import jp.toastkid.yobidashi.libs.db.DatabaseFinder
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class SearchHistoryInsertionTest {
+
+    private lateinit var searchHistoryInsertion: SearchHistoryInsertion
+
+    @MockK
+    private lateinit var appDatabase: AppDatabase
+
+    @MockK
+    private lateinit var repository: SearchHistoryRepository
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        mockkConstructor(DatabaseFinder::class)
+        every { anyConstructed<DatabaseFinder>().invoke(any()) }.returns(appDatabase)
+        every { appDatabase.searchHistoryRepository() }.returns(repository)
+        coEvery { repository.insert(any()) }.answers { Unit }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun insert() {
+        searchHistoryInsertion = SearchHistoryInsertion.make(
+                mockk(),
+                "test-category",
+                "test-query"
+        )
+
+        searchHistoryInsertion.insert()
+
+        verify(exactly = 1) { anyConstructed<DatabaseFinder>().invoke(any()) }
+        verify(exactly = 1) { appDatabase.searchHistoryRepository() }
+        coVerify(exactly = 1) { repository.insert(any()) }
+    }
+
+}
