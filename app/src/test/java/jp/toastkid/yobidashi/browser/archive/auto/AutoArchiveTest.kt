e5e2e76bf0b278276eb21535f3a093ccc40bb3d2
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-18 07:02:44 +0900

    refactor #220 Add save() test case.

--- app/src/test/java/jp/toastkid/yobidashi/browser/archive/auto/AutoArchiveTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/archive/auto/AutoArchiveTest.kt
@@ -1,13 +1,66 @@
 package jp.toastkid.yobidashi.browser.archive.auto
 
+import android.webkit.WebView
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.lib.file.ExtensionRemover
+import jp.toastkid.lib.storage.StorageWrapper
+import org.junit.After
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import java.io.File
 
 /**
  * @author toastkidjp
  */
 class AutoArchiveTest {
+
+    @InjectMockKs
+    private lateinit var autoArchive: AutoArchive
+
+    @MockK
+    private lateinit var filesDir: StorageWrapper
+
+    @MockK
+    private lateinit var extensionRemover: ExtensionRemover
+
+    @MockK
+    private lateinit var file: File
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { file.getAbsolutePath() }.returns("test/test.mht")
+        every { filesDir.assignNewFile(any<String>()) }.returns(file)
+        every { filesDir.delete(any()) }.answers { Unit }
+        every { filesDir.listFiles() }.returns(arrayOf(file))
+        every { extensionRemover.invoke(any()) }.returns("test")
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun testSave() {
+        val webView = mockk<WebView>(relaxed = true)
+        every { webView.saveWebArchive(any()) }.answers { Unit }
+
+        autoArchive.save(webView, "test")
+
+        verify(exactly = 1) { webView.saveWebArchive(any()) }
+        verify(exactly = 1) { filesDir.assignNewFile(any<String>()) }
+        verify(exactly = 1) { file.getAbsolutePath() }
+    }
 
     @Test
     fun test() {
