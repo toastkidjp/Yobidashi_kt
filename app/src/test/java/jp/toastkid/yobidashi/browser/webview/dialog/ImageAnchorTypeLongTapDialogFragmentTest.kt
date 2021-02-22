ab57d3b88dbff83e42998be43f357711b59f5ff0
Toast Kid <super_q1m@yahoo.co.jp>
2021-02-23 07:30:43 +0900

    refactor #605 Implement ImageAnchorTypeLongTapDialogFragment's factory test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/webview/dialog/ImageAnchorTypeLongTapDialogFragmentTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser.webview.dialog
+
+import android.os.Bundle
+import io.mockk.every
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ImageAnchorTypeLongTapDialogFragmentTest {
+
+    @Before
+    fun setUp() {
+        mockkConstructor(Bundle::class)
+        every { anyConstructed<Bundle>().putCharSequence(any(), any()) }.answers { Unit }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test() {
+        ImageAnchorTypeLongTapDialogFragment
+                .make("testTitle", "testExtra", "testAnchor")
+
+        verify(atLeast = 3) { anyConstructed<Bundle>().putCharSequence(any(), any()) }
+    }
+
+}
