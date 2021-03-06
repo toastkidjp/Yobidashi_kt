7ab8b3263337b7da827f6f747fe295005ef5aee8
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-07 08:17:30 +0900

    refactor #648 Add false case.

--- app/src/test/java/jp/toastkid/yobidashi/settings/background/ImageLoadingUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/settings/background/ImageLoadingUseCaseTest.kt
@@ -91,4 +91,22 @@ class ImageLoadingUseCaseTest {
         verify(exactly = 1) { imageLoader.enqueue(any()) }
     }
 
+    @Test
+    fun testNotContainsKey() {
+        every { arguments.containsKey(any()) }.returns(false)
+        every { arguments.getParcelable<Bitmap>(any()) }.returns(mockk())
+
+        imageLoadingUseCase.invoke(contentView, arguments)
+
+        verify(atLeast = 1) { arguments.containsKey(any()) }
+        verify(exactly = 0) { arguments.getParcelable<Bitmap>(any()) }
+        verify(exactly = 1) { contentView.findViewById<ImageView>(any()) }
+        verify(exactly = 0) { imageView.getContext() }
+
+        verify(exactly = 0) { anyConstructed<ImageRequest.Builder>().data(any()) }
+        verify(exactly = 0) { imageRequestBuilder.target(any<ImageView>()) }
+        verify(exactly = 0) { imageRequestBuilder.build() }
+        verify(exactly = 0) { imageLoader.enqueue(any()) }
+    }
+
 }
