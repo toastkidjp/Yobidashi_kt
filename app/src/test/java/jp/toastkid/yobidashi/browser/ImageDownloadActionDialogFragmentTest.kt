573d7b52763444827eeed107d85885eb318399e5
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-09 07:18:35 +0900

    refactor #183 Implement ImageDownloadActionDialogFragment's unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/ImageDownloadActionDialogFragmentTest.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ImageDownloadActionDialogFragmentTest {
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        mockkConstructor(Bundle::class)
+        every { anyConstructed<Bundle>().putCharSequence(any(), any()) }.answers { Unit }
+
+        mockkConstructor(ImageDownloadActionDialogFragment::class)
+        every { anyConstructed<ImageDownloadActionDialogFragment>().setArguments(any()) }.answers { Unit }
+        every { anyConstructed<ImageDownloadActionDialogFragment>().show(any<FragmentManager>(), any()) }.answers { Unit }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test() {
+        val context = mockk<Context>()
+
+        ImageDownloadActionDialogFragment.show(context, "test")
+
+        verify(exactly = 0) { anyConstructed<Bundle>().putCharSequence(any(), any()) }
+        verify(exactly = 0) { anyConstructed<ImageDownloadActionDialogFragment>().setArguments(any()) }
+        verify(exactly = 0) { anyConstructed<ImageDownloadActionDialogFragment>().show(any<FragmentManager>(), any()) }
+    }
+
+}
