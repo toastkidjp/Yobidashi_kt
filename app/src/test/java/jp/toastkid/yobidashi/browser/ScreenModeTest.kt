630a843f59b60bfd28097c5288aeac163ed68ffc
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-09 07:19:00 +0900

    refactor #313 Implement ScreenMode's unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/ScreenModeTest.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser
+
+import org.junit.Assert.assertSame
+import org.junit.Test
+
+class ScreenModeTest {
+
+    @Test
+    fun testFindWithEmpty() {
+        assertSame(ScreenMode.EXPANDABLE, ScreenMode.find(""))
+    }
+
+}
