8e6ae664238ffbb970f951be41e74ad3ad12eced
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-13 08:34:04 +0900

    refactor #299 Add testClipboardCorrectCase.

--- app/src/test/java/jp/toastkid/yobidashi/libs/clip/ClippingUrlOpenerTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/libs/clip/ClippingUrlOpenerTest.kt
@@ -8,15 +8,18 @@
 
 package jp.toastkid.yobidashi.libs.clip
 
+import android.content.Context
 import android.view.View
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import io.mockk.mockkConstructor
 import io.mockk.mockkObject
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.lib.Urls
+import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.yobidashi.libs.Toaster
 import jp.toastkid.yobidashi.libs.network.NetworkChecker
 import org.junit.After
@@ -28,10 +31,14 @@ class ClippingUrlOpenerTest {
     @MockK
     private lateinit var view: View
 
+    @MockK
+    private lateinit var context: Context
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        every { view.getContext() }.returns(mockk())
+        every { view.getContext() }.returns(context)
+        every { context.getSharedPreferences(any(), any()) }.returns(mockk())
 
         mockkObject(NetworkChecker)
         every { NetworkChecker.isNotAvailable(any()) }.returns(false)
@@ -94,6 +101,23 @@ class ClippingUrlOpenerTest {
         verify(exactly = 1) { Clipboard.getPrimary(any()) }
         verify(exactly = 1) { Urls.isInvalidUrl(any()) }
         verify(exactly = 0) { Toaster.withAction(any(), any<String>(), any<Int>(), any(), any(), any()) }
+    }
+
+    @Test
+    fun testClipboardCorrectCase() {
+        every { Clipboard.getPrimary(any()) }.returns("https://www.yahoo.co.jp")
+        every { Urls.isInvalidUrl(any()) }.returns(false)
+
+        mockkConstructor(PreferenceApplier::class)
+        every { anyConstructed<PreferenceApplier>().colorPair() }.returns(mockk())
+
+        ClippingUrlOpener.invoke(view, { })
+
+        verify(atLeast = 1) { view.getContext() }
+        verify(exactly = 1) { NetworkChecker.isNotAvailable(any()) }
+        verify(exactly = 1) { Clipboard.getPrimary(any()) }
+        verify(exactly = 1) { Urls.isInvalidUrl(any()) }
+        verify(exactly = 1) { Toaster.withAction(any(), any<String>(), any<Int>(), any(), any(), any()) }
     }
 
 }
