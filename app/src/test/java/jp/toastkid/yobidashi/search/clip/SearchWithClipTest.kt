b842d3b57cb1e98a79087f40e0fe18815029a253
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-16 07:02:44 +0900

    refactor #412 Implement SearchWithClip's unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/search/clip/SearchWithClipTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.search.clip
+
+import android.content.ClipboardManager
+import android.view.View
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.lib.BrowserViewModel
+import jp.toastkid.lib.preference.ColorPair
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class SearchWithClipTest {
+
+    @InjectMockKs
+    private lateinit var searchWithClip: SearchWithClip
+
+    @MockK
+    private lateinit var cm: ClipboardManager
+
+    @MockK
+    private lateinit var parent: View
+
+    @MockK
+    private lateinit var colorPair: ColorPair
+
+    @MockK
+    private lateinit var browserViewModel: BrowserViewModel
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { cm.addPrimaryClipChangedListener(any()) }.answers { Unit }
+        every { cm.removePrimaryClipChangedListener(any()) }.answers { Unit }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        searchWithClip.invoke()
+
+        verify(exactly = 1) { cm.addPrimaryClipChangedListener(any()) }
+    }
+
+    @Test
+    fun dispose() {
+        searchWithClip.dispose()
+
+        verify(exactly = 1) { cm.removePrimaryClipChangedListener(any()) }
+    }
+
+}
