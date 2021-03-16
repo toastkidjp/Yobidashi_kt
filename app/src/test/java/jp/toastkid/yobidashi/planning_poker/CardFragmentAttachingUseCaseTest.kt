bdeae0ce912aecb0968093bb167986d01f3456ac
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-17 06:56:14 +0900

    refactor #605 Implement CardFragmentAttachingUseCase' unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/planning_poker/CardFragmentAttachingUseCaseTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.planning_poker
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class CardFragmentAttachingUseCaseTest {
+
+    @InjectMockKs
+    private lateinit var cardFragmentAttachingUseCase: CardFragmentAttachingUseCase
+
+    @MockK
+    private lateinit var fragmentManager: FragmentManager
+
+    @MockK
+    private lateinit var transaction: FragmentTransaction
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { fragmentManager.beginTransaction() }.returns(transaction)
+        every { transaction.setCustomAnimations(any(), any(), any(), any()) }.returns(transaction)
+        every { transaction.add(any<Int>(), any()) }.returns(transaction)
+        every { transaction.addToBackStack(any()) }.returns(transaction)
+        every { transaction.commit() }.returns(0)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        cardFragmentAttachingUseCase.invoke("1")
+
+        verify(exactly = 1) { fragmentManager.beginTransaction() }
+        verify(exactly = 1) { transaction.setCustomAnimations(any(), any(), any(), any()) }
+        verify(exactly = 1) { transaction.add(any<Int>(), any()) }
+        verify(exactly = 1) { transaction.addToBackStack(any()) }
+        verify(exactly = 1) { transaction.commit() }
+    }
+
+}
