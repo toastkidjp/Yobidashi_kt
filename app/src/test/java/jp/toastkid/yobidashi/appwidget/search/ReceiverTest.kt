99b0734045bd6aa6205091742fa0420ed559c13b
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-24 07:30:53 +0900

    refactor #605 Add Receiver's unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/appwidget/search/ReceiverTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.appwidget.search
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ReceiverTest {
+
+    private lateinit var receiver: Receiver
+
+    @MockK
+    private lateinit var context: Context
+
+    @MockK
+    private lateinit var intent: Intent
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        every { intent.getAction() }.returns(AppWidgetManager.ACTION_APPWIDGET_UPDATE)
+        every { context.getApplicationContext() }.returns(mockk())
+
+        mockkObject(Provider.Companion)
+        every { Provider.updateWidget(any(), any()) }.answers { Unit }
+
+        mockkObject(RemoteViewsFactory)
+        every { RemoteViewsFactory.make(any()) }.returns(mockk())
+
+        receiver = Receiver()
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun onReceive() {
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { intent.getAction() }
+        verify(exactly = 1) { context.getApplicationContext() }
+        verify(exactly = 1) { Provider.updateWidget(any(), any()) }
+        verify(exactly = 1) { RemoteViewsFactory.make(any()) }
+    }
+
+}
