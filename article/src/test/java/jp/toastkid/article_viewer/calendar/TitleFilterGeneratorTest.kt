cc484f7b6f4595523074b70c5b54560f0b394cf0
Toast Kid <super_q1m@yahoo.co.jp>
2021-05-05 07:40:50 +0900

    refactor #580 Reformat code.

--- article/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
+++ article/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
@@ -19,19 +19,19 @@ class TitleFilterGeneratorTest {
     @Test
     fun test() {
         assertEquals(
-            "2019-01-01%",
+                "2019-01-01%",
                 titleFilterFenerator(2019, 1, 1)
         )
         assertEquals(
-            "2019-10-10%",
+                "2019-10-10%",
                 titleFilterFenerator(2019, 10, 10)
         )
         assertEquals(
-            "2019-09-09%",
+                "2019-09-09%",
                 titleFilterFenerator(2019, 9, 9)
         )
         assertEquals(
-            "2019-11-11%",
+                "2019-11-11%",
                 titleFilterFenerator(2019, 11, 11)
         )
     }
