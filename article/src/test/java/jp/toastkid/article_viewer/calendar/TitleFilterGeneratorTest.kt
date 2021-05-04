3ec8f2bed3c92611503b17826b770b3504029e68
Toast Kid <super_q1m@yahoo.co.jp>
2021-05-05 07:33:37 +0900

    refactor #580 Modify TitleFilterGenerator's usages in unit test.

--- article/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
+++ article/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
@@ -12,19 +12,19 @@ class TitleFilterGeneratorTest {
     fun test() {
         assertEquals(
             "2019-01-01%",
-            TitleFilterGenerator(2019, 1, 1)
+            TitleFilterGenerator()(2019, 1, 1)
         )
         assertEquals(
             "2019-10-10%",
-            TitleFilterGenerator(2019, 10, 10)
+            TitleFilterGenerator()(2019, 10, 10)
         )
         assertEquals(
             "2019-09-09%",
-            TitleFilterGenerator(2019, 9, 9)
+            TitleFilterGenerator()(2019, 9, 9)
         )
         assertEquals(
             "2019-11-11%",
-            TitleFilterGenerator(2019, 11, 11)
+            TitleFilterGenerator()(2019, 11, 11)
         )
     }
 }
