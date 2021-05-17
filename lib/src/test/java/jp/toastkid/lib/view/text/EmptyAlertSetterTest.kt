a1cc9a6b44dad3c526642c10f79852c010192007
Toast Kid <super_q1m@yahoo.co.jp>
2021-05-18 07:11:51 +0900

    refactor #605 Implement unit test.

---
+++ lib/src/test/java/jp/toastkid/lib/view/text/EmptyAlertSetterTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.lib.view.text
+
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class EmptyAlertSetterTest {
+
+    private lateinit var emptyAlertSetter: EmptyAlertSetter
+
+    @MockK
+    private lateinit var inputLayout: TextInputLayout
+
+    @MockK
+    private lateinit var editText: EditText
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        every { inputLayout.getEditText() }.returns(editText)
+        every { editText.addTextChangedListener(any()) }.returns(Unit)
+
+        emptyAlertSetter = EmptyAlertSetter()
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        emptyAlertSetter.invoke(inputLayout)
+
+        verify(exactly = 1) { inputLayout.getEditText() }
+        verify(exactly = 1) { editText.addTextChangedListener(any()) }
+    }
+
+}
