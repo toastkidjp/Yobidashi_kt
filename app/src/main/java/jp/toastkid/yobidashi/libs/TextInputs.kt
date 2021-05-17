a34930ee4e3431e75e0b9574ea1c13d84622ccf9
Toast Kid <super_q1m@yahoo.co.jp>
2021-05-18 07:14:49 +0900

    refactor #605 Delete useless function.

--- app/src/main/java/jp/toastkid/yobidashi/libs/TextInputs.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/TextInputs.kt
@@ -1,15 +1,12 @@
 package jp.toastkid.yobidashi.libs
 
 import android.content.Context
-import android.text.Editable
 import android.text.InputType
-import android.text.TextWatcher
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.LinearLayout
 import com.google.android.material.textfield.TextInputLayout
-import jp.toastkid.yobidashi.R
 
 /**
  * [TextInputLayout] utility.
@@ -25,31 +22,6 @@ object TextInputs  {
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
     )
-
-    /**
-     * Set empty alert.
-     *
-     * @param inputLayout [TextInputLayout]
-     */
-    fun setEmptyAlert(inputLayout: TextInputLayout): EditText {
-        val input: EditText? = inputLayout.editText
-
-        input?.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
-
-            override fun afterTextChanged(s: Editable) {
-                if (s.toString().isEmpty()) {
-                    inputLayout.error = inputLayout.context
-                            .getString(R.string.favorite_search_addition_dialog_empty_message)
-                    return
-                }
-                inputLayout.isErrorEnabled = false
-            }
-        })
-        return input ?: EditText(inputLayout.context)
-    }
 
     /**
      * Make [TextInputLayout] instance.
