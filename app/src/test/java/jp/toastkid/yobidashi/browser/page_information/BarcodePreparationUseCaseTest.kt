a7361ca3118fee55dd8359e4a7df631810aa1ea4
Toast Kid <super_q1m@yahoo.co.jp>
2021-05-27 07:43:44 +0900

    refactor #523 Implement BarcodePreparationUseCase' unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/page_information/BarcodePreparationUseCaseTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser.page_information
+
+import android.view.View
+import android.widget.ImageView
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class BarcodePreparationUseCaseTest {
+
+    private lateinit var barcodePreparationUseCase: BarcodePreparationUseCase
+
+    @MockK
+    private lateinit var contentView: View
+
+    @MockK
+    private lateinit var imageView: ImageView
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        coEvery { contentView.findViewById<ImageView>(any()) }.returns(imageView)
+        coEvery { imageView.setImageBitmap(any()) }.returns(Unit)
+        coEvery { imageView.setVisibility(any()) }.returns(Unit)
+        coEvery { imageView.getContext() }.returns(mockk())
+        coEvery { imageView.setOnClickListener(any()) }.returns(mockk())
+
+        mockkConstructor(BarcodeEncoder::class)
+        coEvery { anyConstructed<BarcodeEncoder>().encodeBitmap(any(), any(), any(), any()) }
+            .returns(mockk())
+
+        barcodePreparationUseCase = BarcodePreparationUseCase()
+
+        Dispatchers.setMain(Dispatchers.Unconfined)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun invoke() {
+        barcodePreparationUseCase.invoke(contentView, "https://www.yahoo.co.jp")
+
+        coVerify(atLeast = 1) { contentView.findViewById<ImageView>(any()) }
+        coVerify(exactly = 1) { imageView.setImageBitmap(any()) }
+        coVerify(exactly = 1) { imageView.setVisibility(any()) }
+        coVerify(exactly = 1) { imageView.getContext() }
+        coVerify(exactly = 1) { imageView.setOnClickListener(any()) }
+        coVerify(exactly = 1) {
+            anyConstructed<BarcodeEncoder>().encodeBitmap(any(), any(), any(), any())
+        }
+    }
+
+}
