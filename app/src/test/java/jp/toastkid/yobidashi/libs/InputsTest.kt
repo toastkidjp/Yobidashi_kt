6433f767b73091156d47f95a77dd7e020267d6c6
Toast Kid <super_q1m@yahoo.co.jp>
2021-05-29 07:48:03 +0900

    refactor #385 Implement showKeyboard's unit test.

---
+++ app/src/test/java/jp/toastkid/yobidashi/libs/InputsTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.libs
+
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author toastkidjp
+ */
+class InputsTest {
+
+    @MockK
+    private lateinit var inputMethodManager: InputMethodManager
+
+    @MockK
+    private lateinit var view: EditText
+
+    @MockK
+    private lateinit var context: Activity
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        every { view.getContext() }.returns(context)
+        every { context.getSystemService(any()) }.returns(inputMethodManager)
+        every { inputMethodManager.isActive() }.returns(true)
+        every { inputMethodManager.showSoftInput(any(), any()) }.returns(true)
+        every { inputMethodManager.hideSoftInputFromWindow(any(), any()) }.returns(true)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun showKeyboard() {
+        Inputs.showKeyboard(context, view)
+
+        verify(exactly = 1) { context.getSystemService(any()) }
+        verify(exactly = 1) { inputMethodManager.isActive() }
+        verify(exactly = 1) { inputMethodManager.showSoftInput(any(), any()) }
+    }
+
+    @Test
+    fun hideKeyboard() {
+    }
+
+    @Test
+    fun showKeyboardForInputDialog() {
+    }
+}
