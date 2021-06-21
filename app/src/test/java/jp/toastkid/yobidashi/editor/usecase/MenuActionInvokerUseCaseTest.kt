bf75b6e990c17a59bbfc67904c68f4c1e4c5b845
Toast Kid <super_q1m@yahoo.co.jp>
2021-06-22 07:26:29 +0900

    refactor #511 Add context_edit_insert_as_plain test case.

--- app/src/test/java/jp/toastkid/yobidashi/editor/usecase/MenuActionInvokerUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/editor/usecase/MenuActionInvokerUseCaseTest.kt
@@ -8,19 +8,25 @@
 
 package jp.toastkid.yobidashi.editor.usecase
 
+import android.text.Editable
 import android.widget.EditText
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import io.mockk.mockkObject
 import io.mockk.unmockkAll
+import io.mockk.unmockkObject
 import io.mockk.verify
 import jp.toastkid.lib.BrowserViewModel
+import jp.toastkid.yobidashi.R
 import jp.toastkid.yobidashi.editor.ListHeadAdder
+import jp.toastkid.yobidashi.libs.clip.Clipboard
 import jp.toastkid.yobidashi.libs.speech.SpeechMaker
 import org.junit.After
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -44,11 +50,17 @@ class MenuActionInvokerUseCaseTest {
     @MockK
     private lateinit var listHeadAdder: ListHeadAdder
 
+    @MockK
+    private lateinit var editable: Editable
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
 
         every { editText.context }.returns(mockk())
+        every { editText.getText() }.returns(editable)
+        every { editText.getSelectionStart() }.returns(1)
+        every { editable.insert(any(), any()) }.returns(editable)
     }
 
     @After
@@ -61,5 +73,17 @@ class MenuActionInvokerUseCaseTest {
         assertFalse(menuActionInvokerUseCase.invoke(-1, ""))
 
         verify(exactly = 1) { editText.context }
+    }
+
+    @Test
+    fun test() {
+        mockkObject(Clipboard)
+        every { Clipboard.getPrimary(any()) }.returns("clipped")
+
+        val invoke = menuActionInvokerUseCase.invoke(R.id.context_edit_insert_as_plain, "")
+
+        assertTrue(invoke)
+        verify(exactly = 1) { Clipboard.getPrimary(any()) }
+        unmockkObject(Clipboard)
     }
 }
