333dc9720e932511e9107bddcd780b1cd5ea2b58
Toast Kid <super_q1m@yahoo.co.jp>
2021-07-09 07:17:50 +0900

    refactor #727 Divide to use case.

---
+++ app/src/main/java/jp/toastkid/yobidashi/about/license/LicenseContentLoaderUseCase.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi.about.license
+
+import android.content.res.AssetManager
+import jp.toastkid.licence.LicensesHtmlLoader
+import okio.buffer
+import okio.source
+
+/**
+ * @author toastkidjp
+ */
+class LicenseContentLoaderUseCase(private val assetManager: AssetManager) {
+
+    operator fun invoke(): String? {
+        return LicensesHtmlLoader(assetManager).invoke().source().use { source ->
+            source.buffer().readUtf8()
+        }
+    }
+}
