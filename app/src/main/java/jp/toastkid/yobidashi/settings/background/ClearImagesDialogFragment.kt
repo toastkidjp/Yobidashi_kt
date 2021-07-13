70beed5d0bc6e4c4955e53fcc39219150f8b307f
Toast Kid <super_q1m@yahoo.co.jp>
2021-07-14 07:15:13 +0900

    refactor #734 Use Html instead of HtmlCompat.

--- app/src/main/java/jp/toastkid/yobidashi/settings/background/ClearImagesDialogFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/background/ClearImagesDialogFragment.kt
@@ -2,10 +2,10 @@ package jp.toastkid.yobidashi.settings.background
 
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Html
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import jp.toastkid.yobidashi.R
-import jp.toastkid.yobidashi.libs.HtmlCompat
 
 /**
  * @author toastkidjp
@@ -31,7 +31,12 @@ internal class ClearImagesDialogFragment : DialogFragment() {
 
         return AlertDialog.Builder(activityContext)
                 .setTitle(R.string.clear_all)
-                .setMessage(HtmlCompat.fromHtml(activityContext.getString(R.string.confirm_clear_all_settings)))
+                .setMessage(
+                    Html.fromHtml(
+                        activityContext.getString(R.string.confirm_clear_all_settings),
+                        Html.FROM_HTML_MODE_COMPACT
+                    )
+                )
                 .setCancelable(true)
                 .setPositiveButton(R.string.ok) { d, _ ->
                     onClick?.onClickClearImages()
