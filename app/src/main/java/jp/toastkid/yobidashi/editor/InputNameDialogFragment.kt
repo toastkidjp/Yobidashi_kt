2a1f4f0b9c40a90613f4a61614cb4c3557036dca
Toast Kid <super_q1m@yahoo.co.jp>
2021-08-29 08:00:10 +0900

    refactor #754 Delete useless callback.

--- app/src/main/java/jp/toastkid/yobidashi/editor/InputNameDialogFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/editor/InputNameDialogFragment.kt
@@ -29,13 +29,6 @@ import jp.toastkid.yobidashi.libs.Inputs
  */
 class InputNameDialogFragment : DialogFragment() {
 
-    /**
-     * Callback interface.
-     */
-    interface Callback {
-
-    }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val activityContext = context ?: return super.onCreateDialog(savedInstanceState)
 
