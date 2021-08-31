50b8f7eec34b510967ea0b1680a9ac7bdf64f774
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-01 07:20:30 +0900

    refactor #523 Add test case.

--- app/src/test/java/jp/toastkid/yobidashi/browser/webview/usecase/RedirectionUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/webview/usecase/RedirectionUseCaseTest.kt
@@ -9,10 +9,16 @@
 package jp.toastkid.yobidashi.browser.webview.usecase
 
 import android.net.Uri
+import android.webkit.WebView
 import io.mockk.MockKAnnotations
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockkStatic
 import io.mockk.unmockkAll
+import io.mockk.verify
 import org.junit.After
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
@@ -20,6 +26,12 @@ import org.junit.Before
 import org.junit.Test
 
 class RedirectionUseCaseTest {
+
+    @InjectMockKs
+    private lateinit var redirectionUseCase: RedirectionUseCase
+
+    @MockK
+    private lateinit var webView: WebView
 
     @MockK
     private lateinit var uri: Uri
@@ -45,6 +57,20 @@ class RedirectionUseCaseTest {
     fun testIsNotTarget() {
         every { uri.host }.returns("www.yahoo.co.jp")
         assertFalse(RedirectionUseCase.isTarget(uri))
+    }
+
+    @Test
+    fun test() {
+        mockkStatic(Uri::class)
+        every { uri.getQueryParameter(any()) }.returns("https://www.yahoo.co.jp")
+        every { Uri.decode(any()) }.returns("https://www.yahoo.co.jp")
+        every { webView.loadUrl(any()) }.just(Runs)
+
+        redirectionUseCase.invoke(webView, uri)
+
+        verify(exactly = 1) { uri.getQueryParameter(any()) }
+        verify(exactly = 1) { Uri.decode(any()) }
+        verify(exactly = 1) { webView.loadUrl(any()) }
     }
 
 }
