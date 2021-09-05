b5b0f66a4fa79cf004f30d27c974aed46a939d9d
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-06 07:28:18 +0900

    refactor #431 Implement WindowRectCalculatorCompat.

---
+++ lib/src/main/java/jp/toastkid/lib/window/WindowRectCalculatorCompat.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.lib.window
+
+import android.app.Activity
+import android.graphics.Rect
+import android.os.Build
+
+class WindowRectCalculatorCompat {
+
+    @Suppress("DEPRECATION")
+    operator fun invoke(activity: Activity?): Rect? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            activity?.windowManager?.currentWindowMetrics?.bounds
+        } else {
+            val rect = Rect()
+            activity?.windowManager?.defaultDisplay?.getRectSize(rect)
+            return rect
+        }
+    }
+
+}
