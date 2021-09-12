33388f3270ede165478592b709d51305002e2853
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-13 07:16:15 +0900

    refactor #511 Add test case.

--- app/src/test/java/jp/toastkid/yobidashi/editor/StringSurroundingUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/editor/StringSurroundingUseCaseTest.kt
@@ -29,4 +29,23 @@ class StringSurroundingUseCaseTest {
 
         verify(atLeast = 1) { text.replace(2, 3, any<String>()) }
     }
+
+    @Test
+    fun test() {
+        val useCase = StringSurroundingUseCase()
+
+        val text = mockk<Editable>()
+        every { text.subSequence(any(), any()) }.returns("tomato")
+        every { text.replace(any(), any(), any<String>()) }.returns(mockk())
+
+        val editText = mockk<EditText>()
+        every { editText.text }.returns(text)
+        every { editText.selectionStart }.returns(2)
+        every { editText.selectionEnd }.returns(3)
+
+        useCase.invoke(editText, "~~")
+
+        verify(atLeast = 1) { text.replace(2, 3, any<String>()) }
+    }
+
 }
