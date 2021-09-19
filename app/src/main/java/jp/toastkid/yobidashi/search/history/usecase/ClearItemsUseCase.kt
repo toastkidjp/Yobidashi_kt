4714471c85fc3d0c0e77ccae26fae22ce97d4354
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-19 09:07:50 +0900

    refactor #412 Enable to inject testing disptachers.

--- app/src/main/java/jp/toastkid/yobidashi/search/history/usecase/ClearItemsUseCase.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/history/usecase/ClearItemsUseCase.kt
@@ -10,17 +10,22 @@ package jp.toastkid.yobidashi.search.history.usecase
 
 import androidx.fragment.app.FragmentActivity
 import jp.toastkid.yobidashi.libs.db.DatabaseFinder
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class ClearItemsUseCase(private val adapterClearAll: () -> Unit) {
+class ClearItemsUseCase(
+    private val adapterClearAll: () -> Unit,
+    private val mainDispatcher: CoroutineDispatcher = Dispatchers.Main,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
 
     operator fun invoke(activity: FragmentActivity?) {
         activity ?: return
-        CoroutineScope(Dispatchers.Main).launch {
-            withContext(Dispatchers.IO) {
+        CoroutineScope(mainDispatcher).launch {
+            withContext(ioDispatcher) {
                 DatabaseFinder().invoke(activity).searchHistoryRepository().deleteAll()
             }
 
