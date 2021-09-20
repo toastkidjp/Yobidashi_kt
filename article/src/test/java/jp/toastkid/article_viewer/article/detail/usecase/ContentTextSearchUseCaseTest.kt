79ff33cad5043a537d5b62ba6039f91070c41faf
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-21 07:16:08 +0900

    refactor #580 Add test case.

---
+++ article/src/test/java/jp/toastkid/article_viewer/article/detail/usecase/ContentTextSearchUseCaseTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.article_viewer.article.detail.usecase
+
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.unmockkAll
+import jp.toastkid.lib.view.TextViewHighlighter
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ContentTextSearchUseCaseTest {
+
+    @InjectMockKs
+    private lateinit var contentTextSearchUseCase: ContentTextSearchUseCase
+
+    @MockK
+    private lateinit var textViewHighlighter: TextViewHighlighter
+
+    @MockK
+    private lateinit var inputChannel: Channel<String>
+
+    private val mainDispatcher: CoroutineDispatcher = Dispatchers.Unconfined
+
+    private val backgroundDispatcher: CoroutineDispatcher = Dispatchers.Unconfined
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        coEvery { inputChannel.send(any()) }.just(Runs)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        contentTextSearchUseCase.invoke("test")
+
+        coVerify(exactly = 1) { inputChannel.send(any()) }
+    }
+
+}
