0a8e6c1df442aee1043772d4ad372115a0b5f3ca
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-28 07:02:26 +0900

    bugfix #511 Fix bug of title fetcher which is occurred with specifying charset in meta tag.

--- app/src/main/java/jp/toastkid/yobidashi/editor/usecase/LinkTitleFetcherUseCase.kt
+++ app/src/main/java/jp/toastkid/yobidashi/editor/usecase/LinkTitleFetcherUseCase.kt
@@ -9,35 +9,15 @@
 package jp.toastkid.yobidashi.editor.usecase
 
 import androidx.annotation.WorkerThread
-import jp.toastkid.yobidashi.libs.network.HttpClientFactory
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.util.regex.Pattern
+import org.jsoup.Jsoup
+import java.net.URL
 
-class LinkTitleFetcherUseCase(
-    private val okHttpClient: OkHttpClient = HttpClientFactory.withTimeout(5)
-) {
+class LinkTitleFetcherUseCase {
 
     @WorkerThread
     operator fun invoke(url: String): String {
-        val content = okHttpClient.newCall(Request.Builder().url(url).build())
-            .execute()
-            .body
-            ?.string()
-            ?.split(System.lineSeparator())
-            ?.firstOrNull { it.contains("<title") } ?: return url
-
-        val matcher = PATTERN.matcher(content)
-        return if (matcher.find()) "[${matcher.group(1)}]($url)" else url
-    }
-
-    companion object {
-
-        private val PATTERN = Pattern.compile(
-            "<title.*>(.+?)</title>",
-            Pattern.DOTALL or Pattern.CASE_INSENSITIVE
-        )
-
+        val document = Jsoup.parse(URL(url), 1000)
+        return document.title() ?: url
     }
 
 }
