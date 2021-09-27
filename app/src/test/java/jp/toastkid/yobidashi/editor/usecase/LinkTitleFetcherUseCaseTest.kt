a32a9df00f2eb70378aac9ee811f884397cd6b2f
Toast Kid <super_q1m@yahoo.co.jp>
2021-09-28 07:03:17 +0900

    refactor #511 Implement real test case.

---
+++ app/src/test/java/jp/toastkid/yobidashi/editor/usecase/LinkTitleFetcherUseCaseTest.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.editor.usecase
+
+import org.junit.Assert.assertEquals
+import org.junit.Ignore
+
+class LinkTitleFetcherUseCaseTest {
+
+    @Ignore("Because this function is used network connection.")
+    fun invoke() {
+        assertEquals(
+            "投資信託のモーニングスター｜投資信託・株式・国内/海外ＥＴＦ（上場投資信託）・為替/指数　マーケット情報サイト",
+            LinkTitleFetcherUseCase()
+                .invoke("https://www.morningstar.co.jp")
+        )
+    }
+
+}
