745cef5c567dea2ec0c36003fa43a200aab20837
Toast Kid <super_q1m@yahoo.co.jp>
2021-10-04 07:21:36 +0900

    refactor #605 Divide to other class.

---
+++ app/src/main/java/jp/toastkid/yobidashi/main/initial/FirstLaunchInitializer.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.main.initial
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import jp.toastkid.lib.preference.PreferenceApplier
+import jp.toastkid.search.SearchCategory
+import jp.toastkid.yobidashi.R
+import jp.toastkid.yobidashi.browser.FaviconFolderProviderService
+import jp.toastkid.yobidashi.browser.bookmark.BookmarkInitializer
+import jp.toastkid.yobidashi.settings.background.DefaultBackgroundImagePreparation
+import jp.toastkid.yobidashi.settings.color.DefaultColorInsertion
+
+/**
+ * @param preferenceApplier
+ */
+class FirstLaunchInitializer(
+    private val context: Context,
+    private val preferenceApplier: PreferenceApplier,
+    private val defaultColorInsertion: DefaultColorInsertion = DefaultColorInsertion(),
+    private val faviconFolderProviderService: FaviconFolderProviderService = FaviconFolderProviderService(),
+    private val defaultBackgroundImagePreparation: DefaultBackgroundImagePreparation = DefaultBackgroundImagePreparation()
+) {
+
+    /**
+     * Process for first launch.
+     */
+    operator fun invoke() {
+        if (!preferenceApplier.isFirstLaunch()) {
+            return
+        }
+
+        preferenceApplier.color = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+
+        defaultColorInsertion.insert(context)
+        BookmarkInitializer(faviconFolderProviderService.invoke(context))(context)
+        defaultBackgroundImagePreparation(context) {
+            preferenceApplier.backgroundImagePath = it.absolutePath
+        }
+
+        preferenceApplier.setDefaultSearchEngine(SearchCategory.getDefaultCategoryName())
+    }
+
+}
