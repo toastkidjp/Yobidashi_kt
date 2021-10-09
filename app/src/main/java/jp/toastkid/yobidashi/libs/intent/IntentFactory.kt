5f5722fd350e554843625a92612c009047607fd1
Toast Kid <super_q1m@yahoo.co.jp>
2021-10-10 07:47:53 +0900

    refactor #412 Delete useless 'makeGetContent'.

--- app/src/main/java/jp/toastkid/yobidashi/libs/intent/IntentFactory.kt
+++ app/src/main/java/jp/toastkid/yobidashi/libs/intent/IntentFactory.kt
@@ -1,25 +1,10 @@
 package jp.toastkid.yobidashi.libs.intent
 
-import android.content.Intent
-
 /**
  * Common [android.content.Intent] factory.
  *
  * @author toastkidjp
  */
 object IntentFactory {
-
-    /**
-     * Make Storage Access Framework intent.
-     *
-     * @param type mime type
-     * @return [Intent]
-     */
-    fun makeGetContent(type: String): Intent {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = type
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
-        return intent
-    }
 
 }
