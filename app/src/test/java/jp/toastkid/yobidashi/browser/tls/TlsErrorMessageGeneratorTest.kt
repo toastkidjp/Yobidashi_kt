88e778159403b36e9c88ec24e84a07e69c8d6ef5
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-05 07:16:53 +0900

    refactor #451 Add test case.

--- app/src/test/java/jp/toastkid/yobidashi/browser/tls/TlsErrorMessageGeneratorTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/tls/TlsErrorMessageGeneratorTest.kt
@@ -77,4 +77,16 @@ class TlsErrorMessageGeneratorTest {
         verify(atLeast = 2) { context.getString(any()) }
     }
 
+    @Test
+    fun test() {
+        every { error.primaryError }.returns(SslError.SSL_IDMISMATCH)
+
+        tlsErrorMessageGenerator.invoke(context, error)
+
+        verify(exactly = 1) { error.certificate }
+        verify(exactly = 1) { error.url }
+        verify(atLeast = 2) { context.getString(any()) }
+        verify(exactly = 1) { cert.issuedTo }
+    }
+
 }
