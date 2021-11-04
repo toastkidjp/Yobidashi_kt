d9546d370e095fc7ab95b1921388952bf1a1fa86
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-05 07:14:04 +0900

    refactor #451 Add test case.

--- app/src/test/java/jp/toastkid/yobidashi/browser/tls/TlsErrorMessageGeneratorTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/tls/TlsErrorMessageGeneratorTest.kt
@@ -1,10 +1,24 @@
 package jp.toastkid.yobidashi.browser.tls
 
+import android.content.Context
+import android.net.http.SslCertificate
+import android.net.http.SslError
+import android.text.format.DateFormat
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import java.util.Date
 
 /**
  * @author toastkidjp
@@ -12,7 +26,42 @@ import org.robolectric.RuntimeEnvironment
 @RunWith(RobolectricTestRunner::class)
 class TlsErrorMessageGeneratorTest {
 
-    private val tlsErrorMessageGenerator = TlsErrorMessageGenerator()
+    @InjectMockKs
+    private lateinit var tlsErrorMessageGenerator: TlsErrorMessageGenerator
+
+    @MockK
+    private lateinit var error: SslError
+
+    @MockK
+    private lateinit var cert: SslCertificate
+
+    @MockK
+    private lateinit var dName: SslCertificate.DName
+
+    @MockK
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { error.certificate }.returns(cert)
+        every { error.url }.returns("https://www.bad.com")
+        every { cert.issuedTo }.returns(dName)
+        every { dName.cName }.returns("c_name")
+        every { cert.issuedBy }.returns(dName)
+        every { dName.cName }.returns("d_name")
+        every { cert.validNotAfterDate }.returns(Date())
+        every { context.getString(any()) }.returns("test")
+
+        mockkStatic(DateFormat::class)
+        every { DateFormat.format(any(), any<Date>()) }.returns("2021/11/11")
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
 
     @Test
     fun testNullCase() {
@@ -20,4 +69,16 @@ class TlsErrorMessageGeneratorTest {
             tlsErrorMessageGenerator.invoke(RuntimeEnvironment.systemContext, null).isEmpty()
         )
     }
+
+    @Test
+    fun test() {
+        every { error.primaryError }.returns(SslError.SSL_EXPIRED)
+
+        tlsErrorMessageGenerator.invoke(context, error)
+
+        verify(exactly = 1) { error.certificate }
+        verify(exactly = 1) { error.url }
+        verify(atLeast = 2) { context.getString(any()) }
+    }
+
 }
