05566454376cd9444fba7dadf986219b666112fd
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-08 07:24:07 +0900

    refactor #451 Add test case.

---
+++ app/src/test/java/jp/toastkid/yobidashi/browser/usecase/HtmlSourceExtractionUseCaseTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser.usecase
+
+import android.webkit.WebView
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
+import jp.toastkid.yobidashi.browser.HtmlSourceExtractionUseCase
+import org.junit.Before
+import org.junit.Test
+
+class HtmlSourceExtractionUseCaseTest {
+
+    @MockK
+    private lateinit var webView: WebView
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun test() {
+        every { webView.url }.returns("https://www.yahoo.co.jp")
+        every { webView.evaluateJavascript(any(), any()) }.answers { Unit }
+
+        HtmlSourceExtractionUseCase().invoke(webView, mockk())
+
+        verify (exactly = 1) { webView.evaluateJavascript(any(), any()) }
+    }
+
+    @Test
+    fun testExceptingCase() {
+        every { webView.url }.returns("https://accounts.google.com/signin")
+        every { webView.evaluateJavascript(any(), any()) }.answers { Unit }
+
+        HtmlSourceExtractionUseCase().invoke(webView, mockk())
+
+        verify (exactly = 0) { webView.evaluateJavascript(any(), any()) }
+    }
+
+}
