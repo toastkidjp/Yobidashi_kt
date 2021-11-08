79a85bc7a3c31b075c6c873733462e8a6529374c
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-09 07:12:55 +0900

    refactor #451 Rename test case.

--- app/src/test/java/jp/toastkid/yobidashi/search/usecase/SearchFragmentFactoryUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/search/usecase/SearchFragmentFactoryUseCaseTest.kt
@@ -55,7 +55,7 @@ class SearchFragmentFactoryUseCaseTest {
     }
 
     @Test
-    fun test() {
+    fun testCannotExtractedQueryAndNotUrlCase() {
         every { anyConstructed<SearchQueryExtractor>().invoke(any<String>()) }.returns(null)
         every { Urls.isValidUrl(any()) }.returns(false)
 
