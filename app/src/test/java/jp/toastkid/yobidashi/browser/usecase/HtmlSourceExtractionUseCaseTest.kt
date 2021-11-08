3a039dc97aed8ae655b6d7a3204795addbe43f70
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-09 07:07:43 +0900

    refactor #451 Fix importing.

--- app/src/test/java/jp/toastkid/yobidashi/browser/usecase/HtmlSourceExtractionUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/usecase/HtmlSourceExtractionUseCaseTest.kt
@@ -14,7 +14,6 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.verify
-import jp.toastkid.yobidashi.browser.HtmlSourceExtractionUseCase
 import org.junit.Before
 import org.junit.Test
 
