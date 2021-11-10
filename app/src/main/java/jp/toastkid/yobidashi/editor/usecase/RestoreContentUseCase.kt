bffcea1a8e7285fcac335c182befa6fa0754f1a9
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-11 07:48:09 +0900

    refactor #511 Divide content restoring to use case.

---
+++ app/src/main/java/jp/toastkid/yobidashi/editor/usecase/RestoreContentUseCase.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.editor.usecase
+
+import android.widget.EditText
+import jp.toastkid.lib.ContentViewModel
+import jp.toastkid.yobidashi.editor.ContentHolderService
+
+class RestoreContentUseCase(
+    private val contentHolderService: ContentHolderService,
+    private val contentViewModel: ContentViewModel?,
+    private val editorInput: EditText,
+    private val setContentText: (String) -> Unit
+) {
+
+    operator fun invoke() {
+        if (contentHolderService.isBlank()) {
+            contentViewModel?.snackShort("Backup is empty.")
+            return
+        }
+
+        val selectionStart = editorInput.selectionStart
+        val contentStr = contentHolderService.getContent()
+        setContentText(contentStr)
+        editorInput.setSelection(
+            if (contentStr.length <= selectionStart) contentStr.length else selectionStart
+        )
+    }
+
+}
