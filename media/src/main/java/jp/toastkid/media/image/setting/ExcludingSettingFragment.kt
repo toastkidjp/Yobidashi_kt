257259177566cb33b4b1e7fb8d07731948fce11d
Toast Kid <super_q1m@yahoo.co.jp>
2021-11-28 10:42:22 +0900

    refactor #802 Move media functions to media module. Fix #802.

---
+++ media/src/main/java/jp/toastkid/media/image/setting/ExcludingSettingFragment.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.media.image.setting
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import jp.toastkid.lib.preference.PreferenceApplier
+import jp.toastkid.lib.view.RightSwipeActionAttachment
+import jp.toastkid.media.R
+import jp.toastkid.media.databinding.FragmentSettingImageExcludingBinding
+
+/**
+ * @author toastkidjp
+ */
+class ExcludingSettingFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentSettingImageExcludingBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_setting_image_excluding,
+                container,
+                false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val preferenceApplier = PreferenceApplier(view.context)
+        val excludedItems = preferenceApplier.excludedItems()
+        if (excludedItems.isEmpty()) {
+            dismiss()
+            return
+        }
+
+        binding.excludingItems.layoutManager = LinearLayoutManager(activity)
+
+        val viewModel = ViewModelProvider(this)
+                .get(ExcludingSettingFragmentViewModel::class.java)
+
+        viewModel.dismiss.observe(this, {
+            dismiss()
+        })
+
+        val adapter = Adapter(preferenceApplier, viewModel)
+        binding.excludingItems.adapter = adapter
+        RightSwipeActionAttachment().invoke(binding.excludingItems) {
+            adapter.removeAt(it)
+        }
+        adapter.addAll(excludedItems)
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        parentFragmentManager.setFragmentResult("excluding", Bundle.EMPTY)
+    }
+
+}
---
+++ media/src/main/java/jp/toastkid/media/music/popup/playback/speed/PlaybackSpeedAdapter.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.media.music.popup.playback.speed
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.annotation.LayoutRes
+import jp.toastkid.media.R
+import jp.toastkid.lib.preference.ColorPair
+
+/**
+ * @author toastkidjp
+ */
+class PlaybackSpeedAdapter(
+        private val layoutInflater: LayoutInflater,
+        private val colorPair: ColorPair
+) : BaseAdapter() {
+
+    private val playingSpeeds = PlayingSpeed.values()
+
+    override fun getCount(): Int = playingSpeeds.size
+
+    override fun getItem(position: Int): PlayingSpeed = playingSpeeds[position]
+
+    override fun getItemId(position: Int): Long = playingSpeeds[position].textId.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val playingSpeed = playingSpeeds[position]
+
+        if (convertView == null) {
+            val view = layoutInflater.inflate(
+                    ITEM_LAYOUT_ID,
+                    parent,
+                    false
+            )
+
+            val viewHolder = ViewHolder(view.findViewById(R.id.text))
+            view.tag = viewHolder
+            viewHolder.bind(playingSpeed, colorPair)
+            return view
+        }
+
+        val viewHolder = convertView.tag as? ViewHolder?
+        viewHolder?.bind(playingSpeed, colorPair)
+        return convertView
+    }
+
+    companion object {
+
+        @LayoutRes
+        private val ITEM_LAYOUT_ID = R.layout.item_playback_speed_spinner
+
+    }
+}
