920f9589e1f41f4106f59c22e0bbe5001edd24f1
Toast Kid <super_q1m@yahoo.co.jp>
2021-12-11 09:01:17 +0900

    refactor #802 Modify packages.

--- lib/src/main/java/jp/toastkid/lib/viewmodel/PageSearcherViewModel.kt
+++ lib/src/main/java/jp/toastkid/lib/viewmodel/PageSearcherViewModel.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2019 toastkidjp.
+ * Copyright (c) 2021 toastkidjp.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompany this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
  */
-package jp.toastkid.yobidashi.browser.page_search
+package jp.toastkid.lib.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
