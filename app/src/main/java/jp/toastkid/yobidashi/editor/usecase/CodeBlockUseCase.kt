7860c4be02c373b18106dd3e970d71cbc3e832db
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-03 07:54:25 +0900

    refactor #511 Divide to use case.

---
+++ app/src/main/java/jp/toastkid/yobidashi/editor/usecase/CodeBlockUseCase.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.editor.usecase
+
+import android.widget.EditText
+
+class CodeBlockUseCase {
+
+    operator fun invoke(editText: EditText, text: String) {
+        val selectionStart = editText.selectionStart
+        val selectionEnd = editText.selectionEnd
+        val lineSeparator = System.lineSeparator()
+
+        editText.text.replace(
+            selectionStart,
+            selectionEnd,
+            "```$lineSeparator$text$lineSeparator```"
+        )
+    }
+}
