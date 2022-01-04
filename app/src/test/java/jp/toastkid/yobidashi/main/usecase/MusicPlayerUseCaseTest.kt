83f84d219a91df62cc2c0c5da4bd742589fcba26
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-05 07:17:24 +0900

    refactor #496 Add test case.

---
+++ app/src/test/java/jp/toastkid/yobidashi/main/usecase/MusicPlayerUseCaseTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.main.usecase
+
+import androidx.activity.result.ActivityResultLauncher
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.media.music.popup.MediaPlayerPopup
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MusicPlayerUseCaseTest {
+
+    @InjectMockKs
+    private lateinit var musicPlayerUseCase: MusicPlayerUseCase
+
+    @MockK
+    private lateinit var mediaPermissionRequestLauncher: ActivityResultLauncher<((Boolean) -> Unit)?>
+
+    @MockK
+    private lateinit var mediaPlayerPopup: MediaPlayerPopup
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { mediaPermissionRequestLauncher.launch(any()) }.just(Runs)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        musicPlayerUseCase.invoke(null)
+
+        verify(inverse = true) { mediaPermissionRequestLauncher.launch(any()) }
+    }
+}
