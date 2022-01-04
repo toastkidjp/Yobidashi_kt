7a5f7a6a2373bb40fc63aea51f1b404f4b8b4651
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-05 07:18:26 +0900

    refactor #496 Rename test case.

--- app/src/test/java/jp/toastkid/yobidashi/main/usecase/MusicPlayerUseCaseTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/main/usecase/MusicPlayerUseCaseTest.kt
@@ -15,7 +15,6 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
-import io.mockk.mockk
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.media.music.popup.MediaPlayerPopup
@@ -47,7 +46,7 @@ class MusicPlayerUseCaseTest {
     }
 
     @Test
-    fun invoke() {
+    fun testNoOpByPassingNull() {
         musicPlayerUseCase.invoke(null)
 
         verify(inverse = true) { mediaPermissionRequestLauncher.launch(any()) }
