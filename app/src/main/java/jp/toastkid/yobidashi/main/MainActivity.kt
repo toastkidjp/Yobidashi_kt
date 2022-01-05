2e215c2883977aa59f58503df5d0ea12a7377d8b
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-06 07:20:41 +0900

    refactor #496 Remove useless local variable.

--- app/src/main/java/jp/toastkid/yobidashi/main/MainActivity.kt
+++ app/src/main/java/jp/toastkid/yobidashi/main/MainActivity.kt
@@ -377,8 +377,7 @@ class MainActivity : AppCompatActivity(), TabListDialogFragment.Callback {
         MenuBinder(this, menuViewModel, binding.menuStub, binding.menuSwitch)
 
         musicPlayerUseCase = MusicPlayerUseCase(mediaPermissionRequestLauncher)
-        val menuUseCase = MenuUseCase({ this }, menuViewModel, contentViewModel, musicPlayerUseCase)
-        menuUseCase.observe()
+        MenuUseCase({ this }, menuViewModel, contentViewModel, musicPlayerUseCase).observe()
     }
 
     private fun initializeContentViewModel() {
