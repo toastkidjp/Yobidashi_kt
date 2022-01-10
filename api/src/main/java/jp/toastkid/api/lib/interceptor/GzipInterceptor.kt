4a2ebff179fb153913c69bbf6749731493dbc278
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-11 07:18:10 +0900

    refactor #433 Remove unsafe unwrapping.

--- api/src/main/java/jp/toastkid/api/lib/interceptor/GzipInterceptor.kt
+++ api/src/main/java/jp/toastkid/api/lib/interceptor/GzipInterceptor.kt
@@ -33,7 +33,7 @@ class GzipInterceptor : Interceptor {
             .header(KEY_HEADER, "gzip")
             .method(
                 originalRequest.method,
-                withContentLength(gzip(originalRequest.body!!))
+                withContentLength(gzip(originalRequest.body))
             )
             .build()
         return chain.proceed(compressedRequest)
@@ -60,11 +60,11 @@ class GzipInterceptor : Interceptor {
         }
     }
 
-    private fun gzip(body: RequestBody): RequestBody {
+    private fun gzip(body: RequestBody?): RequestBody {
         return object : RequestBody() {
 
             override fun contentType(): MediaType? {
-                return body.contentType()
+                return body?.contentType()
             }
 
             override fun contentLength(): Long {
@@ -74,7 +74,7 @@ class GzipInterceptor : Interceptor {
             @Throws(IOException::class)
             override fun writeTo(sink: BufferedSink) {
                 val gzipSink = GzipSink(sink).buffer()
-                body.writeTo(gzipSink)
+                body?.writeTo(gzipSink)
                 gzipSink.close()
             }
         }
