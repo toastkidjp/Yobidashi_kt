2c06a7461be7cc6002760dba81a9aeaa48a4b00d
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-24 07:29:26 +0900

    refactor #827 Remove parameter 'position'.

--- app/src/main/java/jp/toastkid/yobidashi/browser/history/Adapter.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/history/Adapter.kt
@@ -109,10 +109,10 @@ internal class Adapter(
      * @param position
      */
     fun removeAt(position: Int) {
-        remove(getItem(position), position)
+        remove(getItem(position))
     }
 
-    private fun remove(item: ViewHistory, position: Int = -1) {
+    private fun remove(item: ViewHistory) {
         CoroutineScope(Dispatchers.Main).launch {
             val copy = ArrayList<ViewHistory>(currentList)
             withContext(Dispatchers.IO) {
