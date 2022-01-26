aed5d0e3527b2d6d3c2301455ba4e461636ad5e2
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-27 07:43:48 +0900

    refactor #605 Use explicit importing.

--- app/src/test/java/jp/toastkid/yobidashi/browser/archive/IdGeneratorTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/browser/archive/IdGeneratorTest.kt
@@ -1,6 +1,8 @@
 package jp.toastkid.yobidashi.browser.archive
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
