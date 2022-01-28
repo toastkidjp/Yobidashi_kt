0e1153fe10f4693fe3445269fc70a1b3b2ee0f17
Toast Kid <super_q1m@yahoo.co.jp>
2022-01-29 08:14:03 +0900

    feature #412 Use coroutines.

--- app/src/main/java/jp/toastkid/yobidashi/search/url_suggestion/RtsSuggestionUseCase.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/url_suggestion/RtsSuggestionUseCase.kt
@@ -12,25 +12,32 @@ import androidx.core.net.toUri
 import jp.toastkid.lib.Urls
 import jp.toastkid.yobidashi.browser.UrlItem
 import jp.toastkid.yobidashi.browser.history.ViewHistory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class RtsSuggestionUseCase(
     private val itemCallback: (UrlItem) -> Unit
 ) {
 
-    operator fun invoke(input: String?) {
-        if (Urls.isValidUrl(input)) {
-            val uri = input?.toUri() ?: return
-            if (uri.host?.endsWith("twitter.com") == true) {
-                val candidate = uri.pathSegments[0]
-                if (candidate.isNotBlank()) {
-                    itemCallback(
-                        ViewHistory().also {
-                            it.title = "$candidate をリアルタイム検索で見る"
-                            it.url = "https://search.yahoo.co.jp/realtime/search?p=id:$candidate"
-                        }
-                    )
+    suspend operator fun invoke(input: String?) {
+        val candidate = withContext(Dispatchers.Default) {
+            if (Urls.isValidUrl(input)) {
+                val uri = input?.toUri() ?: return@withContext null
+                if (uri.host?.endsWith("twitter.com") == true) {
+                    return@withContext uri.pathSegments[0]
                 }
             }
+
+            return@withContext null
+        }
+
+        if (candidate?.isNotBlank() == true) {
+            itemCallback(
+                ViewHistory().also {
+                    it.title = "$candidate をリアルタイム検索で見る"
+                    it.url = "https://search.yahoo.co.jp/realtime/search?p=id:$candidate"
+                }
+            )
         }
     }
 }
