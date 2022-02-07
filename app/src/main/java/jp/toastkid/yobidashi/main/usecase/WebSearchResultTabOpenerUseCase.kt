8375db40c2785b31e4fb6dcb402f1535b278475c
Toast Kid <super_q1m@yahoo.co.jp>
2022-02-08 06:57:27 +0900

    refactor #412 Extract to use case.

---
+++ app/src/main/java/jp/toastkid/yobidashi/main/usecase/WebSearchResultTabOpenerUseCase.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.main.usecase
+
+import android.net.Uri
+import jp.toastkid.lib.Urls
+import jp.toastkid.lib.preference.PreferenceApplier
+import jp.toastkid.search.UrlFactory
+
+class WebSearchResultTabOpenerUseCase(
+    private val preferenceApplier: PreferenceApplier,
+    private val openNewWebTab: (Uri) -> Unit
+) {
+
+    operator fun invoke(query: String) {
+        val validatedUrl = Urls.isValidUrl(query)
+        if (validatedUrl) {
+            openNewWebTab(Uri.parse(query))
+            return
+        }
+
+        val category = preferenceApplier.getDefaultSearchEngine() ?: return
+        val searchUri = UrlFactory()(category, query)
+        openNewWebTab(searchUri)
+    }
+
+}
