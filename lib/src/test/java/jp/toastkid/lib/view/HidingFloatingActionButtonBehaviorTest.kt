c3c1ebdd3416f2d75644a069084f80ff8c73541e
Toast Kid <super_q1m@yahoo.co.jp>
2022-02-16 07:17:54 +0900

    refactor #523 Add test case.

--- lib/src/test/java/jp/toastkid/lib/view/HidingFloatingActionButtonBehaviorTest.kt
+++ lib/src/test/java/jp/toastkid/lib/view/HidingFloatingActionButtonBehaviorTest.kt
@@ -120,6 +120,20 @@ class HidingFloatingActionButtonBehaviorTest {
     }
 
     @Test
+    fun onInterceptTouchEvent() {
+        val motionEvent = mockk<MotionEvent>()
+        every { motionEvent.action }.returns(MotionEvent.ACTION_MOVE)
+        every { motionEvent.rawY }.returns(21f)
+        every { child.visibility }.returns(View.GONE)
+        mockkConstructor(FloatingActionButton.Behavior::class)
+        every { anyConstructed<FloatingActionButton.Behavior>().onInterceptTouchEvent(any(), any(), any()) }.returns(true)
+
+        hidingFloatingActionButtonBehavior.onInterceptTouchEvent(mockk(), child, motionEvent)
+
+        verify { child.show() }
+    }
+
+    @Test
     fun testOnStartNestedScroll() {
         assertTrue(
             hidingFloatingActionButtonBehavior.onStartNestedScroll(
