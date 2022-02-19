d44c8ae528b2ac32b4be7b81784abe53af9eee31
Toast Kid <super_q1m@yahoo.co.jp>
2022-02-20 08:53:00 +0900

    refactor #506 Add test case.

---
+++ image/src/test/java/jp/toastkid/image/list/ImageFilterUseCaseTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.image.list
+
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.lib.preference.PreferenceApplier
+
+class ImageFilterUseCaseTest {
+
+    @InjectMockKs
+    private lateinit var imageFilterUseCase: ImageFilterUseCase
+
+    @MockK
+    private lateinit var preferenceApplier: PreferenceApplier
+
+    @MockK
+    private lateinit var adapter: Adapter
+
+    @MockK
+    private lateinit var imageLoaderUseCase: ImageLoaderUseCase
+
+    @MockK
+    private lateinit var imageLoader: ImageLoader
+
+    @MockK
+    private lateinit var refreshContent: () -> Unit
+
+    @org.junit.Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { imageLoaderUseCase.invoke() }.just(Runs)
+        every { imageLoaderUseCase.clearCurrentBucket() }.just(Runs)
+        every { preferenceApplier.excludedItems() }.returns(emptySet())
+        every { imageLoader.filterBy(any()) }.returns(emptyList())
+        every { adapter.submitList(any()) }.just(Runs)
+    }
+
+    @org.junit.After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @org.junit.Test
+    fun invoke() {
+        imageFilterUseCase.invoke(null)
+
+        verify { imageLoaderUseCase.invoke() }
+        verify(inverse = true) { imageLoaderUseCase.clearCurrentBucket() }
+    }
+}
