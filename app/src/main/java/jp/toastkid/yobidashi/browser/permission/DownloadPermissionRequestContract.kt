6f50fd50cf512169941c0ebae2b5b65fe8ae432d
Toast Kid <super_q1m@yahoo.co.jp>
2022-02-21 07:23:29 +0900

    bugfix #523 Enable to use file download in Web tab.

--- app/src/main/java/jp/toastkid/yobidashi/browser/permission/DownloadPermissionRequestContract.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/permission/DownloadPermissionRequestContract.kt
@@ -12,6 +12,7 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -31,6 +32,9 @@ class DownloadPermissionRequestContract : ActivityResultContract<String?, Pair<B
     }
 
     override fun parseResult(resultCode: Int, intent: Intent?): Pair<Boolean, String?> {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return true to url
+        }
         if (resultCode != AppCompatActivity.RESULT_OK) return false to url
         val granted = intent
             ?.getIntArrayExtra(ActivityResultContracts.RequestMultiplePermissions.EXTRA_PERMISSION_GRANT_RESULTS)
