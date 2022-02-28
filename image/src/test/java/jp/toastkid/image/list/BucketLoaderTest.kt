12bcab9463f7018d0b233f1453b86a78f4942e78
Toast Kid <super_q1m@yahoo.co.jp>
2022-03-01 07:11:05 +0900

    refactor #506 Implement BucketLoader's unit test.

---
+++ image/src/test/java/jp/toastkid/image/list/BucketLoaderTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.image.list
+
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+
+class BucketLoaderTest {
+
+    @InjectMockKs
+    private lateinit var bucketLoader: BucketLoader
+
+    @MockK
+    private lateinit var contentResolver: ContentResolver
+
+    @MockK
+    private lateinit var externalContentUri: Uri
+
+    @MockK
+    private lateinit var cursor: Cursor
+
+    @org.junit.Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { contentResolver.query(any(), any(), any(), any(), any()) }.returns(cursor)
+        every { cursor.getColumnIndex(any()) }.returns(0)
+        every { cursor.getString(any()) }.returns("test")
+        every { cursor.moveToNext() }.returns(false)
+        every { cursor.close() }.just(Runs)
+
+        mockkConstructor(ParentExtractor::class)
+        every { anyConstructed<ParentExtractor>().invoke(any()) }.returns("path")
+    }
+
+    @org.junit.After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @org.junit.Test
+    fun invoke() {
+        every { contentResolver.query(any(), any(), any(), any(), any()) }.returns(null)
+
+        bucketLoader.invoke(Sort.default())
+
+        verify(inverse = true) { cursor.getColumnIndex(any()) }
+        verify(inverse = true) { cursor.close() }
+    }
+}
