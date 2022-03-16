6079f441cb2d0f5a06a6aedafa58f855a9b3abed
Toast Kid <super_q1m@yahoo.co.jp>
2022-03-17 07:15:09 +0900

    feature #853 Rewrite task-board UI with Compose.

--- todo/src/main/java/jp/toastkid/todo/view/board/BoardFragment.kt
+++ todo/src/main/java/jp/toastkid/todo/view/board/BoardFragment.kt
@@ -8,54 +8,254 @@
 package jp.toastkid.todo.view.board
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.children
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Checkbox
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import jp.toastkid.lib.AppBarViewModel
-import jp.toastkid.lib.ContentViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import coil.compose.AsyncImage
 import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.todo.R
-import jp.toastkid.todo.databinding.AppBarBoardBinding
-import jp.toastkid.todo.databinding.FragmentTaskBoardBinding
+import jp.toastkid.todo.data.TodoTaskDataAccessor
+import jp.toastkid.todo.data.TodoTaskDatabase
 import jp.toastkid.todo.model.TodoTask
 import jp.toastkid.todo.view.addition.TaskAdditionDialogFragmentUseCase
 import jp.toastkid.todo.view.addition.TaskAdditionDialogFragmentViewModel
-import jp.toastkid.todo.view.item.menu.ItemMenuPopup
 import jp.toastkid.todo.view.item.menu.ItemMenuPopupActionUseCase
-import jp.toastkid.todo.view.list.TaskListFragmentViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.math.roundToInt
 
 /**
  * @author toastkidjp
  */
 class BoardFragment : Fragment() {
 
-    private lateinit var binding: FragmentTaskBoardBinding
-
-    private lateinit var appBarBinding: AppBarBoardBinding
-
     private var taskAdditionDialogFragmentUseCase: TaskAdditionDialogFragmentUseCase? = null
 
-    private var taskAddingUseCase: TaskAddingUseCase? = null
-
     private var taskClearUseCase: TaskClearUseCase? = null
-
-    private val tasks = mutableListOf<TodoTask>()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_task_board, container, false)
-        appBarBinding = DataBindingUtil.inflate(inflater, R.layout.app_bar_board, container, false)
-        appBarBinding.fragment = this
-        return binding.root
+        val context = context ?: return null
+        val composeView = ComposeView(context)
+        composeView.setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+        )
+
+        val repository = TodoTaskDatabase.find(context).repository()
+        CoroutineScope(Dispatchers.Main).launch {
+            val flow = withContext(Dispatchers.IO) {
+                Pager(
+                    PagingConfig(pageSize = 10, enablePlaceholders = true),
+                    pagingSourceFactory = { repository.allTasks() }
+                )
+                    .flow
+                    .cachedIn(lifecycleScope)
+            }
+            composeView.setContent { TaskBoardUi(flow) }
+        }
+
+        return composeView
     }
+
+
+    @Composable
+    fun TaskBoardUi(flow: Flow<PagingData<TodoTask>>) {
+        val context = view?.context ?: return
+        val repository = TodoTaskDatabase.find(context).repository()
+        val color = PreferenceApplier(context).color
+
+        val taskAdditionDialogFragmentUseCase = TaskAdditionDialogFragmentUseCase(
+            this,
+            ViewModelProvider(this).get(TaskAdditionDialogFragmentViewModel::class.java)
+        ) {
+            CoroutineScope(Dispatchers.Main).launch {
+                withContext(Dispatchers.IO) {
+                    repository.insert(it)
+                }
+            }
+        }
+        val menuUseCase = ItemMenuPopupActionUseCase(
+            { taskAdditionDialogFragmentUseCase.invoke(it) },
+            {
+                CoroutineScope(Dispatchers.Main).launch {
+                    withContext(Dispatchers.IO) {
+                        repository.delete(it)
+                    }
+                }
+            }
+        )
+
+        val tasks = flow.collectAsLazyPagingItems()
+
+        MaterialTheme {
+            LazyColumn {
+                items(tasks) { task ->
+                    task ?: return@items
+                    BoardItem(
+                        task,
+                        repository,
+                        color,
+                        menuUseCase
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun BoardItem(
+        task: TodoTask,
+        repository: TodoTaskDataAccessor,
+        color: Int,
+        menuUseCase: ItemMenuPopupActionUseCase
+    ) {
+        var expanded by remember { mutableStateOf(false) }
+        val items = listOf(
+            stringResource(id = R.string.modify),
+            stringResource(id = R.string.delete)
+        )
+
+        var offsetX by remember { mutableStateOf(task.x) }
+        var offsetY by remember { mutableStateOf(task.y) }
+
+        Surface(
+            elevation = 4.dp,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 2.dp, bottom = 2.dp)
+                .width(140.dp)
+                .height(140.dp)
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consumeAllChanges()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                }
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                Checkbox(
+                    checked = task.done,
+                    onCheckedChange = {
+                        task.done = task.done.not()
+                        CoroutineScope(Dispatchers.IO).launch {
+                            repository.insert(task)
+                        }
+                    },
+                    modifier = Modifier
+                        .width(32.dp)
+                        .fillMaxHeight()
+                )
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = task.description,
+                        fontSize = 16.sp,
+                        maxLines = 1,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = DateFormat.format("yyyy/MM/dd(E)", task.dueDate).toString(),
+                        fontSize = 14.sp,
+                        maxLines = 1,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                AsyncImage(
+                    R.drawable.ic_more,
+                    contentDescription = stringResource(R.string.menu),
+                    colorFilter = ColorFilter.tint(
+                        Color(color),
+                        BlendMode.SrcIn
+                    ),
+                    modifier = Modifier
+                        .width(32.dp)
+                        .fillMaxHeight()
+                        .clickable {
+                            expanded = true
+                        }
+                        .background(color = Color(task.color))
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(colorResource(id = R.color.soft_background))
+                ) {
+                    items.forEachIndexed { index, s ->
+                        DropdownMenuItem(onClick = {
+                            when (index) {
+                                0 -> menuUseCase.modify(task)
+                                1 -> menuUseCase.delete(task)
+                            }
+                            expanded = false
+                        }) { Text(text = s) }
+                    }
+                }
+            }
+        }
+    }
+/*
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -118,16 +318,7 @@ class BoardFragment : Fragment() {
         }
 
         taskAddingUseCase?.invoke(makeSampleTask())
-    }
-
-    private fun makeSampleTask() = SampleTaskMaker().invoke()
-
-    private fun removeTask(task: TodoTask) {
-        tasks.remove(task)
-        binding.board.children
-                .firstOrNull { it.tag == task.id }
-                ?.also { binding.board.removeView(it) }
-    }
+    }*/
 
     fun addTask() {
         taskAdditionDialogFragmentUseCase?.invoke()
