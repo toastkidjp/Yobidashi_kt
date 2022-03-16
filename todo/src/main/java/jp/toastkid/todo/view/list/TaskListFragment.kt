ab624d67084c6965baa169cf4d58370e0b806ee3
Toast Kid <super_q1m@yahoo.co.jp>
2022-03-17 07:14:48 +0900

    feature #853 Enable to use item menu.

--- todo/src/main/java/jp/toastkid/todo/view/list/TaskListFragment.kt
+++ todo/src/main/java/jp/toastkid/todo/view/list/TaskListFragment.kt
@@ -23,10 +23,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Checkbox
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode
@@ -34,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,11 +56,11 @@ import androidx.paging.compose.items
 import coil.compose.AsyncImage
 import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.todo.R
+import jp.toastkid.todo.data.TodoTaskDataAccessor
 import jp.toastkid.todo.data.TodoTaskDatabase
 import jp.toastkid.todo.model.TodoTask
 import jp.toastkid.todo.view.addition.TaskAdditionDialogFragmentUseCase
 import jp.toastkid.todo.view.addition.TaskAdditionDialogFragmentViewModel
-import jp.toastkid.todo.view.item.menu.ItemMenuPopup
 import jp.toastkid.todo.view.item.menu.ItemMenuPopupActionUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -99,103 +106,130 @@ class TaskListFragment : Fragment() {
         val repository = TodoTaskDatabase.find(context).repository()
         val color = PreferenceApplier(context).color
 
+        val taskAdditionDialogFragmentUseCase = TaskAdditionDialogFragmentUseCase(
+            this,
+            ViewModelProvider(this).get(TaskAdditionDialogFragmentViewModel::class.java)
+        ) {
+            CoroutineScope(Dispatchers.Main).launch {
+                withContext(Dispatchers.IO) {
+                    repository.insert(it)
+                }
+            }
+        }
+        val menuUseCase = ItemMenuPopupActionUseCase(
+            { taskAdditionDialogFragmentUseCase.invoke(it) },
+            {
+                CoroutineScope(Dispatchers.Main).launch {
+                    withContext(Dispatchers.IO) {
+                        repository.delete(it)
+                    }
+                }
+            }
+        )
+
         val tasks = flow.collectAsLazyPagingItems()
-        var popup: ItemMenuPopup? = null
+
         MaterialTheme {
             LazyColumn {
                 items(tasks) { task ->
                     task ?: return@items
-                    Surface(
-                        elevation = 4.dp,
-                        modifier = Modifier
-                            .padding(start = 16.dp, end = 16.dp, top = 2.dp, bottom = 2.dp)
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                        ) {
-                            Checkbox(
-                                checked = task.done,
-                                onCheckedChange = {
-                                    task.done = task.done.not()
-                                    CoroutineScope(Dispatchers.IO).launch {
-                                        repository.insert(task)
-                                    }
-                                },
-                                modifier = Modifier
-                                    .width(32.dp)
-                                    .fillMaxHeight()
-                            )
-                            Column(
-                                modifier = Modifier.weight(1f)
-                            ) {
-                                Text(
-                                    text = task.description,
-                                    fontSize = 16.sp,
-                                    maxLines = 1,
-                                    modifier = Modifier.fillMaxWidth()
-                                )
-                                Text(
-                                    text = DateFormat.format("yyyy/MM/dd(E)", task.dueDate).toString(),
-                                    fontSize = 14.sp,
-                                    maxLines = 1,
-                                    modifier = Modifier.fillMaxWidth()
-                                )
-                            }
-                            Box(
-                                modifier = Modifier
-                                    .width(20.dp)
-                                    .fillMaxHeight()
-                                    .background(color = Color(task.color))
-                            )
-                            AsyncImage(
-                                R.drawable.ic_more,
-                                contentDescription = stringResource(R.string.menu),
-                                colorFilter = ColorFilter.tint(
-                                    Color(color),
-                                    BlendMode.SrcIn
-                                ),
-                                modifier = Modifier
-                                    .width(32.dp)
-                                    .fillMaxHeight()
-                                    .clickable {
-                                        //popup?.show(it.first, it.second)
-                                    }
-                            )
+                    TaskListItem(task, repository, color, menuUseCase)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun TaskListItem(
+        task: TodoTask,
+        repository: TodoTaskDataAccessor,
+        color: Int,
+        menuUseCase: ItemMenuPopupActionUseCase
+    ) {
+        var expanded by remember { mutableStateOf(false) }
+        val items = listOf(
+            stringResource(id = R.string.modify),
+            stringResource(id = R.string.delete)
+        )
+
+        Surface(
+            elevation = 4.dp,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 2.dp, bottom = 2.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                Checkbox(
+                    checked = task.done,
+                    onCheckedChange = {
+                        task.done = task.done.not()
+                        CoroutineScope(Dispatchers.IO).launch {
+                            repository.insert(task)
                         }
+                    },
+                    modifier = Modifier
+                        .width(32.dp)
+                        .fillMaxHeight()
+                )
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = task.description,
+                        fontSize = 16.sp,
+                        maxLines = 1,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = DateFormat.format("yyyy/MM/dd(E)", task.dueDate).toString(),
+                        fontSize = 14.sp,
+                        maxLines = 1,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .width(20.dp)
+                        .fillMaxHeight()
+                        .background(color = Color(task.color))
+                )
+                AsyncImage(
+                    R.drawable.ic_more,
+                    contentDescription = stringResource(R.string.menu),
+                    colorFilter = ColorFilter.tint(
+                        Color(color),
+                        BlendMode.SrcIn
+                    ),
+                    modifier = Modifier
+                        .width(32.dp)
+                        .fillMaxHeight()
+                        .clickable {
+                            expanded = true
+                        }
+                        .background(color = Color(task.color))
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier
+                        .background(colorResource(id = R.color.soft_background))
+                ) {
+                    items.forEachIndexed { index, s ->
+                        DropdownMenuItem(onClick = {
+                            when (index) {
+                                0 -> menuUseCase.modify(task)
+                                1 -> menuUseCase.delete(task)
+                            }
+                            expanded = false
+                        }) { Text(text = s) }
                     }
                 }
             }
         }
-
-        val taskAdditionDialogFragmentUseCase =
-            TaskAdditionDialogFragmentUseCase(
-                this,
-                ViewModelProvider(this).get(TaskAdditionDialogFragmentViewModel::class.java)
-            ) {
-                CoroutineScope(Dispatchers.Main).launch {
-                    withContext(Dispatchers.IO) {
-                        repository.insert(it)
-                    }
-                    // TODO refresh()
-                }
-            }
-
-        popup = ItemMenuPopup(
-            context,
-            ItemMenuPopupActionUseCase(
-                { taskAdditionDialogFragmentUseCase.invoke(it) },
-                {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        withContext(Dispatchers.IO) {
-                            repository.delete(it)
-                        }
-                    }
-                }
-            )
-        )
     }
 
 }
