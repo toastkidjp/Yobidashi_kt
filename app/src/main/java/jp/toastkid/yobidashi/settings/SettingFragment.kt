f742dde0ab8c4bbabb83f582897f26bad75b5ddc
Toast Kid <super_q1m@yahoo.co.jp>
2022-03-31 10:06:40 +0900

    feature #853 Rewrite setting top UI with Compose.

--- app/src/main/java/jp/toastkid/yobidashi/settings/SettingFragment.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/SettingFragment.kt
@@ -7,79 +7,130 @@
  */
 package jp.toastkid.yobidashi.settings
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.tabs.TabLayoutMediator
 import jp.toastkid.lib.ContentViewModel
+import jp.toastkid.lib.interop.ComposeViewFactory
 import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.yobidashi.R
-import jp.toastkid.yobidashi.databinding.FragmentSettingsBinding
+import jp.toastkid.yobidashi.settings.color.ColorSettingFragment
+import jp.toastkid.yobidashi.settings.fragment.BrowserSettingFragment
+import jp.toastkid.yobidashi.settings.fragment.ColorFilterSettingFragment
+import jp.toastkid.yobidashi.settings.fragment.DisplayingSettingFragment
+import jp.toastkid.yobidashi.settings.fragment.EditorSettingFragment
+import jp.toastkid.yobidashi.settings.fragment.OtherSettingFragment
+import jp.toastkid.yobidashi.settings.fragment.SearchSettingFragment
 import jp.toastkid.yobidashi.settings.initial.InitialIndexSettingUseCase
+import jp.toastkid.yobidashi.settings.view.screen.BrowserSettingUi
+import jp.toastkid.yobidashi.settings.view.screen.ColorFilterSettingUi
+import jp.toastkid.yobidashi.settings.view.screen.ColorSettingUi
+import jp.toastkid.yobidashi.settings.view.screen.DisplaySettingUi
+import jp.toastkid.yobidashi.settings.view.screen.EditorSettingUi
+import jp.toastkid.yobidashi.settings.view.screen.OtherSettingUi
+import jp.toastkid.yobidashi.settings.view.screen.SearchSettingUi
 
 /**
  * @author toastkidjp
  */
 class SettingFragment : Fragment() {
 
-    /**
-     * DataBinding object.
-     */
-    private lateinit var binding: FragmentSettingsBinding
-
-    private lateinit var preferenceApplier: PreferenceApplier
-
-    private val initialIndexSettingUseCase = InitialIndexSettingUseCase()
-
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        val context = context
-                ?: return super.onCreateView(inflater, container, savedInstanceState)
-        preferenceApplier = PreferenceApplier(context)
+        val activityContext = activity
+            ?: return super.onCreateView(inflater, container, savedInstanceState)
 
-        binding = DataBindingUtil.inflate(inflater, LAYOUT_ID, container, false)
-        binding.fragment = this
-
-        val pagerAdapter = PagerAdapter(this)
-        binding.container.adapter = pagerAdapter
-        binding.container.offscreenPageLimit = 2
-
-        val mediator = TabLayoutMediator(binding.tabLayout, binding.container) { tab, position ->
-            tab.text = pagerAdapter.getPageTitle(position)
-        }
-        mediator.attach()
+        val preferenceApplier = PreferenceApplier(activityContext)
+        val initialIndexSettingUseCase = InitialIndexSettingUseCase()
 
         setHasOptionsMenu(true)
 
-        return binding.root
+        val pages = listOf(
+            DisplayingSettingFragment::class.java to DisplayingSettingFragment,
+            ColorSettingFragment::class.java to ColorSettingFragment,
+            SearchSettingFragment::class.java to SearchSettingFragment,
+            BrowserSettingFragment::class.java to BrowserSettingFragment,
+            EditorSettingFragment::class.java to EditorSettingFragment,
+            ColorFilterSettingFragment::class.java to ColorFilterSettingFragment,
+            OtherSettingFragment::class.java to OtherSettingFragment
+        )
+
+        return ComposeViewFactory().invoke(activityContext) {
+            MaterialTheme() {
+                Column(
+                    modifier = Modifier
+                        .background(colorResource(id = R.color.setting_background))
+                ) {
+                    val selectedIndex = remember { mutableStateOf(initialIndexSettingUseCase.extract(arguments)) }
+                    ScrollableTabRow(
+                        backgroundColor = Color(preferenceApplier.color),
+                        selectedTabIndex = selectedIndex.value,
+                        edgePadding = 4.dp,
+                        divider = {
+                            Divider(
+                                color = Color(preferenceApplier.fontColor),
+                                thickness = 1.dp
+                            )
+                        },
+                        modifier = Modifier.height(44.dp)
+                    ) {
+                        pages.forEachIndexed { index, page ->
+                            Tab(
+                                selected = selectedIndex.value == index,
+                                onClick = {
+                                    selectedIndex.value = index
+                                }
+                            ) {
+                                Text(
+                                    text = stringResource(id = page.second.titleId()),
+                                    color = Color(preferenceApplier.fontColor),
+                                    fontSize = 16.sp
+                                )
+                            }
+                        }
+                    }
+
+                    SwitchContentWithTabIndex(selectedIndex)
+                }
+            }
+        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.container.setCurrentItem(initialIndexSettingUseCase.extract(arguments), false)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        val colorPair = preferenceApplier.colorPair()
-
-        binding.tabLayout.also {
-            it.setBackgroundColor(colorPair.bgColor())
-
-            val fontColor = colorPair.fontColor()
-            it.tabTextColors = ColorStateList.valueOf(fontColor)
-            it.setSelectedTabIndicatorColor(fontColor)
+    @Composable
+    private fun SwitchContentWithTabIndex(selectedIndex: MutableState<Int>) {
+        when (selectedIndex.value) {
+            0 -> DisplaySettingUi()
+            1 -> ColorSettingUi()
+            2 -> SearchSettingUi()
+            3 -> BrowserSettingUi()
+            4 -> EditorSettingUi()
+            5 -> ColorFilterSettingUi()
+            6 -> OtherSettingUi()
+            else -> DisplaySettingUi()
         }
     }
 
@@ -95,16 +146,7 @@ class SettingFragment : Fragment() {
             arguments = Bundle()
         }
 
-        initialIndexSettingUseCase.put(arguments, javaClass)
+        InitialIndexSettingUseCase().put(arguments, javaClass)
     }
 
-    companion object {
-
-        /**
-         * Layout ID.
-         */
-        @LayoutRes
-        private const val LAYOUT_ID = R.layout.fragment_settings
-
-    }
 }
