731edc6295b319c37438c568b1e022fa4b69d222
Toast Kid <super_q1m@yahoo.co.jp>
2022-04-12 07:53:11 +0900

    feature #853 Rewrite favorite-search editor components.

--- app/src/main/java/jp/toastkid/yobidashi/search/favorite/FavoriteSearchListUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/favorite/FavoriteSearchListUi.kt
@@ -10,20 +10,26 @@ package jp.toastkid.yobidashi.search.favorite
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,9 +43,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Popup
 import androidx.lifecycle.ViewModelProvider
 import jp.toastkid.lib.AppBarViewModel
 import jp.toastkid.lib.ContentViewModel
@@ -74,85 +81,71 @@ fun FavoriteSearchListUi() {
 
     ViewModelProvider(activityContext).get(AppBarViewModel::class.java)
         .replace {
-            val editorOpen = remember { mutableStateOf(false) }
+            val spinnerOpen = remember { mutableStateOf(false) }
 
-            Button(
-                onClick = { editorOpen.value = true },
-                colors = ButtonDefaults.textButtonColors(
-                    backgroundColor = colorResource(id = R.color.soft_background),
-                    contentColor = colorResource(id = R.color.colorPrimary),
-                    disabledContentColor = Color.LightGray
+            val categoryName = remember {
+                mutableStateOf(
+                    PreferenceApplier(activityContext).getDefaultSearchEngine()
+                        ?: SearchCategory.getDefaultCategoryName()
                 )
+            }
+
+            val input = remember { mutableStateOf("") }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.height(dimensionResource(id = R.dimen.toolbar_height))
             ) {
-                Text(text = stringResource(id = R.string.add))
+                SearchCategorySpinner(spinnerOpen, categoryName)
 
-                val spinnerOpen = remember { mutableStateOf(false) }
-                val categoryName = remember {
-                    mutableStateOf(
-                        PreferenceApplier(activityContext).getDefaultSearchEngine()
-                            ?: SearchCategory.getDefaultCategoryName()
-                    )
-                }
-                val input = remember { mutableStateOf("") }
-
-                Popup {
-                    Column(
-                        modifier = Modifier
-                            .background(colorResource(id = R.color.soft_background))
-                            .padding(dimensionResource(id = R.dimen.settings_item_left_margin))
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Text(
-                                stringResource(id = R.string.category),
-                                fontSize = 16.sp,
-                                modifier = Modifier.padding(end = 4.dp)
-                            )
-                            SearchCategorySpinner(spinnerOpen, categoryName)
-                        }
-                        // TODO imeActionLabel="@string/title_add"
-                        TextField(
-                            value = input.value,
-                            onValueChange = { input.value = it },
-                            label = { stringResource(id = R.string.word) }
-                        )
-
-                        Button(
-                            onClick = {
-                                if (input.value.isEmpty()) {
-                                    contentViewModel.snackShort(
-                                        R.string.favorite_search_addition_dialog_empty_message
-                                    )
-                                    return@Button
-                                }
-
-                                FavoriteSearchInsertion(
-                                    activityContext,
-                                    categoryName.value,
-                                    input.value
-                                ).invoke()
-
-                                reload(repository, favoriteSearchItems)
-
-                                val message = MessageFormat.format(
-                                    activityContext.getString(R.string.favorite_search_addition_successful_format),
-                                    input.value
-                                )
-                                contentViewModel.snackShort(message)
-                            },
-                            colors = ButtonDefaults.textButtonColors(
-                                backgroundColor = Color(preferenceApplier.color),
-                                contentColor = Color(preferenceApplier.fontColor),
-                                disabledContentColor = Color.LightGray
-                            ),
+                TextField(
+                    value = input.value,
+                    onValueChange = { input.value = it },
+                    label = { stringResource(id = R.string.word) },
+                    singleLine = true,
+                    textStyle = TextStyle(
+                        color = Color(preferenceApplier.fontColor),
+                        textAlign = TextAlign.Start,
+                    ),
+                    trailingIcon = {
+                        Icon(
+                            Icons.Filled.Clear,
+                            contentDescription = "clear text",
+                            tint = Color(preferenceApplier.fontColor),
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(4.dp)
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.title_add)
-                            )
-                        }
-                    }
+                                .clickable {
+                                    input.value = ""
+                                }
+                        )
+                    },
+                    keyboardActions = KeyboardActions {
+                        addItem(input, contentViewModel, activityContext, categoryName, repository, favoriteSearchItems)
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        autoCorrect = true,
+                        imeAction = ImeAction.Done
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+
+                Button(
+                    onClick = {
+                        addItem(
+                            input,
+                            contentViewModel,
+                            activityContext,
+                            categoryName,
+                            repository,
+                            favoriteSearchItems
+                        )
+                    },
+                    colors = ButtonDefaults.textButtonColors(
+                        backgroundColor = colorResource(id = R.color.soft_background),
+                        contentColor = colorResource(id = R.color.colorPrimary),
+                        disabledContentColor = Color.LightGray
+                    )
+                ) {
+                    Text(text = stringResource(id = R.string.add))
                 }
             }
         }
@@ -179,6 +172,36 @@ fun FavoriteSearchListUi() {
             contentViewModel.snackShort(R.string.done_clear)
         }
     }
+}
+
+private fun addItem(
+    input: MutableState<String>,
+    contentViewModel: ContentViewModel,
+    activityContext: ComponentActivity,
+    categoryName: MutableState<String>,
+    repository: FavoriteSearchRepository,
+    favoriteSearchItems: SnapshotStateList<FavoriteSearch>
+) {
+    if (input.value.isEmpty()) {
+        contentViewModel.snackShort(
+            R.string.favorite_search_addition_dialog_empty_message
+        )
+        return
+    }
+
+    FavoriteSearchInsertion(
+        activityContext,
+        categoryName.value,
+        input.value
+    ).invoke()
+
+    reload(repository, favoriteSearchItems)
+
+    val message = MessageFormat.format(
+        activityContext.getString(R.string.favorite_search_addition_successful_format),
+        input.value
+    )
+    contentViewModel.snackShort(message)
 }
 
 @Composable
