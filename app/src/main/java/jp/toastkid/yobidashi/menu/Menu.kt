66a98bcc0d82321a448f1c4185c087b249c5d116
Toast Kid <super_q1m@yahoo.co.jp>
2022-04-13 07:45:55 +0900

    feature #853 Reduce menus.

--- app/src/main/java/jp/toastkid/yobidashi/menu/Menu.kt
+++ app/src/main/java/jp/toastkid/yobidashi/menu/Menu.kt
@@ -31,13 +31,7 @@ enum class Menu(
 
     BOOKMARK(R.string.title_bookmark, R.drawable.ic_bookmark),
 
-    EDITOR(R.string.title_editor, R.drawable.ic_edit),
-
-    PDF(R.string.title_open_pdf, R.drawable.ic_pdf),
-
     CALENDAR(R.string.title_calendar, R.drawable.ic_calendar),
-
-    ARTICLE_VIEWER(R.string.title_article_viewer, R.drawable.ic_article),
 
     CODE_READER(R.string.title_code_reader, R.drawable.ic_barcode),
 
