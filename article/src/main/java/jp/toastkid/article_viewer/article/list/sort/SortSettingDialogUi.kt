6aeeb009cae8b3e888269936be363333b351f49c
Toast Kid <super_q1m@yahoo.co.jp>
2022-05-02 07:59:15 +0900

    refactor #853 Clean up implementation.

--- article/src/main/java/jp/toastkid/article_viewer/article/list/sort/SortSettingDialogUi.kt
+++ article/src/main/java/jp/toastkid/article_viewer/article/list/sort/SortSettingDialogUi.kt
@@ -8,13 +8,11 @@
 
 package jp.toastkid.article_viewer.article.list.sort
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -22,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,42 +33,37 @@ internal fun SortSettingDialogUi(
     openSortDialog: MutableState<Boolean>,
     onSelect: (Sort) -> Unit
 ) {
-    MaterialTheme() {
-        Dialog(
-            onDismissRequest = {
-                openSortDialog.value = false
-            }
-        ) {
-            Surface(
-                modifier = Modifier.background(colorResource(id = R.color.soft_background)),
-                elevation = 4.dp
-            ) {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Text(
-                        text = stringResource(id = R.string.title_sort_setting),
-                        fontSize = 16.sp,
-                        modifier = Modifier.padding(8.dp)
-                    )
+    Dialog(
+        onDismissRequest = {
+            openSortDialog.value = false
+        }
+    ) {
+        Surface(elevation = 4.dp) {
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = stringResource(id = R.string.title_sort_setting),
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
 
-                    val currentSort = Sort.findByName(preferenceApplier.articleSort())
-                    Sort.values().forEach { sort ->
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth()
-                                .clickable {
-                                    preferenceApplier.setArticleSort(sort.name)
-                                    onSelect(sort)
-                                    openSortDialog.value = false
-                                }
-                        ) {
-                            RadioButton(
-                                selected = sort == currentSort,
-                                onClick = {}
-                            )
-                            Text(sort.name)
-                        }
+                val currentSort = Sort.findByName(preferenceApplier.articleSort())
+                Sort.values().forEach { sort ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth()
+                            .clickable {
+                                preferenceApplier.setArticleSort(sort.name)
+                                onSelect(sort)
+                                openSortDialog.value = false
+                            }
+                    ) {
+                        RadioButton(
+                            selected = sort == currentSort,
+                            onClick = {}
+                        )
+                        Text(sort.name)
                     }
                 }
             }
