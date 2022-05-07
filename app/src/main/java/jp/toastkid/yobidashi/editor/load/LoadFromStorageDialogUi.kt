5f59cc1db8693b74e63a1a9de7ee7fec593be99e
Toast Kid <super_q1m@yahoo.co.jp>
2022-05-08 08:51:03 +0900

    refactor #853 Clean up implementation.

--- app/src/main/java/jp/toastkid/yobidashi/editor/load/LoadFromStorageDialogUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/editor/load/LoadFromStorageDialogUi.kt
@@ -8,20 +8,17 @@
 
 package jp.toastkid.yobidashi.editor.load
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,43 +36,38 @@ internal fun LoadFromStorageDialogUi(
 ) {
     files ?: return
 
-    MaterialTheme {
-        Dialog(
-            onDismissRequest = {
-                openDialog.value = false
-            }
-        ) {
-            Surface(
-                modifier = Modifier.background(colorResource(id = R.color.soft_background)),
-                elevation = 4.dp
-            ) {
-                LazyColumn {
-                    item {
-                        Row(
-                            modifier = Modifier.padding(16.dp)
-                        ) {
-                            Icon(
-                                painterResource(id = R.drawable.ic_load),
-                                contentDescription = stringResource(id = R.string.load_from_storage)
-                            )
-                            Text(
-                                text = stringResource(id = R.string.load_from_storage),
-                                fontSize = 18.sp,
-                                modifier = Modifier.padding(4.dp)
-                            )
-                        }
-                    }
-                    items(files) {
+    Dialog(
+        onDismissRequest = {
+            openDialog.value = false
+        }
+    ) {
+        Surface(elevation = 4.dp) {
+            LazyColumn {
+                item {
+                    Row(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Icon(
+                            painterResource(id = R.drawable.ic_load),
+                            contentDescription = stringResource(id = R.string.load_from_storage)
+                        )
                         Text(
-                            text = it.name,
-                            maxLines = 2,
-                            overflow = TextOverflow.Ellipsis,
-                            modifier = Modifier.clickable {
-                                onSelect(it)
-                                openDialog.value = false
-                            }.padding(16.dp)
+                            text = stringResource(id = R.string.load_from_storage),
+                            fontSize = 18.sp,
+                            modifier = Modifier.padding(4.dp)
                         )
                     }
+                }
+                items(files) {
+                    Text(
+                        text = it.name,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.clickable {
+                            onSelect(it)
+                            openDialog.value = false
+                        }.padding(16.dp)
+                    )
                 }
             }
         }
