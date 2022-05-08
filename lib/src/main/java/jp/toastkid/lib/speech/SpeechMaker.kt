9e279f64f05bf6a08c5bce4d58381ff22cb8ec85
Toast Kid <super_q1m@yahoo.co.jp>
2022-05-08 10:27:30 +0900

    refactor #853 Move class and resources for dividing editor to other module.

--- lib/src/main/java/jp/toastkid/lib/speech/SpeechMaker.kt
+++ lib/src/main/java/jp/toastkid/lib/speech/SpeechMaker.kt
@@ -5,7 +5,7 @@
  * which accompany this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
  */
-package jp.toastkid.yobidashi.libs.speech
+package jp.toastkid.libs.speech
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
