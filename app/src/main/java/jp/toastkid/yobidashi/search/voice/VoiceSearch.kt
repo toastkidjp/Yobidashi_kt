d5768f9e1094e01ddc2db917dca5cb8a10dbb514
Toast Kid <super_q1m@yahoo.co.jp>
2022-05-20 09:03:39 +0900

    refactor #412 Delete useless function.

--- app/src/main/java/jp/toastkid/yobidashi/search/voice/VoiceSearch.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/voice/VoiceSearch.kt
@@ -3,12 +3,8 @@ package jp.toastkid.yobidashi.search.voice
 import android.content.Intent
 import android.speech.RecognizerIntent
 import android.view.View
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
-import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.intent.GooglePlayIntentFactory
 import jp.toastkid.yobidashi.BuildConfig
-import jp.toastkid.yobidashi.R
 
 /**
  * Voice search use case.
@@ -31,22 +27,6 @@ class VoiceSearch {
                 )
                 it.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, BuildConfig.APPLICATION_ID)
             }
-
-    /**
-     * Show Google App install suggestion by [com.google.android.material.snackbar.Snackbar].
-     *
-     * @param parent Snackbar parent
-     */
-    fun suggestInstallGoogleApp(parent: View) {
-        val contentViewModel = (parent.context as? ViewModelStoreOwner)?.let {
-            ViewModelProvider(it).get(ContentViewModel::class.java)
-        }
-        contentViewModel?.snackWithAction(
-            parent.context.getString(R.string.message_install_suggestion_google_app),
-            parent.context.getString(R.string.install),
-            { launchGooglePlay(parent) }
-        )
-    }
 
     /**
      * Launch Google Play App.
