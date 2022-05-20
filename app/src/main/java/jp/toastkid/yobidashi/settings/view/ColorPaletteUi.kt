c4f04a06fe540e463589787e3c29cd0d59bae001
Toast Kid <super_q1m@yahoo.co.jp>
2022-05-21 08:38:59 +0900

    feature #853 Add top margin.

--- app/src/main/java/jp/toastkid/yobidashi/settings/view/ColorPaletteUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/view/ColorPaletteUi.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -39,7 +40,8 @@ internal fun ColorPaletteUi(
     onReset: () -> Unit
 ) {
     Surface(
-        elevation = 4.dp
+        elevation = 4.dp,
+        modifier = Modifier.padding(top = 8.dp)
     ) {
         Row(
             modifier = Modifier
