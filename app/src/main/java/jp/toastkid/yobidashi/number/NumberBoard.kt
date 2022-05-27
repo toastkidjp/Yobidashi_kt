1f09c7cf0f101a5255dae9583c5e469e63d0eabf
Toast Kid <super_q1m@yahoo.co.jp>
2022-05-28 08:38:26 +0900

    feature #894 Implement number board model.

---
+++ app/src/main/java/jp/toastkid/yobidashi/number/NumberBoard.kt
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.number
+
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Random
+
+class NumberBoard {
+
+    private val rows: MutableList<MutableList<Int>> = mutableListOf()
+
+    init {
+        fillZero()
+    }
+
+    private fun fillZero() {
+        rows.clear()
+
+        (0 until BOARD_SIZE).forEach { x ->
+            rows.add(mutableListOf())
+            (0 until BOARD_SIZE).forEach { y ->
+                rows[x].add(0)
+            }
+        }
+    }
+
+    fun placeRandom() {
+        fillZero()
+
+        (0 until BOARD_SIZE).forEach { x ->
+            (0 until BOARD_SIZE).forEach { y ->
+                if (rows[y][x] == 0) {
+                    val boxNumbers = getIntInBox(x, y)
+                    val verticalNumbers = getIntVertical(x)
+                    val horizontalNumbers = rows[y]
+                    val next = makeRandomWithout(boxNumbers.union(verticalNumbers).union(horizontalNumbers))
+                    rows[y][x] = next
+                }
+            }
+        }
+    }
+
+    private fun makeRandomWithout(existsNumbers: Set<Int>): Int {
+        val uniqueQueue = makeRandomUniqueQueue(BOARD_SIZE)
+        var n = uniqueQueue.poll()
+        while (existsNumbers.contains(n)) {
+            n = uniqueQueue.poll()
+        }
+        return n ?: -1
+    }
+
+    private fun getIntVertical(x: Int): Set<Int> {
+        val currentNumbers = mutableSetOf<Int>()
+        (0 until BOARD_SIZE).forEach { y ->
+            currentNumbers.add(rows[y][x])
+        }
+
+        return currentNumbers
+    }
+
+    private fun getIntInBox(x: Int, y: Int): Set<Int> {
+        val currentNumbers = mutableSetOf<Int>()
+        (initialIndex(x) .. initialIndex(x) + 2).forEach { _x ->
+            (initialIndex(y)..initialIndex(y) + 2).forEach { _y ->
+                currentNumbers.add(rows[_y][_x])
+            }
+        }
+
+        return currentNumbers
+    }
+
+    private fun initialIndex(n: Int) =
+        when (n) {
+            in (0..2) -> 0
+            in (3..5) -> 3
+            in (6..8) -> 6
+            else -> 0
+        }
+
+    private fun makeRandomUniqueQueue(size: Int): Queue<Int> {
+        val queue = LinkedList<Int>()
+        queue.addAll((1 .. size).shuffled())
+        return queue
+    }
+
+    fun masked(): NumberBoard {
+        val newBoard = NumberBoard()
+        newBoard.copy(this)
+        val random = Random()
+        val randomPair = mutableSetOf<String>()
+
+        while (randomPair.size < 20) {
+            randomPair.add("${random.nextInt(9)}_${random.nextInt(9)}")
+        }
+        randomPair.map {
+            val split = it.split("_")
+            split[0].toInt() to split[1].toInt()
+        }.forEach {
+            newBoard.rows[it.first][it.second] = -1
+        }
+        return newBoard
+    }
+
+    private fun copy(base: NumberBoard) {
+        (0 until BOARD_SIZE).forEach { x ->
+            (0 until BOARD_SIZE).forEach { y ->
+                this.rows[x][y] = base.rows[x][y]
+            }
+        }
+    }
+
+    fun validBoard() =
+        (0 until BOARD_SIZE).none { x ->
+            rows[x].contains(-1)
+        }
+
+    fun rows(): MutableList<MutableList<Int>> {
+        return rows
+    }
+
+    fun isCorrect(correct: NumberBoard?): Boolean {
+        if (correct !is NumberBoard) {
+            return false
+        }
+
+        this.rows.forEachIndexed { rowNumber, row ->
+            correct.rows.get(rowNumber).forEachIndexed { columnIndex, column ->
+                if (column != row.get(columnIndex)) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    companion object {
+
+        private const val BOARD_SIZE = 9
+
+        fun make(): NumberBoard {
+            val numberBoard = NumberBoard()
+            numberBoard.placeRandom()
+            return numberBoard
+        }
+    }
+
+}
