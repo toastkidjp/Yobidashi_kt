b0921867a3555fc83695e4ecc2611e93fd0c01fa
Toast Kid <super_q1m@yahoo.co.jp>
2022-06-04 11:28:15 +0900

    refactor #894 Define repository interface.

---
+++ number/src/main/java/jp/toastkid/number/repository/GameRepository.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.number.repository
+
+import jp.toastkid.number.model.NumberPlaceGame
+import java.io.File
+
+interface GameRepository {
+
+    fun save(file: File, game: NumberPlaceGame)
+
+    fun load(file: File): NumberPlaceGame?
+
+}
