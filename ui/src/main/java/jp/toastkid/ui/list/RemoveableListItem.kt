72f5f596f540b14b60557b8471b812f4d0082845
Toast Kid <super_q1m@yahoo.co.jp>
2022-07-06 09:30:36 +0900

    refactor #853 Remove variable.

--- ui/src/main/java/jp/toastkid/ui/list/RemoveableListItem.kt
+++ ui/src/main/java/jp/toastkid/ui/list/RemoveableListItem.kt
@@ -101,7 +101,6 @@ fun SwipeToDismissItem(
                 state = state,
                 anchors = anchors,
                 thresholds = { from, to ->
-                    getDismissDirection(from, to)
                     FixedThreshold(3000000.dp)
                 },
                 orientation = Orientation.Horizontal,
