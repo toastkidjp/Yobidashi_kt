6b99ba65f31764191c220f6c2ad02c229cb0b4d7
Toast Kid <super_q1m@yahoo.co.jp>
2022-07-08 09:23:15 +0900

    refactor #853 Safe delete 'getDismissDirection'.

--- ui/src/main/java/jp/toastkid/ui/list/RemoveableListItem.kt
+++ ui/src/main/java/jp/toastkid/ui/list/RemoveableListItem.kt
@@ -165,23 +165,3 @@ fun SwipeToDismissItem(
 
     }
 }
-
-private fun getDismissDirection(from: DismissValue, to: DismissValue): DismissDirection? {
-    return when {
-        // settled at the default state
-        from == to && from == DismissValue.Default -> null
-        // has been dismissed to the end
-        from == to && from == DismissValue.DismissedToEnd -> DismissDirection.StartToEnd
-        // has been dismissed to the start
-        from == to && from == DismissValue.DismissedToStart -> DismissDirection.EndToStart
-        // is currently being dismissed to the end
-        from == DismissValue.Default && to == DismissValue.DismissedToEnd -> DismissDirection.StartToEnd
-        // is currently being dismissed to the start
-        from == DismissValue.Default && to == DismissValue.DismissedToStart -> DismissDirection.EndToStart
-        // has been dismissed to the end but is now animated back to default
-        from == DismissValue.DismissedToEnd && to == DismissValue.Default -> DismissDirection.StartToEnd
-        // has been dismissed to the start but is now animated back to default
-        from == DismissValue.DismissedToStart && to == DismissValue.Default -> DismissDirection.EndToStart
-        else -> null
-    }
-}
