9ea1aae9ed3ae51d5cc10102ae7ece98e88510a6
Toast Kid <super_q1m@yahoo.co.jp>
2022-07-11 09:24:51 +0900

    refactor #853 Use divided function.

--- app/src/main/java/jp/toastkid/yobidashi/main/ui/MainUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/main/ui/MainUi.kt
@@ -41,8 +41,6 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
@@ -59,7 +57,6 @@ import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -72,8 +69,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -90,8 +85,6 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
@@ -102,7 +95,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -145,6 +137,7 @@ import jp.toastkid.yobidashi.browser.webview.GlobalWebViewPool
 import jp.toastkid.yobidashi.calendar.view.CalendarUi
 import jp.toastkid.yobidashi.main.RecentAppColoringUseCase
 import jp.toastkid.yobidashi.main.StartUp
+import jp.toastkid.yobidashi.main.finder.FindInPage
 import jp.toastkid.yobidashi.main.usecase.WebSearchResultTabOpenerUseCase
 import jp.toastkid.yobidashi.menu.Menu
 import jp.toastkid.yobidashi.search.favorite.FavoriteSearchListUi
@@ -811,112 +804,6 @@ private fun OverflowMenu(
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun FindInPage(
-    openFindInPageState: MutableState<Boolean>,
-    tint: Color,
-    pageSearcherInput: MutableState<String>
-) {
-    val activity = LocalContext.current as? ViewModelStoreOwner ?: return
-    val pageSearcherViewModel = viewModel(PageSearcherViewModel::class.java, activity)
-    val closeAction = {
-        pageSearcherViewModel.clearInput()
-        pageSearcherViewModel.hide()
-        openFindInPageState.value = false
-    }
-
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            painterResource(id = R.drawable.ic_close),
-            contentDescription = stringResource(id = R.string.content_description_close_find_area),
-            tint = tint,
-            modifier = Modifier
-                .clickable(onClick = closeAction)
-                .padding(start = 16.dp)
-        )
-
-        val focusRequester = remember { FocusRequester() }
-
-        TextField(
-            value = pageSearcherInput.value,
-            onValueChange = { text ->
-                pageSearcherInput.value = text
-                pageSearcherViewModel.find(text)
-            },
-            label = {
-                Text(
-                    stringResource(id = R.string.hint_find_in_page),
-                    color = MaterialTheme.colors.onPrimary
-                )
-            },
-            singleLine = true,
-            textStyle = TextStyle(
-                color = MaterialTheme.colors.onPrimary,
-                textAlign = TextAlign.Start,
-            ),
-            trailingIcon = {
-                Icon(
-                    painterResource(R.drawable.ic_clear_form),
-                    contentDescription = "clear text",
-                    tint = MaterialTheme.colors.onPrimary,
-                    modifier = Modifier
-                        .clickable {
-                            pageSearcherInput.value = ""
-                            pageSearcherViewModel.clearInput()
-                        }
-                )
-            },
-            maxLines = 1,
-            keyboardActions = KeyboardActions {
-                pageSearcherViewModel.findDown(pageSearcherInput.value)
-            },
-            keyboardOptions = KeyboardOptions(
-                autoCorrect = true,
-                imeAction = ImeAction.Search
-            ),
-            modifier = Modifier
-                .weight(1f)
-                .padding(end = 4.dp)
-                .background(Color.Transparent)
-                .focusRequester(focusRequester)
-        )
-        Icon(
-            painterResource(id = R.drawable.ic_up),
-            contentDescription = stringResource(id = R.string.content_description_find_upward),
-            tint = tint,
-            modifier = Modifier
-                .clickable {
-                    pageSearcherViewModel.findUp(
-                        pageSearcherInput.value
-                    )
-                }
-                .padding(8.dp)
-        )
-        Icon(
-            painterResource(id = R.drawable.ic_down),
-            contentDescription = stringResource(id = R.string.content_description_find_downward),
-            tint = tint,
-            modifier = Modifier
-                .clickable {
-                    pageSearcherViewModel.findDown(
-                        pageSearcherInput.value
-                    )
-                }
-                .padding(8.dp)
-        )
-
-        BackHandler(openFindInPageState.value) {
-            closeAction()
-        }
-
-        LaunchedEffect(key1 = "find_in_page_first_launch", block = {
-            if (openFindInPageState.value) {
-                focusRequester.requestFocus()
-            }
-        })
     }
 }
 
