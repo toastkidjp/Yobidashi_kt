572bfc9daea5b4aefebf80e98b8bd41cef9f249c
Toast Kid <super_q1m@yahoo.co.jp>
2022-07-17 09:41:01 +0900

    refactor #412 Remove variable.

--- app/src/main/java/jp/toastkid/yobidashi/search/favorite/FavoriteSearchListUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/search/favorite/FavoriteSearchListUi.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -202,8 +201,6 @@ private fun FavoriteSearchItemList(
     favoriteSearchItems: SnapshotStateList<FavoriteSearch>
 ) {
     val context = LocalContext.current
-
-    rememberCoroutineScope()
 
     val listState = rememberLazyListState()
 
