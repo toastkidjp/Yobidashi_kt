118a09d6361f573ee40108db871021e43dfd1878
Toast Kid <super_q1m@yahoo.co.jp>
2022-07-25 09:11:49 +0900

    refactor #489 Fix unit test.

--- app/src/test/java/jp/toastkid/yobidashi/tab/WebTabTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/tab/WebTabTest.kt
@@ -1,8 +1,9 @@
 package jp.toastkid.yobidashi.tab
 
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
 import jp.toastkid.yobidashi.tab.model.WebTab
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -27,21 +28,21 @@ class WebTabTest {
     }
 
     @Throws(IOException::class)
-    private fun check_fromJson(tabJsonAdapter: JsonAdapter<WebTab>, json: String) {
-        val fromJson = tabJsonAdapter.fromJson(json)
+    private fun check_fromJson(tabJsonAdapter: Json, json: String) {
+        val fromJson = tabJsonAdapter.decodeFromString<WebTab>(json)
         assertEquals("Title", fromJson?.latest?.title())
         assertEquals("URL", fromJson?.latest?.url())
     }
 
-    private fun check_toJson(tab: WebTab, tabJsonAdapter: JsonAdapter<WebTab>): String {
-        val json = tabJsonAdapter.toJson(tab)
-        assertTrue(json.contains("\"histories\":[{\"scrolled\":0,\"title\":\"Title\",\"url\":\"URL\"}]"))
+    private fun check_toJson(tab: WebTab, tabJsonAdapter: Json): String {
+        val json = tabJsonAdapter.encodeToString(tab)
+        assertTrue(json.contains("\"title\":\"Title\""))
+        assertTrue(json.contains("\"url\":\"URL\""))
         return json
     }
 
-    private fun makeTabJsonAdapter(): JsonAdapter<WebTab> {
-        val moshi = Moshi.Builder().build()
-        return moshi.adapter(WebTab::class.java)
+    private fun makeTabJsonAdapter(): Json {
+        return Json { ignoreUnknownKeys = true }
     }
 
     private fun makeTestTab(): WebTab {
