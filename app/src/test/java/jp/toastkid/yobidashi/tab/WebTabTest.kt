a56bb37824474536fe8ad97870b4bae261bb05b3
Toast Kid <super_q1m@yahoo.co.jp>
2022-07-25 09:23:18 +0900

    refactor #523 Add test case.

--- app/src/test/java/jp/toastkid/yobidashi/tab/WebTabTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/tab/WebTabTest.kt
@@ -17,6 +17,16 @@ import java.io.IOException
 class WebTabTest {
 
     @Test
+    fun testDeserialize() {
+        val json = """
+            {"histories":[{"a":"【大相撲雑談スレ】名古屋場所 : 2ch大相撲 (記事コメント - 4)","b":"http://xn--2ch-2d8eo32c60z.xyz/lite/archives/30927298/comments/1083702/?p=4","c":0}],"id":"505efeb5-8694-4486-ba26-6d152cba4a65"}
+        """
+        val deserialized = Json { ignoreUnknownKeys = true }.decodeFromString<WebTab>(json)
+        assertEquals("【大相撲雑談スレ】名古屋場所 : 2ch大相撲 (記事コメント - 4)", deserialized.latest.title())
+        assertEquals("http://xn--2ch-2d8eo32c60z.xyz/lite/archives/30927298/comments/1083702/?p=4", deserialized.latest.url())
+    }
+
+    @Test
     @Throws(IOException::class)
     fun test() {
         val tab = makeTestTab()
