00bad17120412c289d8f1249a5abfb618a5b7a74
Toast Kid <super_q1m@yahoo.co.jp>
2022-08-06 09:08:33 +0900

    feature #506 Simplify image preview.

--- image/src/main/java/jp/toastkid/image/preview/ImagePreviewUi.kt
+++ image/src/main/java/jp/toastkid/image/preview/ImagePreviewUi.kt
@@ -9,18 +9,11 @@
 package jp.toastkid.image.preview
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,11 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import jp.toastkid.image.Image
@@ -41,56 +32,46 @@ import jp.toastkid.image.factory.GifImageLoaderFactory
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun ImagePreviewUi(images: List<Image>, initialIndex: Int) {
-    val listState = rememberLazyListState(initialIndex)
-
     val imageLoader = GifImageLoaderFactory().invoke(LocalContext.current)
 
-    LazyRow(
-        state = listState,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Transparent)
-    ) {
-        items(images, { it.path }) { image ->
-            var scale by remember { mutableStateOf(1f) }
-            var rotation by remember { mutableStateOf(0f) }
-            var offset by remember { mutableStateOf(Offset.Zero) }
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
 
-            val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
-                scale *= zoomChange
-                rotation += rotationChange
-                offset += offsetChange
-            }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
 
-            Row {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(image.path).crossfade(true).build(),
-                    imageLoader = imageLoader,
-                    contentDescription = image.name,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .graphicsLayer(
-                            scaleX = scale,
-                            scaleY = scale,
-                            rotationZ = rotation,
-                            translationX = offset.x,
-                            translationY = offset.y
-                        )
-                        .transformable(state = state)
-                        .pointerInput(Unit) {
-                            detectTapGestures(
-                                onPress = { /* Called when the gesture starts */ },
-                                onDoubleTap = { scale = 1f },
-                                onLongPress = { /* Called on Long Press */ },
-                                onTap = { /* Called on Tap */ }
-                            )
-                        }
+    val index = remember { mutableStateOf(initialIndex) }
+
+    val image = images[index.value]
+
+    Box {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(image.path).crossfade(true).build(),
+            imageLoader = imageLoader,
+            contentDescription = image.name,
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    rotationZ = rotation,
+                    translationX = offset.x,
+                    translationY = offset.y
                 )
-                Spacer(modifier = Modifier
-                    .width(120.dp * scale)
-                    .height(1.dp))
-            }
-        }
+                .transformable(state = state)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = { /* Called when the gesture starts */ },
+                        onDoubleTap = { scale = 1f },
+                        onLongPress = { /* Called on Long Press */ },
+                        onTap = { /* Called on Tap */ }
+                    )
+                }
+        )
     }
 }
