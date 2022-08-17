53243a9b3d3ebba4689454ed83541f69c1fe7d13
Toast Kid <super_q1m@yahoo.co.jp>
2022-08-17 09:20:16 +0900

    refactor #506 Remove useless background color.

--- image/src/main/java/jp/toastkid/image/preview/ImagePreviewUi.kt
+++ image/src/main/java/jp/toastkid/image/preview/ImagePreviewUi.kt
@@ -147,7 +147,6 @@ internal fun ImagePreviewUi(images: List<Image>, initialIndex: Int) {
             modifier = Modifier
                 .fillMaxWidth()
                 .height(56.dp)
-                .background(Color(0xAAFF9999))
                 .align(Alignment.Center)
                 .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
                 .swipeable(
