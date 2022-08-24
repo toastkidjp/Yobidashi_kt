296521cb8b64614c60b29ac64786c501a544a460
Toast Kid <super_q1m@yahoo.co.jp>
2022-08-24 09:05:11 +0900

    refactor #523 Divide to other class.

---
+++ app/src/main/java/jp/toastkid/yobidashi/browser/block/SiteNameChecker.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser.block
+
+class SiteNameChecker {
+
+    operator fun invoke(host: String?) =
+        host?.endsWith(".xyz") == true
+                || host?.endsWith(".jp.net") == true
+                || host == "rt.gsspat.jp"
+                || host == "webnew.net"
+                || host == "jp.img4.uk"
+}
