b7c5759ddef7351994fe9fc9edbb8065a8064883
Toast Kid <super_q1m@yahoo.co.jp>
2022-09-07 09:24:55 +0900

    refactor #853 Rename function.

--- lib/src/main/java/jp/toastkid/lib/compat/ParcelableCompat.kt
+++ lib/src/main/java/jp/toastkid/lib/compat/ParcelableCompat.kt
@@ -12,12 +12,12 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 
-inline fun <reified T> Intent.parcelable(key: String): T? = when {
+inline fun <reified T> Intent.getParcelableCompat(key: String): T? = when {
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
 }
 
-inline fun <reified T> Bundle.parcelable(key: String): T? = when {
+inline fun <reified T> Bundle.getParcelableCompat(key: String): T? = when {
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelable(key) as? T
 }
