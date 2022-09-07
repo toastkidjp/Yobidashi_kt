207aa39bb0e1b091d935cce36e74b241d79ae5fd
Toast Kid <super_q1m@yahoo.co.jp>
2022-09-07 09:14:33 +0900

    refactor #853 Add extension function for compatibility.

---
+++ lib/src/main/java/jp/toastkid/lib/compat/ParcelableCompat.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.lib.compat
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+
+inline fun <reified T> Intent.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+}
+
+inline fun <reified T> Bundle.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
