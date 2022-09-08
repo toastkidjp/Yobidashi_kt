e21e956f3acb25b14b8eb128336eaefabbb18bee
Toast Kid <super_q1m@yahoo.co.jp>
2022-09-08 09:14:13 +0900

    refactor #580 Add dependencies for using WorkManager.

---
+++ article/src/test/java/jp/toastkid/article_viewer/zip/ZipLoaderWorkerTest.kt
@@ -0,0 +1,21 @@
+package jp.toastkid.article_viewer.zip
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.*
+
+class ZipLoaderWorkerTest {
+
+    @org.junit.Before
+    fun setUp() {
+    }
+
+    @org.junit.After
+    fun tearDown() {
+    }
+
+    @org.junit.Test
+    fun doWork() {
+    }
+}
