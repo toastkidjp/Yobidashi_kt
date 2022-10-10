6d860ae8e230e755f0631ebfe7a617506ea51f3f
Toast Kid <super_q1m@yahoo.co.jp>
2022-10-11 08:26:12 +0900

    refactor #496 Wrap too long lines.

--- music/src/main/java/jp/toastkid/media/music/notification/NotificationFactory.kt
+++ music/src/main/java/jp/toastkid/media/music/notification/NotificationFactory.kt
@@ -48,13 +48,15 @@ class NotificationFactory(
     private val previousAction = NotificationCompat.Action(
         R.drawable.ic_previous_media,
         context.getString(R.string.action_skip_to_previous),
-        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS)
+        MediaButtonReceiver
+            .buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS)
     )
 
     private val nextAction = NotificationCompat.Action(
         R.drawable.ic_next_media,
         context.getString(R.string.action_skip_to_next),
-        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_SKIP_TO_NEXT)
+        MediaButtonReceiver
+            .buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_SKIP_TO_NEXT)
     )
 
     operator fun invoke(): Notification? {
