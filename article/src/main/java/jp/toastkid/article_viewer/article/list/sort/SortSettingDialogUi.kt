40ad2a56240947cb61cd58d7171c2efc60c8ba6b
Toast Kid <super_q1m@yahoo.co.jp>
2022-10-14 08:57:07 +0900

    refactor #580 Optimize imports.

--- article/src/main/java/jp/toastkid/article_viewer/article/list/sort/SortSettingDialogUi.kt
+++ article/src/main/java/jp/toastkid/article_viewer/article/list/sort/SortSettingDialogUi.kt
@@ -14,21 +14,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import jp.toastkid.article_viewer.R
-import jp.toastkid.lib.color.IconColorFinder
 import jp.toastkid.lib.preference.PreferenceApplier
 
 @Composable
