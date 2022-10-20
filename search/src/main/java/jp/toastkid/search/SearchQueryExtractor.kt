f3634f43a5d6b54087d8cc1b7cfe56dcd93cb3e1
Toast Kid <super_q1m@yahoo.co.jp>
2022-10-20 09:21:23 +0900

    feature #412 Add search category extraction case.

--- search/src/main/java/jp/toastkid/search/SearchQueryExtractor.kt
+++ search/src/main/java/jp/toastkid/search/SearchQueryExtractor.kt
@@ -61,6 +61,7 @@ class SearchQueryExtractor {
                     or host.equals("you.com")
                     or host.equals("seekingalpha.com")
                     or host.equals("500px.com")
+                    or host.equals("gizmodo.com")
                     or host.equals("www.givero.com") ->
                 uri.getQueryParameter("q")
             host.startsWith("www.amazon.") ->
