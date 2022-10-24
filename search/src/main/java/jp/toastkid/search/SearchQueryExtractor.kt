3790e223fe3f85222e2b79dad7070afedc7e3f83
Toast Kid <super_q1m@yahoo.co.jp>
2022-10-25 08:38:52 +0900

    feature #412 Add search query extraction.

--- search/src/main/java/jp/toastkid/search/SearchQueryExtractor.kt
+++ search/src/main/java/jp/toastkid/search/SearchQueryExtractor.kt
@@ -62,6 +62,7 @@ class SearchQueryExtractor {
                     or host.equals("seekingalpha.com")
                     or host.equals("500px.com")
                     or host.equals("gizmodo.com")
+                    or host.equals("www.politico.com")
                     or host.equals("www.givero.com") ->
                 uri.getQueryParameter("q")
             host.startsWith("www.amazon.") ->
