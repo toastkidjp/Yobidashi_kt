0a0be205aef5d6cd907f76a37f940db91de3863b
Toast Kid <super_q1m@yahoo.co.jp>
2022-11-03 07:49:43 +0900

    refactor #523 Divide to use case.

---
+++ app/src/main/java/jp/toastkid/yobidashi/browser/usecase/PrintCurrentPageUseCase.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.browser.usecase
+
+import android.content.Context
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.webkit.WebView
+
+class PrintCurrentPageUseCase {
+
+    operator fun invoke(webView: WebView?) {
+        webView ?: return
+        val adapter = webView.createPrintDocumentAdapter("${webView.title}.pdf")
+        val printManager = webView.context.getSystemService(Context.PRINT_SERVICE) as? PrintManager
+        printManager?.print("PrintPDF", adapter, PrintAttributes.Builder().build())
+    }
+
+}
