9edf3f43b8716ada30a8d16356d1e4bc119568ad
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-11 09:46:17 +0900

    feature #990 Implement calendar UI with Jetpack Compose.

--- app/src/main/java/jp/toastkid/yobidashi/calendar/view/CalendarUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/calendar/view/CalendarUi.kt
@@ -8,36 +8,51 @@
 
 package jp.toastkid.yobidashi.calendar.view
 
-import android.os.Build
+import android.content.Context
 import android.widget.DatePicker
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.net.toUri
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.toastkid.article_viewer.article.data.AppDatabase
 import jp.toastkid.article_viewer.calendar.DateSelectedActionUseCase
 import jp.toastkid.lib.BrowserViewModel
 import jp.toastkid.lib.ContentViewModel
-import jp.toastkid.lib.Urls
-import jp.toastkid.lib.preference.PreferenceApplier
-import jp.toastkid.ui.parts.InsetDivider
 import jp.toastkid.yobidashi.R
-import jp.toastkid.yobidashi.wikipedia.today.DateArticleUrlFactory
+import jp.toastkid.yobidashi.calendar.model.Week
 import java.util.Calendar
+import java.util.GregorianCalendar
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CalendarUi() {
     val context = LocalContext.current as? ComponentActivity ?: return
@@ -54,22 +69,193 @@ fun CalendarUi() {
         monthOfYear = m
         dayOfMonth = d
     }
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q
-        && PreferenceApplier(context).useDarkMode()) {
-        datePicker.setBackgroundColor(MaterialTheme.colorScheme.onSurface.toArgb())
-    }
 
-    Surface(shadowElevation = 4.dp) {
+
+    val week = arrayOf(
+        Calendar.SUNDAY,
+        Calendar.MONDAY,
+        Calendar.TUESDAY,
+        Calendar.WEDNESDAY,
+        Calendar.THURSDAY,
+        Calendar.FRIDAY,
+        Calendar.SATURDAY
+    )
+
+    val currentDate = rememberSaveable { mutableStateOf(Calendar.getInstance()) }
+
+    Surface(
+        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.75f),
+        shadowElevation = 4.dp
+    ) {
         Column {
-            AndroidView(
-                factory = { datePicker },
-                modifier = Modifier
-                    .padding(8.dp)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Button(onClick = {
+                    currentDate.value = GregorianCalendar(
+                        currentDate.value.get(Calendar.YEAR),
+                        currentDate.value.get(Calendar.MONTH) - 1,
+                        1
+                    )
+                }, modifier = Modifier.padding(8.dp)) {
+                    Text("<", modifier = Modifier.padding(8.dp))
+                }
+
+                Surface(modifier = Modifier.padding(8.dp)) {
+                    val openYearChooser = remember { mutableStateOf(false) }
+                    Box(modifier = Modifier.clickable { openYearChooser.value = true }) {
+                        Text("${currentDate.value.get(Calendar.YEAR)}", fontSize = 16.sp)
+                        DropdownMenu(
+                            expanded = openYearChooser.value,
+                            onDismissRequest = { openYearChooser.value = false }) {
+                            val years = (1900..2200).toList()
+                            Box {
+                                val state =
+                                    rememberLazyListState(years.indexOf(currentDate.value.get(Calendar.YEAR)))
+                                LazyColumn(
+                                    state = state,
+                                    modifier = Modifier.size(200.dp, 500.dp)
+                                ) {
+                                    items(years) {
+                                        Text(
+                                            "${it}",
+                                            fontSize = 16.sp,
+                                            modifier = Modifier
+                                                .padding(8.dp)
+                                                .clickable {
+                                                    currentDate.value.set(Calendar.YEAR, it)
+                                                    openYearChooser.value = false
+                                                })
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Text("/", fontSize = 16.sp, modifier = Modifier.padding(8.dp))
+
+                Surface(modifier = Modifier.padding(8.dp)) {
+                    val openMonthChooser = remember { mutableStateOf(false) }
+                    Box(modifier = Modifier.clickable { openMonthChooser.value = true }) {
+                        Text("${currentDate.value.get(Calendar.MONTH) + 1}", fontSize = 16.sp)
+                        DropdownMenu(
+                            expanded = openMonthChooser.value,
+                            onDismissRequest = { openMonthChooser.value = false }) {
+                            (1..12).forEach {
+                                DropdownMenuItem(
+                                    text = { Text("${it}") },
+                                    onClick = {
+                                        currentDate.value = GregorianCalendar(
+                                            currentDate.value.get(Calendar.YEAR),
+                                            it - 1,
+                                            1
+                                        )
+                                    openMonthChooser.value = false
+                                })
+                            }
+                        }
+                    }
+                }
+
+                Button(
+                    onClick = {
+                        currentDate.value = GregorianCalendar(
+                            currentDate.value.get(Calendar.YEAR),
+                            currentDate.value.get(Calendar.MONTH) + 1,
+                            1
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                ) {
+                    Text(">", modifier = Modifier.padding(8.dp))
+                }
+
+                Button(
+                    onClick = {
+                        currentDate.value = Calendar.getInstance()
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_calendar),
+                        contentDescription = "Current month", modifier = Modifier.padding(8.dp))
+                }
+            }
+            Row {
+                week.forEach { dayOfWeek ->
+                    Surface(modifier = Modifier.weight(1f)) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Text(
+                                "${getDayOfWeekLabel(dayOfWeek)}",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = when (dayOfWeek) {
+                                    Calendar.SUNDAY -> OFF_DAY_FG
+                                    Calendar.SATURDAY -> SATURDAY_FG
+                                    else -> MaterialTheme.colorScheme.onSurface
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+
+            val firstDay = GregorianCalendar(
+                currentDate.value.get(Calendar.YEAR),
+                currentDate.value.get(Calendar.MONTH),
+                1,
             )
 
-            InsetDivider()
+            val weeks = makeMonth(week, firstDay)
 
-            SingleLineMenu(R.string.menu_what_happened_today) {
+            weeks.forEach { w ->
+                Row {
+                    w.days().forEach { day ->
+                        DayLabelView(day.date, day.dayOfWeek, day.offDay,
+                            isToday(currentDate.value, day.date),
+                            modifier = Modifier
+                                .weight(1f)
+                                .combinedClickable(
+                                    enabled = day.date != -1,
+                                    onClick = {
+                                        if (day.date == -1) {
+                                            return@combinedClickable
+                                        }
+                                        openDateArticle(
+                                            context,
+                                            contentViewModel,
+                                            currentDate.value.get(Calendar.YEAR),
+                                            currentDate.value.get(Calendar.MONTH),
+                                            currentDate.value.get(Calendar.DAY_OF_MONTH)
+                                        )
+                                    },
+                                    onLongClick = {
+                                        if (day.date == -1) {
+                                            return@combinedClickable
+                                        }
+                                        openDateArticle(
+                                            context,
+                                            contentViewModel,
+                                            currentDate.value.get(Calendar.YEAR),
+                                            currentDate.value.get(Calendar.MONTH),
+                                            currentDate.value.get(Calendar.DAY_OF_MONTH)
+                                        )
+                                    }
+                                )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+            /*SingleLineMenu(R.string.menu_what_happened_today) {
                 val url = DateArticleUrlFactory()(
                     context,
                     monthOfYear,
@@ -89,22 +275,63 @@ fun CalendarUi() {
                         .articleRepository(),
                     contentViewModel
                 ).invoke(year, monthOfYear, dayOfMonth)
+            }*/
+
+private fun openDateArticle(context: Context, contentViewModel: ContentViewModel, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+    DateSelectedActionUseCase(
+        AppDatabase
+            .find(context)
+            .articleRepository(),
+        contentViewModel
+    ).invoke(year, monthOfYear, dayOfMonth)
+}
+
+private fun isToday(value: Calendar, date: Int): Boolean {
+    return value.get(Calendar.YEAR) == Calendar.getInstance().get(Calendar.YEAR) && value.get(Calendar.MONTH) == Calendar.getInstance().get(Calendar.MONTH) && Calendar.getInstance().get(Calendar.DAY_OF_MONTH) == date
+}
+
+private fun makeMonth(
+    week: Array<Int>,
+    firstDay: Calendar
+): MutableList<Week> {
+    var hasStarted1 = false
+    var current1 = firstDay
+    val weeks = mutableListOf<Week>()
+    for (i in 0..5) {
+        val w = Week()
+        week.forEach { dayOfWeek ->
+            if (hasStarted1.not() && dayOfWeek != firstDay.get(Calendar.DAY_OF_WEEK)) {
+                w.addEmpty()
+                return@forEach
             }
+            hasStarted1 = true
+
+            if (firstDay.get(Calendar.MONTH) != current1.get(Calendar.MONTH)) {
+                w.addEmpty()
+            } else {
+                w.add(current1)
+            }
+            current1.add(Calendar.DAY_OF_MONTH, 1)
         }
-
+        if (w.anyApplicableDate()) {
+            weeks.add(w)
+        }
     }
+    return weeks
 }
 
-@Composable
-private fun SingleLineMenu(
-    titleId: Int,
-    onClick: () -> Unit
-) {
-    Text(
-        stringResource(id = titleId),
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick)
-            .padding(16.dp)
-    )
-}
+private fun getDayOfWeekLabel(dayOfWeek: Int) =
+    when (dayOfWeek) {
+        Calendar.SUNDAY -> "Sun"
+        Calendar.MONDAY -> "Mon"
+        Calendar.TUESDAY -> "Tue"
+        Calendar.WEDNESDAY -> "Wed"
+        Calendar.THURSDAY -> "Thu"
+        Calendar.FRIDAY -> "Fri"
+        Calendar.SATURDAY -> "Sat"
+        else -> ""
+    }
+
+private val DAY_FG: Color = Color.Black
+private val OFF_DAY_FG: Color = Color(190, 50, 55)
+private val SATURDAY_FG: Color = Color(55, 50, 190)
