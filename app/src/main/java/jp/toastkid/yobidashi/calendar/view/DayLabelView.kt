e20d4088d9b468c7ad8cf46ddf7aba49b2a324aa
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-12 07:59:20 +0900

    refactor #990 Delete useless field.

--- app/src/main/java/jp/toastkid/yobidashi/calendar/view/DayLabelView.kt
+++ app/src/main/java/jp/toastkid/yobidashi/calendar/view/DayLabelView.kt
@@ -41,7 +41,6 @@ fun DayLabelView(date: Int, dayOfWeek: Int, offDay: Boolean, today: Boolean, mod
     }
 }
 
-private val DAY_FG: Color = Color.Black
 private val OFF_DAY_FG:  Color = Color(220, 50, 55)
 private val SATURDAY_FG:  Color = Color(55, 50, 190)
 private val DAY_BG: Color = Color(250, 250, 255)
