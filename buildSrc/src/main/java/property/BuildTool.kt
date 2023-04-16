7d8bfae7f32a6052681f2604c0a18185e6150c13
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-17 08:20:44 +0900

    refactor #993 Add buildSrc.

---
+++ buildSrc/src/main/java/property/BuildTool.kt
@@ -0,0 +1,9 @@
+package property
+
+object BuildTool {
+    val minSdk = 24 
+    val compileSdk =  33 
+    val targetSdk =  33 
+    val buildTools =  "33.0.0" 
+    val kotlinVersion =  "1.7.20" 
+}
---
+++ buildSrc/src/main/java/property/Version.kt
@@ -0,0 +1,18 @@
+package property
+
+object Version {
+    private val major  = 2
+    private val middle = 0
+    private val minor  = 47
+
+    init {
+        if (middle >= 100 || minor >= 10_000) {
+            throw IllegalStateException("Middle and Minor version value is allowed under 100.")
+        }
+    }
+
+    val code = ((major * 1_000_000) + (middle * 10_000) + minor) 
+
+    val name = "${major}.${middle}.${minor}" 
+
+}
