cd0cc073c543e5e9eb5c6fddc862faa4a93c0c22
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-17 08:24:18 +0900

    refactor #993 Rewrite build.gradle to kts.

---
+++ api/build.gradle.kts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+import property.*
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("jacoco")
+}
+
+android {
+    compileSdkVersion(BuildTool.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(BuildTool.minSdk)
+    }
+
+    buildTypes {
+        release {
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:${LibraryVersion.okhttp}")
+    implementation("com.jakewharton.timber:timber:${LibraryVersion.timber}")
+
+    testImplementation("junit:junit:${LibraryVersion.junit}")
+    testImplementation("io.mockk:mockk:${LibraryVersion.mockk}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${LibraryVersion.coroutinesTest}")
+}
+
+setUpJaCoCo()
+
+tasks.withType<Test> {
+    configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+}
