7aaa944117d17cae4b892e3655b34f2142f1727e
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-18 07:29:25 +0900

    refactor #993 Rewrite build script to KTS.

---
+++ music/build.gradle.kts
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+import property.BuildTool
+import property.LibraryVersion
+import com.android.build.gradle.tasks.GenerateBuildConfig
+
+plugins {
+  id("com.android.library")
+  id("kotlin-android")
+  id("org.jetbrains.kotlin.kapt")
+}
+//TODO apply(from = "../jacoco.gradle.kts")
+
+android {
+    compileSdkVersion(BuildTool.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(BuildTool.minSdk)
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = LibraryVersion.composeCompiler
+    }
+}
+
+tasks.withType<GenerateBuildConfig> {
+    isEnabled = false
+}
+
+dependencies {
+    implementation(project(path = ":lib"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${BuildTool.kotlinVersion}")
+
+    implementation("androidx.core:core-ktx:${LibraryVersion.ktx}")
+
+    // Compose dependencies.
+    implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
+    implementation("io.coil-kt:coil-compose:${LibraryVersion.coilCompose}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${LibraryVersion.lifecycle}")
+    implementation("androidx.compose.runtime:runtime-livedata:${LibraryVersion.compose}")
+    implementation("androidx.activity:activity-compose:1.4.0")
+
+    implementation("androidx.media:media:1.3.0")
+
+    implementation("com.jakewharton.timber:timber:${LibraryVersion.timber}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${LibraryVersion.coroutines}")
+
+    testImplementation("junit:junit:${LibraryVersion.junit}")
+    testImplementation("io.mockk:mockk:${LibraryVersion.mockk}")
+    testImplementation("org.robolectric:robolectric:${LibraryVersion.robolectric}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${LibraryVersion.coroutinesTest}")
+}
