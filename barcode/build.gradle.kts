eea0a5f778c8bafba1273fd8d23e492a15fc64ba
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-18 07:31:16 +0900

    refactor #993 Rewrite build script to KTS.

---
+++ barcode/build.gradle.kts
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2021 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+import property.*
+import com.android.build.gradle.tasks.GenerateBuildConfig
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("jacoco.definition")
+}
+
+// TODO apply from: '../jacoco.gradle'
+
+android {
+    compileSdkVersion(BuildTool.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(BuildTool.minSdk)
+    }
+
+    buildTypes {
+        release {
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = LibraryVersion.composeCompiler
+    }
+}
+
+tasks.withType<GenerateBuildConfig> {
+    isEnabled = false
+}
+
+dependencies {
+    implementation(project(":lib"))
+
+    implementation("androidx.core:core-ktx:${LibraryVersion.ktx}")
+
+    // Compose dependencies.
+    implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
+    implementation("io.coil-kt:coil-compose:${LibraryVersion.coilCompose}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${LibraryVersion.lifecycle}")
+    implementation("androidx.compose.runtime:runtime-livedata:${LibraryVersion.compose}")
+    implementation("androidx.activity:activity-compose:1.4.0")
+
+    implementation("androidx.camera:camera-camera2:${LibraryVersion.cameraX}")
+    implementation("androidx.camera:camera-lifecycle:${LibraryVersion.cameraX}")
+    implementation("androidx.camera:camera-view:1.0.0-alpha24")
+    implementation("com.google.zxing:core:3.4.1")
+
+    implementation("com.jakewharton.timber:timber:${LibraryVersion.timber}")
+
+    testImplementation("junit:junit:${LibraryVersion.junit}")
+    testImplementation("io.mockk:mockk:${LibraryVersion.mockk}")
+}
