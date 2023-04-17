c64cc0a755e1b3a6184f6e664e9ac88d0a7d03b9
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-18 07:27:58 +0900

    refactor #993 Rewrite build script to KTS.

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
+
+include(":todo")
+include(":search")
+include(":lib")
+include(":app")
+include(":article")
+include(":licenses")
+include(":music")
+include(":pdf")
+include(":api")
+include(":rss")
+include(":image")
+include(":loan")
+include(":about")
+include(":barcode")
+include(":ui")
+include(":editor")
+include(":number")
+include(":lib:display_effect")
