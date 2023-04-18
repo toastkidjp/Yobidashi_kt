7e1dbcc0f69e19855de36ce3320eec067c2c5e2e
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-19 00:22:34 +0900

    refactor #993 Rewrite build script to kts.

---
+++ number/build.gradle.kts
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+import property.BuildTool
+import property.LibraryVersion
+import com.android.build.gradle.tasks.GenerateBuildConfig
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("org.jetbrains.kotlin.plugin.serialization") version("1.6.21")
+}
+
+// TODO apply(from = "../jacoco.gradle.kts")
+// TODO apply(from = "../detekt.gradle.kts")
+
+android {
+    compileSdkVersion(BuildTool.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(BuildTool.minSdk)
+    }
+
+    buildTypes {
+        release {
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "${LibraryVersion.composeCompiler}"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<GenerateBuildConfig> {
+    isEnabled = false
+}
+
+dependencies {
+    implementation(project(path = ":lib"))
+    implementation(project(path = ":ui"))
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${LibraryVersion.lifecycle}")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+
+    testImplementation("junit:junit:${LibraryVersion.junit}")
+    testImplementation("io.mockk:mockk:${LibraryVersion.mockk}")
+}
