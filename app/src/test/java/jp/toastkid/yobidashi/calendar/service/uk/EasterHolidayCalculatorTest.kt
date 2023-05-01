6ec7c5ae122c261a49c20928105525f5d15fec07
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-02 08:41:24 +0900

    feature #990 Implement holiday calculator.

---
+++ app/src/test/java/jp/toastkid/yobidashi/calendar/service/uk/EasterHolidayCalculatorTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.yobidashi.calendar.service.uk
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EasterHolidayCalculatorTest {
+
+    @Test
+    fun test() {
+        val calculator = EasterHolidayCalculator()
+        val holidays = (2022..2024).map { calculator.invoke(it) }
+        // 2022
+        assertEquals(4, holidays[0][0].month)
+        assertEquals(15, holidays[0][0].day)
+        assertEquals(4, holidays[0][1].month)
+        assertEquals(18, holidays[0][1].day)
+        // 2023
+        assertEquals(4, holidays[1][0].month)
+        assertEquals(7, holidays[1][0].day)
+        assertEquals(4, holidays[1][1].month)
+        assertEquals(10, holidays[1][1].day)
+        // 2024
+        assertEquals(3, holidays[2][0].month)
+        assertEquals(29, holidays[2][0].day)
+        assertEquals(4, holidays[2][1].month)
+        assertEquals(1, holidays[2][1].day)
+    }
+
+}
