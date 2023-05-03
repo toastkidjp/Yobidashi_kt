1760bdefd3f693db3f2ad8b9ecc3f46e3ec9fe0d
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-04 06:36:36 +0900

    refactor #388 Implement content and tab events.

---
+++ lib/src/main/java/jp/toastkid/lib/viewmodel/event/content/ToBottomEvent.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+package jp.toastkid.lib.viewmodel.event.content
+
+import jp.toastkid.lib.viewmodel.event.Event
+
+class ToBottomEvent : Event {
+}
