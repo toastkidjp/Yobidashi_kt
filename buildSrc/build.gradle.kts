9f10431cbd7bd9d653c3f3f6f9fcc3fec17a1f21
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-06 08:48:57 +0900

    feature #853 Upgrade kotlin plugin to 1.8.20.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,5 +7,5 @@ plugins {
 }
 dependencies {
     implementation("com.android.tools.build:gradle:7.3.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
 }
