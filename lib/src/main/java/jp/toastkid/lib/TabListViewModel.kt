d9201164d1165d54b243ebaa5fea57db5511ff6a
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-11 09:02:59 +0900

    refactor #853 Safe delete 'tabCount'.

--- lib/src/main/java/jp/toastkid/lib/TabListViewModel.kt
+++ lib/src/main/java/jp/toastkid/lib/TabListViewModel.kt
@@ -7,8 +7,6 @@
  */
 package jp.toastkid.lib
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import jp.toastkid.lib.viewmodel.event.Event
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -22,13 +20,5 @@ class TabListViewModel : ViewModel() {
     private val _event = MutableSharedFlow<Event>()
 
     val event = _event.asSharedFlow()
-
-    private val _tabCount = mutableStateOf(0)
-
-    val tabCount: State<Int> = _tabCount
-
-    fun tabCount(count: Int) {
-        _tabCount.value = count
-    }
 
 }
