48922a5688d30b19d6076b80ec2c2bf24fbfd938
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-12 09:26:09 +0900

    refactor #853 Delete field.

--- lib/src/main/java/jp/toastkid/lib/TabListViewModel.kt
+++ lib/src/main/java/jp/toastkid/lib/TabListViewModel.kt
@@ -8,17 +8,10 @@
 package jp.toastkid.lib
 
 import androidx.lifecycle.ViewModel
-import jp.toastkid.lib.viewmodel.event.Event
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 
 /**
  * @author toastkidjp
  */
 class TabListViewModel : ViewModel() {
-
-    private val _event = MutableSharedFlow<Event>()
-
-    val event = _event.asSharedFlow()
 
 }
