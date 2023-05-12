4ba8763af6c23663779eb7b915ea206d3008c658
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-12 09:24:34 +0900

    refactor #853 Delete useless function.

--- lib/src/main/java/jp/toastkid/lib/viewmodel/PageSearcherViewModel.kt
+++ lib/src/main/java/jp/toastkid/lib/viewmodel/PageSearcherViewModel.kt
@@ -8,12 +8,9 @@
 package jp.toastkid.lib.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import jp.toastkid.lib.viewmodel.event.Event
-import jp.toastkid.lib.viewmodel.event.finder.FindInPageEvent
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.launch
 
 /**
  * @author toastkidjp
@@ -23,11 +20,5 @@ class PageSearcherViewModel : ViewModel() {
     private val _event = MutableSharedFlow<Event>()
 
     val event = _event.asSharedFlow()
-
-    fun find(s: String?) {
-        viewModelScope.launch {
-            _event.emit(FindInPageEvent(s ?: ""))
-        }
-    }
 
 }
