a5252718ba79a70b89de26ed8ccfbfec0a00d3f9
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-14 08:05:10 +0900

    refactor #289 Add spacer.

--- app/src/main/java/jp/toastkid/yobidashi/settings/view/screen/CalendarSettingUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/view/screen/CalendarSettingUi.kt
@@ -10,6 +10,8 @@ package jp.toastkid.yobidashi.settings.view.screen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -88,6 +90,10 @@ internal fun CalendarSettingUi() {
                     booleanState,
                     MaterialTheme.colorScheme.secondary
                 )
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(200.dp))
             }
         }
 
