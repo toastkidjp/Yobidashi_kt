e4aac0fad89cb7bd5309482327f9d77ab9448130
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-14 08:09:03 +0900

    feature #998 Wrap Surface.

--- converter/src/main/java/jp/toastkid/converter/presentation/ui/ConverterToolUi.kt
+++ converter/src/main/java/jp/toastkid/converter/presentation/ui/ConverterToolUi.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import jp.toastkid.converter.domain.model.TatamiCountConverter
 import jp.toastkid.converter.domain.model.UnixTimeConverter
 
@@ -26,22 +28,32 @@ fun ConverterToolUi() {
     val converters = remember { listOf(TatamiCountConverter(), UnixTimeConverter()) }
     val currentIndex = remember { mutableStateOf(0) }
     val openChooser = remember { mutableStateOf(false) }
-    Column {
-        Box(modifier = Modifier.clickable { openChooser.value = true }) {
-            Text(converters[currentIndex.value].title())
-            DropdownMenu(expanded = openChooser.value, onDismissRequest = { openChooser.value = false }) {
-                converters.forEachIndexed { index, converter ->
-                    DropdownMenuItem(
-                        text = { Text(converter.title()) },
-                        onClick = {
-                            currentIndex.value = index
-                            openChooser.value = false
+
+    Surface(
+        shadowElevation = 4.dp
+    ) {
+        Column {
+            Surface(
+                shadowElevation = 4.dp
+            ) {
+                Box(modifier = Modifier.clickable { openChooser.value = true }) {
+                    Text(converters[currentIndex.value].title())
+                    DropdownMenu(expanded = openChooser.value, onDismissRequest = { openChooser.value = false }) {
+                        converters.forEachIndexed { index, converter ->
+                            DropdownMenuItem(
+                                text = { Text(converter.title()) },
+                                onClick = {
+                                    currentIndex.value = index
+                                    openChooser.value = false
+                                }
+                            )
                         }
-                    )
+                    }
                 }
             }
-        }
 
-        TwoValueConverterBox(converters[currentIndex.value])
+
+            TwoValueConverterBox(converters[currentIndex.value])
+        }
     }
 }
