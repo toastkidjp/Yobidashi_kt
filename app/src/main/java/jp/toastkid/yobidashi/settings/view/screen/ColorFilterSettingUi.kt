f3aeadb409ba3aeebd58cc1a238c3371730b4475
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-14 08:05:28 +0900

    refactor #289 Add spacer.

--- app/src/main/java/jp/toastkid/yobidashi/settings/view/screen/ColorFilterSettingUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/view/screen/ColorFilterSettingUi.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -172,6 +174,8 @@ internal fun ColorFilterSettingUi() {
                     end = 16.dp
                 )
             )
+
+            Spacer(modifier = Modifier.height(200.dp))
         }
     }
 }
