bd1196014fe1325f7bce59dc0d87fb0d5d85b3b8
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-15 08:38:58 +0900

    refactor #853 Delete useless import.

--- app/src/main/java/jp/toastkid/yobidashi/browser/bookmark/view/BookmarkListUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/bookmark/view/BookmarkListUi.kt
@@ -44,7 +44,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
