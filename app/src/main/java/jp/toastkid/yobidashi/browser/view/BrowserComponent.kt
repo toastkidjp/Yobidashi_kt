20de702b5474977e66d912bd42f77a2aec72a3cd
Toast Kid <super_q1m@yahoo.co.jp>
2023-05-24 09:19:08 +0900

    refactor #853 Delete useless local variable.

--- app/src/main/java/jp/toastkid/yobidashi/browser/view/BrowserComponent.kt
+++ app/src/main/java/jp/toastkid/yobidashi/browser/view/BrowserComponent.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
@@ -26,8 +25,6 @@ internal fun BrowserTitle(
     progress: Int?,
     modifier: Modifier
 ) {
-    val context = LocalContext.current
-
     Column(
         modifier = modifier
     ) {
