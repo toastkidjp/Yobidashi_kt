71e88730b3d6879ba67cd6ced0274bade4b4999e
Toast Kid <super_q1m@yahoo.co.jp>
2023-06-03 10:02:45 +0900

    feature #993 Make return Holiday object.

--- app/src/main/java/jp/toastkid/yobidashi/calendar/service/japan/EquinoxDayCalculator.kt
+++ app/src/main/java/jp/toastkid/yobidashi/calendar/service/japan/EquinoxDayCalculator.kt
@@ -8,14 +8,26 @@
 
 package jp.toastkid.yobidashi.calendar.service.japan
 
+import jp.toastkid.yobidashi.calendar.model.holiday.Holiday
+
 class EquinoxDayCalculator {
 
-    fun calculateVernalEquinoxDay(year: Int): Int {
-        return (20.8431 + 0.242194 * (year - 1980)).toInt() - ((year - 1980) / 4)
+    fun calculateVernalEquinoxDay(year: Int): Holiday {
+        return Holiday(
+            "Vernal equinox day",
+            3,
+            (20.8431 + 0.242194 * (year - 1980)).toInt() - ((year - 1980) / 4),
+        "\uD83C\uDDEF\uD83C\uDDF5"
+        )
     }
 
-    fun calculateAutumnalEquinoxDay(year: Int): Int {
-        return (23.2488 + 0.242194 * (year - 1980)).toInt() - ((year - 1980) / 4)
+    fun calculateAutumnalEquinoxDay(year: Int): Holiday {
+        return Holiday(
+            "Autumnal equinox day",
+            9,
+            (23.2488 + 0.242194 * (year - 1980)).toInt() - ((year - 1980) / 4),
+            "\uD83C\uDDEF\uD83C\uDDF5"
+        )
     }
 
 }
