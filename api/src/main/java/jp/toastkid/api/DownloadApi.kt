a149a2b7af5c8e3fc327e9c255a17352d4af294c
Toast Kid <super_q1m@yahoo.co.jp>
2023-06-11 08:07:39 +0900

    refactor #523 Modify interface.

--- api/src/main/java/jp/toastkid/api/DownloadApi.kt
+++ api/src/main/java/jp/toastkid/api/DownloadApi.kt
@@ -9,15 +9,30 @@
 package jp.toastkid.api
 
 import jp.toastkid.api.lib.HttpClientFactory
+import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Request
+import okhttp3.Response
+import timber.log.Timber
+import java.io.IOException
+import java.io.InputStream
 
 class DownloadApi {
 
     private val httpClient = HttpClientFactory().withTimeout(3)
 
-    operator fun invoke(url: String, callback: Callback) {
-        httpClient.newCall(Request.Builder().url(url).get().build()).enqueue(callback)
+    operator fun invoke(url: String, callback: (InputStream?) -> Unit) {
+        httpClient.newCall(Request.Builder().url(url).get().build()).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                Timber.e(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                callback(response.body?.byteStream())
+            }
+
+        })
     }
 
 }
