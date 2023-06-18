5098b6e134e2b57cd243b5bab8c46ce9b5d30aeb
Toast Kid <super_q1m@yahoo.co.jp>
2023-06-19 07:56:58 +0900

    refactor #462 Move test class to lib module.

--- lib/src/test/java/jp/toastkid/lib/image/VectorToBitmapTest.kt
+++ lib/src/test/java/jp/toastkid/lib/image/VectorToBitmapTest.kt
@@ -6,7 +6,7 @@
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
  */
 
-package jp.toastkid.yobidashi.libs
+package jp.toastkid.lib.image
 
 import android.content.Context
 import android.graphics.Bitmap
