2d22a801f1496785d83b5004a3853a71dd296d47
Toast Kid <super_q1m@yahoo.co.jp>
2023-06-19 07:51:36 +0900

    refactor #299 Fix incorrect unit test.

--- app/src/test/java/jp/toastkid/yobidashi/libs/clip/ClippingUrlOpenerTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/libs/clip/ClippingUrlOpenerTest.kt
@@ -8,16 +8,19 @@
 
 package jp.toastkid.yobidashi.libs.clip
 
-import android.content.Context
-import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.ViewModelProvider
 import io.mockk.MockKAnnotations
+import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.mockkObject
 import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.Urls
 import jp.toastkid.lib.clip.Clipboard
 import jp.toastkid.lib.preference.PreferenceApplier
@@ -29,19 +32,18 @@ import org.junit.Test
 class ClippingUrlOpenerTest {
 
     private lateinit var clippingUrlOpener: ClippingUrlOpener
-    
-    @MockK
-    private lateinit var view: View
 
     @MockK
-    private lateinit var context: Context
+    private lateinit var context: ComponentActivity
+
+    @MockK
+    private lateinit var contentViewModel: ContentViewModel
 
     @Before
     fun setUp() {
         clippingUrlOpener = ClippingUrlOpener()
         
         MockKAnnotations.init(this)
-        every { view.context }.returns(context)
         every { context.getString(any(), any()) }.returns("Test message")
         every { context.getSharedPreferences(any(), any()) }.returns(mockk())
 
@@ -55,9 +57,17 @@ class ClippingUrlOpenerTest {
         every { Urls.isInvalidUrl(any()) }.returns(false)
 
         mockkConstructor(PreferenceApplier::class)
-        every { anyConstructed<PreferenceApplier>().colorPair() }.returns(mockk())
         every { anyConstructed<PreferenceApplier>().lastClippedWord() }.returns("last-clipped")
         every { anyConstructed<PreferenceApplier>().setLastClippedWord(any()) }.returns(Unit)
+
+        mockkConstructor(ViewModelProvider::class)
+        every { anyConstructed<ViewModelProvider>().get(ContentViewModel::class.java) }.returns(contentViewModel)
+        every { contentViewModel.snackWithAction(any(), any(), any()) }.just(Runs)
+
+        every { context.viewModelStore }.returns(mockk())
+        every { context.defaultViewModelProviderFactory }.returns(mockk())
+        every { context.defaultViewModelCreationExtras }.returns(mockk())
+        every { context.getString(any()) }.returns("jp.toastkid.yobidashi")
     }
 
     @After
@@ -69,7 +79,6 @@ class ClippingUrlOpenerTest {
     fun testParentViewIsNull() {
         clippingUrlOpener.invoke(null) { }
 
-        verify(exactly = 0) { view.getContext() }
         verify(exactly = 0) { anyConstructed<NetworkChecker>().isNotAvailable(any()) }
     }
 
@@ -79,7 +88,6 @@ class ClippingUrlOpenerTest {
 
         clippingUrlOpener.invoke(context) { }
 
-        verify(exactly = 1) { view.getContext() }
         verify(exactly = 1) { anyConstructed<NetworkChecker>().isNotAvailable(any()) }
         verify(exactly = 0) { Clipboard.getPrimary(any()) }
     }
@@ -90,7 +98,6 @@ class ClippingUrlOpenerTest {
 
         clippingUrlOpener.invoke(context) { }
 
-        verify(atLeast = 1) { view.getContext() }
         verify(exactly = 1) { anyConstructed<NetworkChecker>().isNotAvailable(any()) }
         verify(exactly = 1) { Clipboard.getPrimary(any()) }
         verify(exactly = 0) { Urls.isInvalidUrl(any()) }
@@ -103,7 +110,6 @@ class ClippingUrlOpenerTest {
 
         clippingUrlOpener.invoke(context) { }
 
-        verify(atLeast = 1) { view.getContext() }
         verify(exactly = 1) { anyConstructed<NetworkChecker>().isNotAvailable(any()) }
         verify(exactly = 1) { Clipboard.getPrimary(any()) }
         verify(exactly = 1) { Urls.isInvalidUrl(any()) }
@@ -116,14 +122,12 @@ class ClippingUrlOpenerTest {
 
         clippingUrlOpener.invoke(context) { }
 
-        verify(atLeast = 1) { view.getContext() }
         verify(exactly = 1) { anyConstructed<NetworkChecker>().isNotAvailable(any()) }
         verify(exactly = 1) { Clipboard.getPrimary(any()) }
         verify(exactly = 1) { Urls.isInvalidUrl(any()) }
         verify(exactly = 1) { context.getString(any(), any()) }
         verify(exactly = 1) { anyConstructed<PreferenceApplier>().lastClippedWord() }
         verify(exactly = 1) { anyConstructed<PreferenceApplier>().setLastClippedWord(any()) }
-        verify(exactly = 1) { anyConstructed<PreferenceApplier>().colorPair() }
     }
 
 }
