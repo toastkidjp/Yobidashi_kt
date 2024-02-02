3223f329bdf4bb27e20a7f628618968f3bcfaf82
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-02 10:36:06 +0900

    feature #974 Update Compose to 1.6.0.

--- search/build.gradle.kts
+++ search/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(project(":lib"))
     implementation(project(":ui"))
 
-    implementation("androidx.compose.ui:ui:1.4.2")
+    implementation("androidx.compose.ui:ui:1.6.0")
     implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("io.coil-kt:coil-compose:${LibraryVersion.coilCompose}")
