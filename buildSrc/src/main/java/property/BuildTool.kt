189219aaf45b5aa08e679ef1519c847718bef6d8
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-02 11:02:57 +0900

    refactor #853 Upgrade build tool versions to 34.

--- buildSrc/src/main/java/property/BuildTool.kt
+++ buildSrc/src/main/java/property/BuildTool.kt
@@ -2,8 +2,8 @@ package property
 
 object BuildTool {
     val minSdk = 24 
-    val compileSdk =  33 
-    val targetSdk =  33 
-    val buildTools =  "33.0.0" 
+    val compileSdk =  34
+    val targetSdk =  34
+    val buildTools =  "34.0.0"
     val kotlinVersion =  "1.8.20"
 }
