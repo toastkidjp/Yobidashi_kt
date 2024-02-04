b7d988303d8e4f2ac45a32f9dfe413d4d435b35f
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-04 18:10:36 +0900

    refactor #853 Delete useless import.

--- app/src/main/java/jp/toastkid/yobidashi/main/ui/MainUi.kt
+++ app/src/main/java/jp/toastkid/yobidashi/main/ui/MainUi.kt
@@ -42,7 +42,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
