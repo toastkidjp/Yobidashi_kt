3e9c15e9ce768b9cd2dde2783fbaa4535721742f
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-14 08:36:24 +0900

    feature #580 Add function for clicking link.

--- markdown/src/main/java/jp/toastkid/markdown/presentation/TextLineViewModel.kt
+++ markdown/src/main/java/jp/toastkid/markdown/presentation/TextLineViewModel.kt
@@ -11,10 +11,11 @@ package jp.toastkid.markdown.presentation
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
+import jp.toastkid.markdown.domain.service.LinkBehaviorService
 import jp.toastkid.markdown.domain.service.LinkGenerator
 import jp.toastkid.ui.text.KeywordHighlighter
 
-class TextLineViewModel {
+class TextLineViewModel(private val linkBehaviorService: LinkBehaviorService) {
 
     private val lastLayoutResult = mutableStateOf<TextLayoutResult?>(null)
 
@@ -35,4 +36,16 @@ class TextLineViewModel {
     fun putLayoutResult(layoutResult: TextLayoutResult) {
         lastLayoutResult.value = layoutResult
     }
+
+    fun onClick(it: Int) {
+        //val textLayoutResult = lastLayoutResult.value ?: return
+
+        val stringRange = annotatedString
+            .value
+            .getStringAnnotations(tag = "URL", start = it, end = it)
+            .firstOrNull() ?: return
+
+        linkBehaviorService.invoke(stringRange.item)
+    }
+
 }
