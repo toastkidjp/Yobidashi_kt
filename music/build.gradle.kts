b819657e652ee9c674fe13c7f313fbfa8e41335e
Toast Kid <super_q1m@yahoo.co.jp>
2024-07-01 08:39:37 +0900

    refactor #853 Use version.

--- music/build.gradle.kts
+++ music/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
     implementation("io.coil-kt:coil-compose:${LibraryVersion.coilCompose}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${LibraryVersion.lifecycle}")
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.activity:activity-compose:${LibraryVersion.activityCompose}")
 
     implementation("androidx.media:media:1.3.0")
 
