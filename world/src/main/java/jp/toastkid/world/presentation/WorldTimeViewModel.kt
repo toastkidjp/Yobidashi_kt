aa6a668185a512a5b054cfd69d4e24af503b3f24
Toast Kid <super_q1m@yahoo.co.jp>
2024-07-01 08:37:40 +0900

    feature #1048 Add some pickup timezone.

--- world/src/main/java/jp/toastkid/world/presentation/WorldTimeViewModel.kt
+++ world/src/main/java/jp/toastkid/world/presentation/WorldTimeViewModel.kt
@@ -35,6 +35,12 @@ class WorldTimeViewModel {
         "Australia/Sydney",
         "NZ",
         "Asia/Ho_Chi_Minh",
+        "Asia/Ulaanbaatar",
+        "Asia/Tbilisi",
+        "Africa/Johannesburg",
+        "America/Asuncion",
+        "America/Buenos_Aires",
+        "Pacific/Palau",
         "IST"
     )
 
@@ -61,11 +67,17 @@ class WorldTimeViewModel {
             "UTC" -> "UTC"
             "America/New_York" -> "\uD83C\uDDFA\uD83C\uDDF8 New York"
             "US/Pacific" -> "\uD83C\uDDFA\uD83C\uDDF8 US Pacific"
-            "US/Hawaii" -> "Hawaii"
+            "US/Hawaii" -> "\uD83C\uDDFA\uD83C\uDDF8 Hawaii"
             "Europe/Rome" -> "\uD83C\uDDEE\uD83C\uDDF9 Rome"
             "Australia/Sydney" -> "\uD83C\uDDE6\uD83C\uDDFA Sydney"
             "NZ" -> "\uD83C\uDDF3\uD83C\uDDFF New Zealand"
             "Asia/Ho_Chi_Minh" -> "\uD83C\uDDFB\uD83C\uDDF3 Ho Chi Minh"
+            "Asia/Ulaanbaatar" -> "\uD83C\uDDF2\uD83C\uDDF3 Ulaanbaatar"
+            "Asia/Tbilisi" -> "\uD83C\uDDEC\uD83C\uDDEA Tbilisi"
+            "Africa/Johannesburg" -> "\uD83C\uDDFF\uD83C\uDDE6 Johannesburg"
+            "America/Asuncion" -> "\uD83C\uDDF5\uD83C\uDDFE Asuncion"
+            "America/Buenos_Aires" -> "\uD83C\uDDE6\uD83C\uDDF7 Buenos Aires"
+            "Pacific/Palau" -> "\uD83C\uDDF5\uD83C\uDDFC Palau"
             else -> timeZoneId
         }
     }
