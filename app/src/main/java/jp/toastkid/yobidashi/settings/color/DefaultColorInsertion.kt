4d881b9fa9ee2b155522df9988e8168314c3a2dd
Toast Kid <super_q1m@yahoo.co.jp>
2024-07-21 06:53:44 +0900

    refactor #853 Modify resource reference.

--- app/src/main/java/jp/toastkid/yobidashi/settings/color/DefaultColorInsertion.kt
+++ app/src/main/java/jp/toastkid/yobidashi/settings/color/DefaultColorInsertion.kt
@@ -21,25 +21,25 @@ import kotlinx.coroutines.launch
 class DefaultColorInsertion {
 
     private val map = mapOf(
-            R.color.colorPrimaryDark to R.color.textPrimary,
-            R.color.colorPrimaryDark to R.color.deep_yellow,
-            R.color.crimson_bg to R.color.crimson_font,
-            R.color.dark_brown to R.color.pink,
-            R.color.cyan_bg to R.color.white,
-            R.color.red_200_dd to R.color.black,
-            R.color.yellow_200_dd to R.color.black,
+            jp.toastkid.lib.R.color.colorPrimaryDark to R.color.textPrimary,
+        jp.toastkid.lib.R.color.colorPrimaryDark to jp.toastkid.lib.R.color.deep_yellow,
+        jp.toastkid.lib.R.color.crimson_bg to jp.toastkid.lib.R.color.crimson_font,
+        jp.toastkid.lib.R.color.dark_brown to jp.toastkid.lib.R.color.pink,
+        jp.toastkid.lib.R.color.cyan_bg to R.color.white,
+        jp.toastkid.lib.R.color.red_200_dd to R.color.black,
+        jp.toastkid.lib.R.color.yellow_200_dd to R.color.black,
             R.color.black to R.color.textPrimary,
-            R.color.black to R.color.deep_yellow,
-            R.color.indigo_200_dd to R.color.black,
+            R.color.black to jp.toastkid.lib.R.color.deep_yellow,
+        jp.toastkid.lib.R.color.indigo_200_dd to R.color.black,
             R.color.white to R.color.black,
-            R.color.pinky to R.color.white,
-            R.color.lime_bg to R.color.white,
-            R.color.purple_bg to R.color.white,
-            R.color.wa_bg to R.color.wa_font,
-            R.color.light_blue_200_dd to R.color.white,
-            R.color.teal_500_dd to R.color.white,
-            R.color.gray_500_dd to R.color.white,
-            R.color.deep_orange_500_dd to R.color.white
+        jp.toastkid.lib.R.color.pinky to R.color.white,
+        jp.toastkid.lib.R.color.lime_bg to R.color.white,
+        jp.toastkid.lib.R.color.purple_bg to R.color.white,
+        jp.toastkid.lib.R.color.wa_bg to jp.toastkid.lib.R.color.wa_font,
+        jp.toastkid.lib.R.color.light_blue_200_dd to R.color.white,
+        jp.toastkid.lib.R.color.teal_500_dd to R.color.white,
+        jp.toastkid.lib.R.color.gray_500_dd to R.color.white,
+        jp.toastkid.lib.R.color.deep_orange_500_dd to R.color.white
     )
 
     /**
