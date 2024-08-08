f0bb8ccd8150baa1e955aed462be5f8701db0027
Toast Kid <super_q1m@yahoo.co.jp>
2024-08-09 07:09:13 +0900

    refactor #853 Remove useless test function.

--- app/src/test/java/jp/toastkid/yobidashi/main/StartUpTest.kt
+++ app/src/test/java/jp/toastkid/yobidashi/main/StartUpTest.kt
@@ -1,6 +1,6 @@
 package jp.toastkid.yobidashi.main
 
-import jp.toastkid.yobidashi.R
+import jp.toastkid.lib.model.tab.StartUp
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -21,14 +21,6 @@ class StartUpTest {
         assertEquals(StartUp.SEARCH, StartUp.findByName("SEARCH"))
         assertEquals(StartUp.BROWSER, StartUp.findByName("BROWSER"))
         assertEquals(StartUp.BOOKMARK, StartUp.findByName("BOOKMARK"))
-    }
-
-    /**
-     * Check of [StartUp#findById].
-     */
-    @Test
-    fun testGetRadioButtonId() {
-        assertEquals(StartUp.BROWSER, StartUp.findById(R.id.start_up_browser))
     }
 
 }
