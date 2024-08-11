b260d9facbb3b807deee407c3c1889e6e9a27822
Toast Kid <super_q1m@yahoo.co.jp>
2024-08-12 06:59:54 +0900

    refactor #998 Delete useless state.

--- converter/src/main/java/jp/toastkid/converter/presentation/ui/TwoValueConverterBox.kt
+++ converter/src/main/java/jp/toastkid/converter/presentation/ui/TwoValueConverterBox.kt
@@ -11,7 +11,6 @@ package jp.toastkid.converter.presentation.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,7 +33,7 @@ fun TwoValueConverterBox(unixTimeConverterService: TwoStringConverter) {
     ) {
         val firstInput = remember { mutableStateOf(TextFieldValue(unixTimeConverterService.defaultFirstInputValue())) }
         val secondInput = remember { mutableStateOf(TextFieldValue(unixTimeConverterService.defaultSecondInputValue())) }
-        val result = remember { mutableStateOf("") }
+
         Row {
             Column {
                 TextField(
