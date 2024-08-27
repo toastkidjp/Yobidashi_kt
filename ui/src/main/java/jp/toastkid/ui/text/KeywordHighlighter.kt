94c033da20add9d457f3a002156ae0bf2fc086bb
Toast Kid <super_q1m@yahoo.co.jp>
2024-08-27 09:36:03 +0900

    bugfix #580 Fix incorrect link conversion.

--- ui/src/main/java/jp/toastkid/ui/text/KeywordHighlighter.kt
+++ ui/src/main/java/jp/toastkid/ui/text/KeywordHighlighter.kt
@@ -22,6 +22,7 @@ class KeywordHighlighter {
 
     operator fun invoke(text: String, finderTarget: String? = null) = buildAnnotatedString {
         var lastIndex = 0
+        var matcherEnd = 0
         val matcher = internalLinkPattern.matcher(text)
         while (matcher.find()) {
             val title = matcher.group(1)
@@ -50,8 +51,10 @@ class KeywordHighlighter {
                 start = annotateStart,
                 end = annotateStart + title.length
             )
-            lastIndex = if (matcher.find()) annotateStart else endIndex
+            lastIndex = matcher.end()
+            matcherEnd = matcher.end()
         }
+        lastIndex = matcherEnd
 
         if (lastIndex >= text.length) {
             return@buildAnnotatedString
