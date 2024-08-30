537b245a5a50a1c7761a5e99f7e0f93a687c2190
Toast Kid <super_q1m@yahoo.co.jp>
2024-08-30 09:42:22 +0900

    feature #1039 Add safety settings.

--- chat/src/main/java/jp/toastkid/chat/domain/model/Chat.kt
+++ chat/src/main/java/jp/toastkid/chat/domain/model/Chat.kt
@@ -29,7 +29,15 @@ data class Chat(private val texts: MutableList<ChatMessage> = mutableStateListOf
             {
                 "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
                 "threshold": "BLOCK_ONLY_HIGH"
-            }
+            },
+                        {
+                            "category": "HARM_CATEGORY_HARASSMENT",
+                            "threshold": "BLOCK_ONLY_HIGH"
+                        },
+                        {
+                            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                            "threshold": "BLOCK_ONLY_HIGH"
+                        }
         ]
       }
     """.trimIndent()
