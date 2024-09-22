f29001ff8ec86debf5498543ca5b1be17402722b
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-23 07:17:16 +0900

    refactor #993 Rename to UPPER_CASE.

--- buildSrc/src/main/java/property/Version.kt
+++ buildSrc/src/main/java/property/Version.kt
@@ -2,17 +2,17 @@ package property
 
 object Version {
     private const val MAJOR  = 2
-    private const val middle = 0
+    private const val MIDDLE = 0
     private const val minor  = 73
 
     init {
-        if (middle >= 100 || minor >= 10_000) {
+        if (MIDDLE >= 100 || minor >= 10_000) {
             throw IllegalStateException("Middle and Minor version value is allowed under 100.")
         }
     }
 
-    val code = ((MAJOR * 1_000_000) + (middle * 10_000) + minor)
+    val code = ((MAJOR * 1_000_000) + (MIDDLE * 10_000) + minor)
 
-    val name = "${MAJOR}.${middle}.${minor}"
+    val name = "${MAJOR}.${MIDDLE}.${minor}"
 
 }
