2f93218ca4ce030e646da0ce635e62f2c8a091a1
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-24 08:26:20 +0900

    refactor #853 Modify dependencies.

--- web/src/main/java/jp/toastkid/web/archive/view/ArchveListUi.kt
+++ web/src/main/java/jp/toastkid/web/archive/view/ArchveListUi.kt
@@ -29,13 +29,13 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.view.list.ListActionAttachment
