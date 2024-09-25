794cbb4c74cd8c7ef181d38688f36e82f162e56f
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-25 09:13:44 +0900

    refactor #853 Modify dependencies.

--- about/src/main/java/jp/toastkid/about/view/AboutThisAppUi.kt
+++ about/src/main/java/jp/toastkid/about/view/AboutThisAppUi.kt
@@ -29,7 +29,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -37,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import jp.toastkid.about.R
 import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.intent.GooglePlayIntentFactory
