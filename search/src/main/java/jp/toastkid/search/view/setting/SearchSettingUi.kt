b6332ac8887f9cff9315e72a352cc7b2eae4ab90
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-26 09:32:14 +0900

    refactor #853 Modify dependencies.

--- search/src/main/java/jp/toastkid/search/view/setting/SearchSettingUi.kt
+++ search/src/main/java/jp/toastkid/search/view/setting/SearchSettingUi.kt
@@ -29,12 +29,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import jp.toastkid.lib.ContentViewModel
