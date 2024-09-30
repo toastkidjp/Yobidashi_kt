b0ba415e260063280e7d8b439fff459fc66417d8
Toast Kid <super_q1m@yahoo.co.jp>
2024-10-01 08:55:34 +0900

    feature #853 Upgrade Android gradle plugin to 8.6.1.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
     `kotlin-dsl`
 }
 dependencies {
-    implementation("com.android.tools.build:gradle:8.5.1")
+    implementation("com.android.tools.build:gradle:8.6.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
 }
