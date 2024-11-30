68918a77fa859b2003d501a9128734eb39f0d896
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-30 10:13:02 +0900

    release #533 Upgrade to 2.0.78.

--- buildSrc/src/main/java/property/Version.kt
+++ buildSrc/src/main/java/property/Version.kt
@@ -3,7 +3,7 @@ package property
 object Version {
     private const val MAJOR  = 2
     private const val MIDDLE = 0
-    private const val MINOR  = 77
+    private const val MINOR  = 78
 
     init {
         if (MIDDLE >= 100 || MINOR >= 10_000) {
