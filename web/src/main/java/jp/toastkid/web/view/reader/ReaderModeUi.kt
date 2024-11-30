2b623466f9ff757e235b6675302738d7923862fd
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-30 09:57:14 +0900

    refactor #523 Keep instance.

--- web/src/main/java/jp/toastkid/web/view/reader/ReaderModeUi.kt
+++ web/src/main/java/jp/toastkid/web/view/reader/ReaderModeUi.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -35,7 +36,8 @@ import jp.toastkid.lib.preference.PreferenceApplier
 
 @Composable
 internal fun ReaderModeUi(title: String, text: String, onClose: () -> Unit) {
-    val preferenceApplier = PreferenceApplier(LocalContext.current)
+    val context = LocalContext.current
+    val preferenceApplier = remember { PreferenceApplier(context) }
 
     val scrollState = rememberScrollState()
 
