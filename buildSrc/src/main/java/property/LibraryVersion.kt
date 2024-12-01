4c7e71f07e79e871be3c6fe25e86a983cdf50c05
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-01 09:15:28 +0900

    feature #506 Upgrade Coil-Compose to 2.7.0.

--- buildSrc/src/main/java/property/LibraryVersion.kt
+++ buildSrc/src/main/java/property/LibraryVersion.kt
@@ -12,7 +12,7 @@ object LibraryVersion {
     val composeCompiler = "1.4.6"
     val composeMaterial3 = "1.3.0"
     val navigationCompose = "2.7.7"
-    val coilCompose = "2.0.0-rc01"
+    val coilCompose = "2.7.0"
     val activityCompose = "1.9.0"
     val junit = "4.12"
     val robolectric = "4.8.1"
