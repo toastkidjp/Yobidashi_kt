c2e1012019d238aa03bd1ced7d890759112f01b6
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-02 09:18:34 +0900

    feature #853 Upgrade coil dependencies.

--- markdown/build.gradle.kts
+++ markdown/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation(project(":ui"))
     implementation("androidx.compose.material3:material3:${property.LibraryVersion.composeMaterial3}")
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("io.coil-kt:coil-compose:${property.LibraryVersion.coilCompose}")
+    implementation("io.coil-kt.coil3:coil-compose:${property.LibraryVersion.coilCompose}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${property.LibraryVersion.lifecycle}")
     implementation("androidx.activity:activity-compose:${property.LibraryVersion.activityCompose}")
 
