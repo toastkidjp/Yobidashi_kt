df5a3ecc8981fac012dd10b80952a8469205096b
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-04 00:06:38 +0900

    refactor #853 Fix import.

--- web/src/main/java/jp/toastkid/web/floating/view/FloatingPreview.kt
+++ web/src/main/java/jp/toastkid/web/floating/view/FloatingPreview.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.web.floating.FloatingPreviewViewModel
