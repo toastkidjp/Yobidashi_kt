868b50c821fbe3cb0e01ad149293fa0fb62fd30e
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-04 00:03:45 +0900

    refactor #853 Fix import.

--- search/src/main/java/jp/toastkid/search/view/setting/SearchSettingUi.kt
+++ search/src/main/java/jp/toastkid/search/view/setting/SearchSettingUi.kt
@@ -36,7 +36,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.search.R
