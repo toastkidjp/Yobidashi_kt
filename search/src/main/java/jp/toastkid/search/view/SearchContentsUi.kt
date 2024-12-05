82f1a9698baaff584503ad07fce090df2b3be156
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-06 00:02:29 +0900

    refactor #412 Remove opt-in marker.

--- search/src/main/java/jp/toastkid/search/view/SearchContentsUi.kt
+++ search/src/main/java/jp/toastkid/search/view/SearchContentsUi.kt
@@ -31,7 +31,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
@@ -57,7 +56,6 @@ import jp.toastkid.search.url_suggestion.ItemDeletionUseCase
 import jp.toastkid.search.viewmodel.SearchUiViewModel
 
 @OptIn(
-    ExperimentalComposeUiApi::class,
     ExperimentalFoundationApi::class,
     ExperimentalLayoutApi::class
 )
