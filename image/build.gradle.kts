3538cd2a1bb97c9290769a540d4b37901882829b
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-06 00:03:27 +0900

    refactor #853 Use to catalog.

--- image/build.gradle.kts
+++ image/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
 
     // Compose dependencies.
     implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
-    implementation("io.coil-kt.coil3:coil-compose:${LibraryVersion.coilCompose}")
+    implementation(libraries.coilCompose)
     implementation("io.coil-kt.coil3:coil-gif:${LibraryVersion.coilCompose}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${LibraryVersion.lifecycle}")
     implementation("androidx.activity:activity-compose:${LibraryVersion.activityCompose}")
