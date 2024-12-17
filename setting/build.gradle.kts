4ec064ba69446b03ae59686473ecd231ea7208c2
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-18 00:10:08 +0900

    refactor #853 Use catalog.

--- setting/build.gradle.kts
+++ setting/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(libraries.activityCompose)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${LibraryVersion.lifecycle}")
     implementation("androidx.exifinterface:exifinterface:${LibraryVersion.exifinterface}")
-    implementation("com.jakewharton.timber:timber:${LibraryVersion.timber}")
+    implementation(libraries.timber)
     implementation("com.godaddy.android.colorpicker:compose-color-picker:0.4.2")
 
     testImplementation("junit:junit:${property.LibraryVersion.junit}")
