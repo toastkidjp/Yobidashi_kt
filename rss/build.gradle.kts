07db5bf7dc35a846f1e39abfd2362120e0644671
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-25 00:01:52 +0900

    refactor #853 Use catalog.

--- rss/build.gradle.kts
+++ rss/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${LibraryVersion.coroutines}")
 
-    implementation("androidx.compose.material3:material3:${LibraryVersion.composeMaterial3}")
+    implementation(libraries.composeMaterial3)
     implementation(libraries.activityCompose)
 
     testImplementation("junit:junit:${LibraryVersion.junit}")
