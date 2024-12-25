a27da455a57b7ee8ce4211777ffa70bc3b105bcb
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-26 07:32:03 +0900

    refactor #853 Delete useless version.

--- buildSrc/src/main/java/property/LibraryVersion.kt
+++ buildSrc/src/main/java/property/LibraryVersion.kt
@@ -5,7 +5,6 @@ object LibraryVersion {
     val ktx = "1.1.0"
     val lifecycle = "2.8.5"
     val composeCompiler = "1.4.6"
-    val composeMaterial3 = "1.3.0"
     val junit = "4.12"
     val robolectric = "4.8.1"
     val mockk = "1.10.6"
