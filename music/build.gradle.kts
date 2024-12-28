cf2dfd2909956c17c53d0c54ec80043650296f6c
Toast Kid <super_q1m@yahoo.co.jp>
2024-12-28 11:13:36 +0900

    refactor #853 Use catalog.

--- music/build.gradle.kts
+++ music/build.gradle.kts
@@ -38,7 +38,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = LibraryVersion.composeCompiler
+        kotlinCompilerExtensionVersion = libraries.versions.kotlinComposeCompilerExtension.get()
     }
 }
 
