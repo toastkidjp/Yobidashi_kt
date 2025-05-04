2803afc084bd7c15c65941497b273075e315b2e1
toastkidjp <10fmi13@gmail.com>
2025-05-05 07:39:00 +0900

    refactor #512 Upgrade byte-buddy.

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     testImplementation("junit:junit:4.12")
     testImplementation("io.mockk:mockk:${LibraryVersion.mockk}")
     testImplementation("org.robolectric:robolectric:${LibraryVersion.robolectric}")
-    testImplementation("net.bytebuddy:byte-buddy:1.11.22")
+    testImplementation(testLibraries.bytebuddy)
 }
