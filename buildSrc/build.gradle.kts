ca4e61971b56e5f1af60fccc63afd2207c68a3f0
toastkidjp <10fmi13@gmail.com>
2025-05-16 06:55:36 +0900

    refactor #993 Upgrade AGP to 8.9.2.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
     `kotlin-dsl`
 }
 dependencies {
-    implementation("com.android.tools.build:gradle:8.7.1")
+    implementation("com.android.tools.build:gradle:8.9.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
 }
