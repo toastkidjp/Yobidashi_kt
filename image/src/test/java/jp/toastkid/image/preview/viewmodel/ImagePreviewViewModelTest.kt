03232c929fa0de4f0a077f39689838770b13aecc
toastkidjp <10fmi13@gmail.com>
2025-05-19 07:28:11 +0900

    refactor #506 Add test cases.

---
+++ image/src/test/java/jp/toastkid/image/preview/viewmodel/ImagePreviewViewModelTest.kt
@@ -0,0 +1,57 @@
+package jp.toastkid.image.preview.viewmodel
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import jp.toastkid.image.Image
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ImagePreviewViewModelTest {
+
+    private lateinit var subject: ImagePreviewViewModel
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        subject = ImagePreviewViewModel(0)
+
+        subject.replaceImages(
+            listOf(
+                Image("/path/to/image1.webp", "image1.webp"),
+                Image("/path/to/image2.webp", "image2.webp"),
+            )
+        )
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun pagerState() {
+        assertEquals(0, subject.pagerState().currentPage)
+    }
+
+    @Test
+    fun sharedElementKey() {
+        assertEquals("image_/path/to/image1.webp", subject.sharedElementKey(0))
+        assertEquals("image_/path/to/image2.webp_", subject.sharedElementKey(1))
+    }
+
+    @Test
+    fun getCurrentImage() {
+    }
+
+    @Test
+    fun getImage() {
+
+    }
+
+}
