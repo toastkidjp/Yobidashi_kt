a820832963360a35d9a49d1d41c4af328325a76d
toastkidjp <10fmi13@gmail.com>
2025-05-20 08:37:20 +0900

    refactor #990 Divide constant.

---
+++ calendar/src/main/java/jp/toastkid/calendar/model/color/CalendarColor.kt
@@ -0,0 +1,6 @@
+package jp.toastkid.calendar.model.color
+
+import androidx.compose.ui.graphics.Color
+
+internal val CALENDAR_OFF_DAY: Color = Color(220, 50, 55)
+internal val CALENDAR_SATURDAY: Color = Color(95, 90, 250)
