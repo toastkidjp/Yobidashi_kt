4bb95a72f8f84c3ba5c14d14028209a22b3e4e69
toastkidjp <10fmi13@gmail.com>
2025-06-19 08:52:46 +0900

    refactor #580 Optimize imports.

--- markdown/src/main/java/jp/toastkid/markdown/presentation/TableLineView.kt
+++ markdown/src/main/java/jp/toastkid/markdown/presentation/TableLineView.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
