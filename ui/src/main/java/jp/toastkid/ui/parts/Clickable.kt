e082178576968d6bec8831c0100bd3273db36b9d
toastkidjp <10fmi13@gmail.com>
2025-06-23 09:11:34 +0900

    refactor #853 Add new component.

--- ui/src/main/java/jp/toastkid/ui/parts/Clickable.kt
+++ ui/src/main/java/jp/toastkid/ui/parts/Clickable.kt
@@ -10,11 +10,16 @@ package jp.toastkid.ui.parts
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,4 +34,32 @@ fun SingleLineText(
         fontSize = 16.sp,
         modifier = Modifier.fillMaxWidth().padding(16.dp).clickable(onClick = onClick)
     )
+}
+
+@Composable
+fun WithIcon(
+    textId: Int,
+    clickable: () -> Unit,
+    iconTint: Color,
+    iconId: Int
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable(onClick = clickable)
+            .padding(16.dp)
+    ) {
+        Icon(
+            painterResource(id = iconId),
+            tint = iconTint,
+            contentDescription = stringResource(id = textId),
+            modifier = Modifier.padding(end = 4.dp)
+        )
+
+        Text(
+            stringResource(id = textId),
+            modifier = Modifier
+                .weight(1f)
+        )
+    }
 }
