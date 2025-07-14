13838cc82d921cd5ff1f91538bd8547fb1a9cc8c
toastkidjp <10fmi13@gmail.com>
2025-07-14 09:11:03 +0900

    feature #1048 Add components.

--- world/src/main/java/jp/toastkid/world/presentation/WorldTimeView.kt
+++ world/src/main/java/jp/toastkid/world/presentation/WorldTimeView.kt
@@ -8,26 +8,40 @@
 
 package jp.toastkid.world.presentation
 
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.world.domain.model.WorldTime
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WorldTimeView(modifier: Modifier = Modifier) {
+    val context = LocalContext.current as? ComponentActivity ?: return
+    val contentViewModel = viewModel(ContentViewModel::class.java, context)
     val viewModel = remember { WorldTimeViewModel() }
 
     Surface(
@@ -50,6 +64,102 @@ fun WorldTimeView(modifier: Modifier = Modifier) {
                     }
 
                     HorizontalDivider(modifier = Modifier.padding(horizontal = 8.dp))
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        contentViewModel.replaceAppBarContent {
+            Row {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.clickable {
+                        viewModel.openChooser()
+                    }.padding(end = 8.dp)
+                ) {
+                    Text(
+                        viewModel.currentTimezoneLabel()
+                    )
+                    DropdownMenu(
+                        viewModel.openingChooser(),
+                        viewModel::closeChooser
+                    ) {
+                        viewModel.pickupTimeZone().forEach {
+                            DropdownMenuItem(
+                                {
+                                    Text(viewModel.label(it))
+                                },
+                                {
+                                    viewModel.choose(it)
+                                }
+                            )
+                        }
+                    }
+                }
+
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.clickable {
+                        viewModel.openHourChooser()
+                    }
+                ) {
+                    Text(
+                        viewModel.currentHour(),
+                        fontSize = 24.sp
+                    )
+                    DropdownMenu(
+                        viewModel.openingHourChooser(),
+                        viewModel::closeHourChooser
+                    ) {
+                        (0..23).forEach {
+                            DropdownMenuItem(
+                                {
+                                    Text("$it",
+                                            fontSize = 24.sp,
+                                    )
+                                },
+                                {
+                                    viewModel.chooseHour(it)
+                                }
+                            )
+                        }
+                    }
+                }
+
+                Text(":",
+                    fontSize = 24.sp,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                            .align(Alignment.CenterVertically)
+                )
+
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.clickable {
+                        viewModel.openMinuteChooser()
+                    }
+                ) {
+                    Text(
+                        viewModel.currentMinute(),
+                        fontSize = 24.sp
+                        )
+                    DropdownMenu(
+                        viewModel.openingMinuteChooser(),
+                        viewModel::closeMinuteChooser
+                    ) {
+                        (0..59).forEach {
+                            DropdownMenuItem(
+                                {
+                                    Text("$it",
+                                        fontSize = 24.sp
+                                    )
+                                },
+                                {
+                                    viewModel.chooseMinute(it)
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
