ab3546f825e128da80cbb8a7e04ecc91c2d96ef3
toastkidjp <10fmi13@gmail.com>
2025-07-15 09:15:37 +0900

    refactor #1048 Merge effects.

--- world/src/main/java/jp/toastkid/world/presentation/WorldTimeView.kt
+++ world/src/main/java/jp/toastkid/world/presentation/WorldTimeView.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -163,9 +162,7 @@ fun WorldTimeView(modifier: Modifier = Modifier) {
                 }
             }
         }
-    }
 
-    SideEffect {
         viewModel.start()
     }
 }
