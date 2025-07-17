369af052b7dfbf4d5ad5cf679c9f9ef65e43a9a6
toastkidjp <10fmi13@gmail.com>
2025-07-18 07:35:08 +0900

    refactor #853 Delete useless option.

--- loan/build.gradle.kts
+++ loan/build.gradle.kts
@@ -35,9 +35,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = LibraryVersion.composeCompiler
-    }
 }
 
 dependencies {
