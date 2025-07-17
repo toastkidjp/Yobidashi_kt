84d2a3d12a44ffbe8d549f156077525ac77d0555
toastkidjp <10fmi13@gmail.com>
2025-07-17 09:38:05 +0900

    refactor #853 Delete useless option.

--- barcode/ui/build.gradle.kts
+++ barcode/ui/build.gradle.kts
@@ -35,10 +35,6 @@ android {
     buildFeatures {
         compose = true
     }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = LibraryVersion.composeCompiler
-    }
 }
 
 tasks.withType<GenerateBuildConfig> {
