ba0d98a9e462e9d127a0fc4afe5b8f6925c32c4b
toastkidjp <10fmi13@gmail.com>
2025-07-20 08:11:36 +0900

    refactor #853 Upgrade lifecycle to 2.9.2.

--- buildSrc/src/main/java/property/LibraryVersion.kt
+++ buildSrc/src/main/java/property/LibraryVersion.kt
@@ -3,7 +3,7 @@ package property
 object LibraryVersion {
     val coroutines = "1.5.1"
     val ktx = "1.1.0"
-    val lifecycle = "2.8.5"
+    val lifecycle = "2.9.2"
     val junit = "4.12"
     val robolectric = "4.14"
     val mockk = "1.10.6"
