e5568db7fa067226b7412edd63bbbc3f0f077a8d
toastkidjp <10fmi13@gmail.com>
2025-07-21 07:52:15 +0900

    refactor #1048 Optimize imports.

--- world/src/main/java/jp/toastkid/world/presentation/WorldTimeView.kt
+++ world/src/main/java/jp/toastkid/world/presentation/WorldTimeView.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
