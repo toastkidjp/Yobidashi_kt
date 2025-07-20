114268c39424f99e1b0786ae02a725a0402d4dd6
toastkidjp <10fmi13@gmail.com>
2025-07-21 07:57:22 +0900

    feature #853 Upgrade KTX to 1.16.0.

--- buildSrc/src/main/java/property/LibraryVersion.kt
+++ buildSrc/src/main/java/property/LibraryVersion.kt
@@ -2,7 +2,7 @@ package property
 
 object LibraryVersion {
     val coroutines = "1.5.1"
-    val ktx = "1.1.0"
+    val ktx = "1.16.0"
     val lifecycle = "2.9.2"
     val junit = "4.12"
     val robolectric = "4.14"
