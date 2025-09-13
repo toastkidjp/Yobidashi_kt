e114e57861e1dc685a9b73667af89a58122a7c2b
toastkidjp <10fmi13@gmail.com>
2025-09-13 11:09:58 +0900

    feature #853 Update CameraX to 1.5.0.

--- barcode/ui/build.gradle.kts
+++ barcode/ui/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libraries.activityCompose)
 
     implementation("androidx.camera:camera-camera2:1.0.2")
-    implementation("androidx.camera:camera-lifecycle:1.0.2")
+    implementation("androidx.camera:camera-lifecycle:1.5.0")
     implementation("androidx.camera:camera-view:1.5.0")
 
     implementation(libraries.timber)
