97789f3f1c3188d7bf2a9d894d734961f0e8ad61
toastkidjp <10fmi13@gmail.com>
2025-09-15 08:52:24 +0900

    feature #1039 Modify interface for enabling to pass model.

--- chat/src/main/java/jp/toastkid/chat/domain/service/ChatService.kt
+++ chat/src/main/java/jp/toastkid/chat/domain/service/ChatService.kt
@@ -2,10 +2,11 @@ package jp.toastkid.chat.domain.service
 
 import jp.toastkid.chat.domain.model.Chat
 import jp.toastkid.chat.domain.model.ChatMessage
+import jp.toastkid.chat.domain.model.GenerativeAiModel
 
 interface ChatService {
 
-    fun send(text: String): String?
+    fun send(model: GenerativeAiModel, text: String): String?
 
     fun setChat(chat: Chat)
 
