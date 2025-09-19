2e2fc852995f6235c7371b67a9b99feacbe7e94b
toastkidjp <10fmi13@gmail.com>
2025-09-19 09:39:02 +0900

    refactor #1039 Fix incorrect encoding.

--- chat/src/main/java/jp/toastkid/chat/presentation/MessageContentViewModel.kt
+++ chat/src/main/java/jp/toastkid/chat/presentation/MessageContentViewModel.kt
@@ -2,40 +2,61 @@ package jp.toastkid.chat.presentation
 
 import android.graphics.BitmapFactory
 import android.util.Base64
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.AnnotatedString
+import jp.toastkid.lib.image.BitmapScaling
 import jp.toastkid.ui.text.KeywordHighlighter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
-import java.util.concurrent.atomic.AtomicReference
 
 class MessageContentViewModel {
 
     private val keywordHighlighter = KeywordHighlighter()
 
-    private val imageHolder = AtomicReference<ImageBitmap>()
+    private val defaultImage = ImageBitmap(1, 1)
+
+    private val imageHolder = mutableStateOf<ImageBitmap>(defaultImage)
 
     fun lineText(listLine: Boolean, text: String): AnnotatedString {
         return keywordHighlighter(if (listLine) text.substring(2) else text)
     }
 
-    fun image(base64Image: String): ImageBitmap {
-        val current = imageHolder.get()
-        if (current != null) {
-            return current
-        }
-
-        val loadImage = loadImage(base64Image) ?: ImageBitmap(1, 1)
-        imageHolder.set(loadImage)
-
-        return loadImage
+    fun image(): ImageBitmap {
+        return imageHolder.value
     }
 
-    private fun loadImage(base64Image: String): ImageBitmap? {
+    suspend fun loadImage(base64Image: String) {
+        println("launch image $base64Image")
+        val loadImage = withContext(Dispatchers.IO) {
+            loadImageInternal(base64Image) ?: defaultImage
+        }
+        imageHolder.value = loadImage
+    }
+
+    private fun loadImageInternal(base64Image: String): ImageBitmap? {
+        println("start image")
         val stream = ByteArrayInputStream(
-            Base64.decode(base64Image, Base64.DEFAULT)
+            Base64.decode(base64Image, Base64.NO_WRAP)
         )
-        return stream.use(BitmapFactory::decodeStream)?.asImageBitmap()
+        println("stream image")
+        val bitmap = stream.use(BitmapFactory::decodeStream)
+        println("decoded image")
+        if (bitmap == null) {
+            return null
+        }
+        val scaled = BitmapScaling().invoke(bitmap, 360.0, 360.0)
+        println("sclaed image")
+        return scaled.asImageBitmap()
+    }
+
+    fun showImage(base64Image: String?): Boolean {
+        if (imageHolder.value == defaultImage) {
+            return false
+        }
+        return base64Image.isNullOrEmpty().not()
     }
 
 }
