d1bb8be0931d37f46f3a0938997e30b487b8d68d
toastkidjp <10fmi13@gmail.com>
2025-09-23 09:11:10 +0900

    feature #1039 Add store image menu.

--- chat/src/main/java/jp/toastkid/chat/presentation/MessageContent.kt
+++ chat/src/main/java/jp/toastkid/chat/presentation/MessageContent.kt
@@ -1,5 +1,9 @@
 package jp.toastkid.chat.presentation
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.os.Build
+import android.provider.MediaStore
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,7 +25,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jp.toastkid.lib.intent.BitmapShareIntentFactory
 import jp.toastkid.ui.image.EfficientImage
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @Composable
@@ -69,6 +75,29 @@ internal fun MessageContent(
                     viewModel.openingImageDropdownMenu(),
                     onDismissRequest = viewModel::closeImageDropdownMenu
                 ) {
+                    if (Build.VERSION_CODES.Q <= Build.VERSION.SDK_INT) {
+                        DropdownMenuItem(
+                            { Text("Store") },
+                            {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    val contentResolver = context.contentResolver
+                                    val values = ContentValues().also { contentValues ->
+                                        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+                                        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "chat_image_${System.currentTimeMillis()}.png")
+                                    }
+
+                                    val imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                                        ?: return@launch
+
+                                    contentResolver.openOutputStream(imageUri)?.use { outputStream ->
+                                        viewModel.image().compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                                        outputStream.flush()
+                                    }
+                                }
+                                viewModel.closeImageDropdownMenu()
+                            }
+                        )
+                    }
                     DropdownMenuItem(
                         { Text("Share") },
                         {
