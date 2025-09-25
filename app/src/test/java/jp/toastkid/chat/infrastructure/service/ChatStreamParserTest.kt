fa9f92d03da8c291eab26cd34fe0ebfdb3a06ab7
toastkidjp <10fmi13@gmail.com>
2025-09-25 09:43:38 +0900

    refactor #1039 Remove useless test case.

--- app/src/test/java/jp/toastkid/chat/infrastructure/service/ChatStreamParserTest.kt
+++ app/src/test/java/jp/toastkid/chat/infrastructure/service/ChatStreamParserTest.kt
@@ -61,16 +61,6 @@ class ChatStreamParserTest {
     }
 
     @Test
-    fun otherError() {
-        val item =
-            subject.invoke("data: {\"candidates\": [{\"finishReason\": \"OTHER\",\"index\": 0,\"safetyRatings\": [{\"category\": \"HARM_CATEGORY_SEXUALLY_EXPLICIT\",\"probability\": \"NEGLIGIBLE\"},{\"category\": \"HARM_CATEGORY_HATE_SPEECH\",\"probability\": \"LOW\"},{\"category\": \"HARM_CATEGORY_HARASSMENT\",\"probability\": \"LOW\"},{\"category\": \"HARM_CATEGORY_DANGEROUS_CONTENT\",\"probability\": \"NEGLIGIBLE\"}]}],\"usageMetadata\": {\"promptTokenCount\": 1996,\"totalTokenCount\": 1996}}\n") ?: return fail()
-
-        assertEquals("[OTHER]", item.message())
-        assertTrue(item.error())
-        assertFalse(item.image())
-    }
-
-    @Test
     fun image() {
         val item =
             subject.invoke("data: {\"candidates\": [{\"content\": {\"parts\": [{\"inlineData\": {\"mimeType\": \"image/png\",\"data\": \"base64encoded-image\" } }],\"role\": \"model\"},\"index\": 0}],\"usageMetadata\": {\"promptTokenCount\": 8,\"candidatesTokenCount\": 1341,\"totalTokenCount\": 1349,\"promptTokensDetails\": [{\"modality\": \"TEXT\",\"tokenCount\": 8}],\"candidatesTokensDetails\": [{\"modality\": \"IMAGE\",\"tokenCount\": 1290}]}}") ?: return fail()
