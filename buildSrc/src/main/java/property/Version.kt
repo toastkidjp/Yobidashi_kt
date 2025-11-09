423279fef0419d167ba9c3f4614e0699e9366bba
toastkidjp <10fmi13@gmail.com>
2025-11-09 09:46:46 +0900

    release #533 Upgrade to 2.1.2.

--- buildSrc/src/main/java/property/Version.kt
+++ buildSrc/src/main/java/property/Version.kt
@@ -3,7 +3,7 @@ package property
 object Version {
     private const val MAJOR  = 2
     private const val MIDDLE = 1
-    private const val MINOR  = 1
+    private const val MINOR  = 2
 
     init {
         if (MIDDLE >= 100 || MINOR >= 10_000) {
