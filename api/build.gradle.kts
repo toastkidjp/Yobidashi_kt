cd72e97eed1c977998f3068c0abb400f3f67f2a5
toastkidjp <10fmi13@gmail.com>
2025-11-30 18:43:47 +0900

    feature #993 Upgrade OkHttp to 5.3.2.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:${LibraryVersion.ktx}")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
     implementation(libraries.timber)
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation(libraries.jsoup)
