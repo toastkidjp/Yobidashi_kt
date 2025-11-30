7fb0b9eea24d5dd935a0c118676fa0b72515165a
toastkidjp <10fmi13@gmail.com>
2025-11-30 19:46:35 +0900

    feature #853 Upgrade Kotlin Gradle Plugin to 2.2.20.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,5 +7,5 @@ plugins {
 }
 dependencies {
     implementation("com.android.tools.build:gradle:8.9.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
 }
