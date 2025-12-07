0c1ab80df2f419d689e32252e70e16d0278456ba
toastkidjp <10fmi13@gmail.com>
2025-12-07 09:46:58 +0900

    refactor #853 Safe delete 'lifecycle'.

--- buildSrc/src/main/java/property/LibraryVersion.kt
+++ buildSrc/src/main/java/property/LibraryVersion.kt
@@ -3,7 +3,6 @@ package property
 object LibraryVersion {
     val coroutines = "1.5.1"
     val ktx = "1.16.0"
-    val lifecycle = "2.10.0"
     val junit = "4.12"
     val robolectric = "4.14"
     val mockk = "1.14.6"
