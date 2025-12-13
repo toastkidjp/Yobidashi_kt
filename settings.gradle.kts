4d5ea296ada9365959cc2a15aa6bc4cd92f770f5
toastkidjp <10fmi13@gmail.com>
2025-12-13 10:55:01 +0900

    refactor #853 Add version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,6 +38,7 @@ dependencyResolutionManagement {
         }
 
         create("testLibraries") {
+            library("junit", "junit", "junit").version("4.12")
             library("bytebuddy", "net.bytebuddy", "byte-buddy").version("1.18.2")
             library("robolectric", "org.robolectric", "robolectric").version("4.14")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-test").version("1.5.1")
