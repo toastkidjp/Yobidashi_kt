cce17cfa1a4c24e0a06b7186bb3050f977fcd3ce
toastkidjp <10fmi13@gmail.com>
2025-12-19 08:18:23 +0900

    refactor #853 Use version.

--- number/build.gradle.kts
+++ number/build.gradle.kts
@@ -7,7 +7,6 @@
  */
 import com.android.build.gradle.tasks.GenerateBuildConfig
 import property.BuildTool
-import property.LibraryVersion
 
 plugins {
     id("com.android.library")
@@ -63,6 +62,6 @@ dependencies {
     implementation(libraries.kotlinSerialization)
 
     testImplementation(testLibraries.junit)
-    testImplementation("io.mockk:mockk:${LibraryVersion.mockk}")
+    testImplementation(testLibraries.mockK)
     testImplementation(testLibraries.bytebuddy)
 }
