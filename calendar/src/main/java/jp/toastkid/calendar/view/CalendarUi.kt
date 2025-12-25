ec4c76e5dff23a3f652efabd6a745f632f2f2d52
toastkidjp <10fmi13@gmail.com>
2025-12-25 11:21:11 +0900

    refactor #990 Remove useless import.

--- calendar/src/main/java/jp/toastkid/calendar/view/CalendarUi.kt
+++ calendar/src/main/java/jp/toastkid/calendar/view/CalendarUi.kt
@@ -42,7 +42,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
