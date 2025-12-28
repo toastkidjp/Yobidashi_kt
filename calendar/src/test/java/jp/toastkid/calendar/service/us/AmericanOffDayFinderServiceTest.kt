f383c3c270f349abbad9654c7e890e2b5d96f513
toastkidjp <10fmi13@gmail.com>
2025-12-28 10:29:39 +0900

    refactor #990 Add test case.

--- calendar/src/test/java/jp/toastkid/calendar/service/us/AmericanOffDayFinderServiceTest.kt
+++ calendar/src/test/java/jp/toastkid/calendar/service/us/AmericanOffDayFinderServiceTest.kt
@@ -20,5 +20,6 @@ class AmericanOffDayFinderServiceTest {
         assertEquals(0, subject.invoke(2025, 3).size)
         assertEquals(0, subject.invoke(2025, 4).size)
         assertEquals(1, subject.invoke(2025, 5).size)
+        assertEquals(1, subject.invoke(2025, 6).size)
     }
 }
