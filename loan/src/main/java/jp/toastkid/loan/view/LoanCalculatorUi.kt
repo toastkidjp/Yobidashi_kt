f5721b426895d9e638e5bb298a880cbd885284a1
toastkidjp <10fmi13@gmail.com>
2025-12-29 12:18:41 +0900

    refactor #818 Wrap too long line.

--- loan/src/main/java/jp/toastkid/loan/view/LoanCalculatorUi.kt
+++ loan/src/main/java/jp/toastkid/loan/view/LoanCalculatorUi.kt
@@ -181,7 +181,10 @@ fun LoanCalculatorUi() {
                         stringResource(R.string.title_column_loan_payment_count),
                         modifier = Modifier.weight(0.7f)
                     )
-                    Text(stringResource(R.string.title_column_principal), modifier = Modifier.weight(1f))
+                    Text(
+                        stringResource(R.string.title_column_principal),
+                        modifier = Modifier.weight(1f)
+                    )
                     Text(stringResource(R.string.title_column_interest), modifier = Modifier.weight(1f))
                     Text(stringResource(R.string.title_column_balance), modifier = Modifier.weight(1f))
                 }
