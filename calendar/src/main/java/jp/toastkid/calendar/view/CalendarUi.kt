dbf6f2f1e1cce72553acf73429f34cc1e7ec8b92
toastkidjp <10fmi13@gmail.com>
2025-12-30 09:42:01 +0900

    feature #990 Modify year chooser's tap area.

--- calendar/src/main/java/jp/toastkid/calendar/view/CalendarUi.kt
+++ calendar/src/main/java/jp/toastkid/calendar/view/CalendarUi.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -127,7 +128,6 @@ fun CalendarUi() {
                                             "$it",
                                             fontSize = 16.sp,
                                             modifier = Modifier
-                                                .padding(8.dp)
                                                 .clickable {
                                                     val fromPage =
                                                         viewModel.fromPage(pagerState.currentPage)
@@ -138,7 +138,10 @@ fun CalendarUi() {
                                                         )
                                                     }
                                                     openYearChooser.value = false
-                                                })
+                                                }
+                                                .fillMaxWidth()
+                                                .padding(8.dp)
+                                        )
                                     }
                                 }
                             }
