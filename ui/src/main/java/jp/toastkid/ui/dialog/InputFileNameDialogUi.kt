0318e7521f3927183451295da1e989e5f640c551
toastkidjp <10fmi13@gmail.com>
2026-01-18 09:06:04 +0900

    refactor #853 Use local drawable.

--- ui/src/main/java/jp/toastkid/ui/dialog/InputFileNameDialogUi.kt
+++ ui/src/main/java/jp/toastkid/ui/dialog/InputFileNameDialogUi.kt
@@ -11,8 +11,6 @@ package jp.toastkid.ui.dialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import jp.toastkid.ui.R
@@ -57,7 +56,7 @@ fun InputFileNameDialogUi(
                 ),
                 trailingIcon = {
                     Icon(
-                        Icons.Filled.Clear,
+                        painter = painterResource(jp.toastkid.lib.R.drawable.ic_clear_form),
                         contentDescription = "clear text",
                         modifier = Modifier
                             .offset(x = 8.dp)
