7b5d9c886885bf44ee88f07f0e1e7559ae63e865
toastkidjp <10fmi13@gmail.com>
2026-02-02 10:48:09 +0900

    refactor #511 Add license.

--- editor/src/main/java/jp/toastkid/editor/view/menu/ContextMenuBuilder.kt
+++ editor/src/main/java/jp/toastkid/editor/view/menu/ContextMenuBuilder.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.editor.view.menu
 
 import androidx.compose.foundation.text.contextmenu.builder.TextContextMenuBuilderScope
