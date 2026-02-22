a179b0425c802c6466516278e30e49309f3ca286
toastkidjp <10fmi13@gmail.com>
2026-02-22 09:58:33 +0900

    feature #1039 Update models.

--- chat/src/main/java/jp/toastkid/chat/domain/model/GenerativeAiModel.kt
+++ chat/src/main/java/jp/toastkid/chat/domain/model/GenerativeAiModel.kt
@@ -4,35 +4,37 @@ enum class GenerativeAiModel(
     private val label: String,
     private val urlParameter: String,
     private val versionPath: String,
+    private val webGrounding: Boolean = false,
     private val image: Boolean = false
 ) {
 
+    GEMINI_3_0_FLASH(
+        "Gemini 3 Flash",
+        "gemini-3-flash-preview",
+        "v1beta"
+    ),
     GEMINI_2_5_FLASH(
         "Gemini 2.5 Flash",
         "gemini-2.5-flash",
-        "v1"
+        "v1beta",
+        webGrounding = true,
     ),
     GEMINI_2_5_FLASH_LITE(
         "Gemini 2.5 Flash Lite",
         "gemini-2.5-flash-lite",
-        "v1beta"
-    ),
-    GEMINI_2_0_FLASH_IMAGE(
-        "Image generation",
-        "gemini-2.0-flash-preview-image-generation",
         "v1beta",
-        true
+        webGrounding = true,
     ),
-    GEMINI_2_0_FLASH(
-        "Gemini 2.0 Flash",
-        "gemini-2.0-flash",
-        "v1"
+    GEMINI_2_5_FLASH_WITHOUT_WEB_GROUNDING(
+        "Gemini 2.5 Flash(Web Grounding なし)",
+        "gemini-2.5-flash",
+        "v1beta",
     ),
-    GEMINI_2_0_FLASH_LITE(
-        "Gemini 2.0 Flash Lite",
-        "gemini-2.0-flash-lite",
-        "v1"
-    )
+    GEMINI_2_5_FLASH_LITE_WITHOUT_WEB_GROUNDING(
+        "Gemini 2.5 Flash Lite(Web Grounding なし)",
+        "gemini-2.5-flash-lite",
+        "v1beta",
+    ),
     ;
 
     fun label(): String = label
@@ -41,6 +43,8 @@ enum class GenerativeAiModel(
         return "https://generativelanguage.googleapis.com/${versionPath}/models/${urlParameter}" +
                 ":streamGenerateContent?alt=sse&key="
     }
+
+    fun webGrounding() = webGrounding
 
     fun image() = image
 
