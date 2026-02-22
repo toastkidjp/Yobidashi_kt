a148ce700631e50e50090d144d00ade3f7b98be2
toastkidjp <10fmi13@gmail.com>
2026-02-22 09:59:33 +0900

    feature #1039 Add sources.

--- chat/src/main/java/jp/toastkid/chat/domain/repository/ChatResponseItem.kt
+++ chat/src/main/java/jp/toastkid/chat/domain/repository/ChatResponseItem.kt
@@ -1,16 +1,21 @@
 package jp.toastkid.chat.domain.repository
 
+import jp.toastkid.chat.domain.model.Source
+
 data class ChatResponseItem(
     private val message: String,
     private val error: Boolean = false,
-    private val image: Boolean = false
-) {
+    private val image: Boolean = false,
+    private val sources: List<Source> = emptyList<Source>(),
+    ) {
 
     fun message() = message
 
     fun error() = error
 
     fun image() = image
+
+    fun sources() = sources
 
     companion object {
 
