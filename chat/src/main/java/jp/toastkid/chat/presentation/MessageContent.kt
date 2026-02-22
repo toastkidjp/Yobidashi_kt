375c60be48fee12fad187923f4c4aece08786642
toastkidjp <10fmi13@gmail.com>
2026-02-22 10:00:34 +0900

    feature #1039 Implement components.

--- chat/src/main/java/jp/toastkid/chat/presentation/MessageContent.kt
+++ chat/src/main/java/jp/toastkid/chat/presentation/MessageContent.kt
@@ -4,13 +4,17 @@ import android.content.ContentValues
 import android.graphics.Bitmap
 import android.os.Build
 import android.provider.MediaStore
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,6 +27,11 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import jp.toastkid.chat.domain.model.Source
+import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.intent.BitmapShareIntentFactory
 import jp.toastkid.ui.image.EfficientImage
 import kotlinx.coroutines.CoroutineScope
@@ -34,10 +43,14 @@ import kotlinx.coroutines.withContext
 internal fun MessageContent(
     text: String,
     base64Image: String? = null,
+    sources: List<Source>,
     modifier: Modifier
 ) {
     val viewModel = remember { MessageContentViewModel() }
     val context = LocalContext.current
+
+    val contentViewModel = (context as? ViewModelStoreOwner)?.let { ViewModelProvider(it) }
+        ?.get(ContentViewModel::class)
 
     Column(modifier) {
         text.split("\n").forEach {
@@ -59,6 +72,15 @@ internal fun MessageContent(
                     fontSize = 16.sp
                 )
             }
+        }
+
+        if (sources.isNotEmpty()) {
+            SourceArea(
+                sources,
+                { contentViewModel?.open(it.toUri()) },
+                { contentViewModel?.openBackground(it.toUri()) },
+                viewModel.horizontalSourceScrollState()
+            )
         }
 
         if (viewModel.showImage(base64Image)) {
@@ -114,6 +136,42 @@ internal fun MessageContent(
 
             withContext(Dispatchers.IO) {
                 viewModel.loadImage(base64Image)
+            }
+        }
+    }
+}
+
+@Composable
+private fun SourceArea(
+    sources: List<Source>,
+    openLink: (String) -> Unit,
+    openLinkOnBackground: (String) -> Unit,
+    horizontalSourceScrollState: ScrollState
+) {
+    Text(
+        "Sources",
+        fontSize = 16.sp,
+        modifier = Modifier.padding(top = 12.dp, start = 4.dp, end = 4.dp, bottom = 4.dp)
+    )
+
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.horizontalScroll(
+        horizontalSourceScrollState
+    )) {
+        sources.forEachIndexed { index, source ->
+            Surface(
+                shadowElevation = 2.dp,
+                modifier = Modifier.padding(4.dp)
+            ) {
+                Text(
+                    source.title,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .combinedClickable(
+                            enabled = true,
+                            onClick = { openLink(source.url) },
+                            onLongClick = { openLinkOnBackground(source.url) }
+                        )
+                )
             }
         }
     }
