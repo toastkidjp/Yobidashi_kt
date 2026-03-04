b5d5edf700fde5692fc42247657182bcf3727ebe
toastkidjp <10fmi13@gmail.com>
2026-03-04 09:31:52 +0900

    refactor #853 Delete useless plugin.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,7 +10,6 @@ import property.BuildTool
 plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version(libraries.versions.kotlin.get())
     id("com.android.library")
-    id("kotlin-android")
     id("jacoco.definition")
 }
 
