16cff7310e60e45548f430b20e4bc3f87b53e2c4
toastkidjp <10fmi13@gmail.com>
2026-03-07 00:56:26 +0900

    feature #580 Upgrade Room to 2.8.4.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
         create("libraries") {
             version("kotlin", "2.2.20")
             version("kotlinCompilerExtension", "1.5.15")
-            version("room", "2.7.2")
+            version("room", "2.8.4")
             version("cameraX", "1.5.3")
             version("coilCompose", "3.1.0")
             version("coroutines", "1.10.2")
