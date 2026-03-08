7a51c70447ac9a2472bf10e74eadefad3f427583
toastkidjp <10fmi13@gmail.com>
2026-03-08 11:03:41 +0900

    refactor #853 Add compile option.

--- licenses/build.gradle.kts
+++ licenses/build.gradle.kts
@@ -19,6 +19,11 @@ android {
 
     compileSdkVersion(BuildTool.compileSdk)
 
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_21)
+        targetCompatibility(JavaVersion.VERSION_21)
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
