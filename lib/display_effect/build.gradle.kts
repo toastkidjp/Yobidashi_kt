82faac9e3e20a396111d3c7e1289356551152117
toastkidjp <10fmi13@gmail.com>
2026-03-17 08:53:39 +0900

    refactor #853 Delete duplicated settings.

--- lib/display_effect/build.gradle.kts
+++ lib/display_effect/build.gradle.kts
@@ -19,10 +19,6 @@ android {
             isMinifyEnabled = false
         }
     }
-    compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_21)
-        targetCompatibility(JavaVersion.VERSION_21)
-    }
 }
 
 dependencies {
