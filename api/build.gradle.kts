8a40ebee84558027a0c1c1eb02cf8a65338d33fa
toastkidjp <10fmi13@gmail.com>
2026-03-17 08:53:20 +0900

    refactor #853 Delete duplicated settings.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -26,10 +26,6 @@ android {
         release {
         }
     }
-    compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_21)
-        targetCompatibility(JavaVersion.VERSION_21)
-    }
 }
 
 dependencies {
