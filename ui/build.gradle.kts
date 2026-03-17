567ae6ac4c63a6ad2690c618eb4dadfed958e138
toastkidjp <10fmi13@gmail.com>
2026-03-18 07:35:50 +0900

    refactor #853 Delete useless configuration.

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -27,11 +27,6 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_21)
-        targetCompatibility(JavaVersion.VERSION_21)
-    }
-
     buildFeatures {
         compose = true
     }
