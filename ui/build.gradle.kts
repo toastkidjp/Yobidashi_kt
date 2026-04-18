ea5357f4d143885bb1856f6173f97d7115f718f6
toastkidjp <10fmi13@gmail.com>
2026-04-19 08:33:55 +0900

    feature #853 Add navigation's dependencies.

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -41,5 +41,9 @@ dependencies {
     implementation(libraries.coilNetwork)
     implementation(libraries.composeMaterial3)
     implementation(libraries.lifecycleViewModelCompose)
+    implementation("androidx.navigation:navigation-compose:2.9.2")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.9.2")
+    api("androidx.navigation:navigation-compose:2.9.2")
+
     testImplementation("junit:junit:4.12")
 }
