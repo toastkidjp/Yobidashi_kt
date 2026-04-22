407b06d3ec30115b3ab68c0ac67458182ad1a3d5
toastkidjp <10fmi13@gmail.com>
2026-04-23 00:07:14 +0900

    bugfix #523 Enable to open from send action.

--- app/src/main/java/jp/toastkid/yobidashi/main/MainActivity.kt
+++ app/src/main/java/jp/toastkid/yobidashi/main/MainActivity.kt
@@ -8,10 +8,14 @@
 
 package jp.toastkid.yobidashi.main
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.URLUtil
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModelProvider
 import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.preference.PreferenceApplier
@@ -35,6 +39,47 @@ class MainActivity : ComponentActivity() {
                 isSystemInDarkTheme() || preferenceApplier.useDarkMode()
             ) {
                 Content()
+            }
+        }
+
+        // 起動時の Intent を処理
+        handleIntent(intent)
+    }
+
+    // Activity が既に起動している状態で別の Intent を受け取った場合
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        if (intent == null) {
+            return
+        }
+
+        val url = when (intent.action) {
+            Intent.ACTION_VIEW -> {
+                intent.dataString
+            }
+
+            Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    intent.getStringExtra(Intent.EXTRA_TEXT)
+                } else null
+            }
+
+            "jp.toastkid.yobidashi.search", Intent.ACTION_ASSIST -> {
+                // 必要に応じてクエリなどを URL に変換
+                intent.getStringExtra(SearchManager.QUERY)?.let { "https://www.google.com/search?q=$it" }
+            }
+
+            else -> null
+        }
+
+        url?.let {
+            if (URLUtil.isValidUrl(it)) {
+                val contentViewModel = ViewModelProvider(this).get(ContentViewModel::class.java)
+                contentViewModel.open(it.toUri())
             }
         }
     }
