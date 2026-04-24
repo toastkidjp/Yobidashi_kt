c8f2dfd2ed2fb95644ca2ec322c4b3424b05ad21
toastkidjp <10fmi13@gmail.com>
2026-04-25 00:14:44 +0900

    feature #853 Upgrade activity-compose to 1.13.0.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
             plugin("composeCompiler", "org.jetbrains.kotlin.plugin.compose").versionRef("kotlin")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-android").versionRef("coroutines")
             library("coreKtx", "androidx.core", "core-ktx").version("1.16.0")
-            library("activityCompose", "androidx.activity", "activity-compose").version("1.9.0")
+            library("activityCompose", "androidx.activity", "activity-compose").version("1.13.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.6.0")
             library("workManager", "androidx.work", "work-runtime").version("2.10.0")
             library("jsoup", "org.jsoup", "jsoup").version("1.22.1")
