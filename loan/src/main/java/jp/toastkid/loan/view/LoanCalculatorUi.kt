2359756b72a69c1ebed5decd5b9ea71d2fd1f14b
toastkidjp <10fmi13@gmail.com>
2026-05-02 12:55:08 +0900

    refactor #818 Optimize imports.

--- loan/src/main/java/jp/toastkid/loan/view/LoanCalculatorUi.kt
+++ loan/src/main/java/jp/toastkid/loan/view/LoanCalculatorUi.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
