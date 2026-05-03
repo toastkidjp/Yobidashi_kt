697a659e33734adfa881b5e93bd316ccd36618fb
toastkidjp <10fmi13@gmail.com>
2026-05-03 09:28:50 +0900

    feature #818 Enable to switch calculator.

--- loan/src/main/java/jp/toastkid/loan/usecase/DebouncedCalculatorUseCase.kt
+++ loan/src/main/java/jp/toastkid/loan/usecase/DebouncedCalculatorUseCase.kt
@@ -10,14 +10,16 @@ package jp.toastkid.loan.usecase
 
 import jp.toastkid.loan.model.Factor
 import jp.toastkid.loan.model.LoanPayment
+import jp.toastkid.loan.model.calculator.LoanPaymentCalculator
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
@@ -25,7 +27,7 @@ class DebouncedCalculatorUseCase(
     private val inputChannel: Channel<String>,
     private val currentFactorProvider: () -> Factor,
     private val onResult: (LoanPayment) -> Unit,
-    private val calculator: LevelPaymentCalculator = LevelPaymentCalculator(),
+    private val calculatorFlow: Flow<LoanPaymentCalculator>,
     private val debounceMillis: Long = 1000,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
     private val mainDispatcher: CoroutineDispatcher = Dispatchers.Main
@@ -34,17 +36,20 @@ class DebouncedCalculatorUseCase(
     @FlowPreview
     operator fun invoke() {
         CoroutineScope(ioDispatcher).launch {
-            inputChannel
-                .receiveAsFlow()
-                .distinctUntilChanged()
-                .debounce(debounceMillis)
-                .flowOn(mainDispatcher)
-                .collect {
-                    val factor = currentFactorProvider()
-                    val payment = calculator(factor)
+            combine(
+                inputChannel
+                    .receiveAsFlow()
+                    .distinctUntilChanged(),
+                calculatorFlow
+                    .distinctUntilChanged()
+            ) { _, calculator ->
+                val factor = currentFactorProvider()
+                val payment = calculator(factor)
 
-                    onResult(payment)
-                }
+                payment
+            }
+                .debounce(debounceMillis)
+                .collect { onResult(it) }
         }
     }
 
