2331b79ed0f0e97244fcf19035cd88c56fd82c29
toastkidjp <10fmi13@gmail.com>
2026-05-10 10:46:16 +0900

    feature #853 Use PrimaryScrollableTabRow.

--- setting/src/main/java/jp/toastkid/setting/presentation/screen/SettingTopUi.kt
+++ setting/src/main/java/jp/toastkid/setting/presentation/screen/SettingTopUi.kt
@@ -10,7 +10,9 @@ package jp.toastkid.setting.presentation.screen
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,19 +21,18 @@ import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.PrimaryScrollableTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -69,41 +70,50 @@ fun SettingTopUi() {
                 R.string.subhead_others
             )
 
-            ScrollableTabRow(
-                selectedTabIndex = pagerState.currentPage,
-                edgePadding = 8.dp,
-                containerColor = Color.Transparent,
-                indicator = { tabPositions ->
-                    val primaryColor = MaterialTheme.colorScheme.onPrimary
-                    Box(
-                        modifier = Modifier
-                            .tabIndicatorOffset(tabPositions[pagerState.currentPage])
-                            .height(2.dp)
-                            .clip(RoundedCornerShape(8.dp)) // clip modifier not working
-                            .padding(horizontal = 4.dp)
-                            .drawBehind { drawRect(primaryColor) }
-                    )
-                },
+            Column(
                 modifier = Modifier.fillMaxHeight()
             ) {
-                pages.forEachIndexed { index, page ->
-                    val coroutineScope = rememberCoroutineScope()
-                    Tab(
-                        selected = pagerState.currentPage == index,
-                        onClick = {
-                            coroutineScope.launch {
-                                pagerState.animateScrollToPage(index)
-                            }
-                        },
-                        modifier = Modifier.padding(start = 4.dp, end = 4.dp)
-                    ) {
-                        Text(
-                            text = stringResource(id = page),
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            fontSize = 16.sp
+                PrimaryScrollableTabRow(
+                    selectedTabIndex = pagerState.currentPage,
+                    edgePadding = 8.dp,
+                    containerColor = Color.Transparent,
+                    indicator = {
+                        TabRowDefaults.PrimaryIndicator(
+                            modifier = Modifier
+                                .tabIndicatorOffset(
+                                    selectedTabIndex = pagerState.currentPage,
+                                )
+                                .background(
+                                    color = MaterialTheme.colorScheme.onPrimary,
+                                    shape = RoundedCornerShape(topStart = 3.dp, topEnd = 3.dp)
+                                ),
+                            width = Dp.Unspecified,
+                            height = 2.dp,
+                            color = Color.Transparent
                         )
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    pages.forEachIndexed { index, page ->
+                        val coroutineScope = rememberCoroutineScope()
+                        Tab(
+                            selected = pagerState.currentPage == index,
+                            onClick = {
+                                coroutineScope.launch {
+                                    pagerState.animateScrollToPage(index)
+                                }
+                            },
+                            modifier = Modifier.padding(start = 4.dp, end = 4.dp)
+                        ) {
+                            Text(
+                                text = stringResource(id = page),
+                                color = MaterialTheme.colorScheme.onPrimary,
+                                fontSize = 16.sp
+                            )
+                        }
                     }
                 }
+                Spacer(modifier = Modifier.height(20.dp))
             }
         }
 
