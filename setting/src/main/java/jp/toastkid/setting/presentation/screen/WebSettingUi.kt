10e70f7d61195485b4801e7810466b7366af74d8
toastkidjp <10fmi13@gmail.com>
2026-05-23 09:45:16 +0900

    refactor #853 Modify reference.

--- setting/src/main/java/jp/toastkid/setting/presentation/screen/WebSettingUi.kt
+++ setting/src/main/java/jp/toastkid/setting/presentation/screen/WebSettingUi.kt
@@ -46,7 +46,7 @@ import jp.toastkid.lib.ContentViewModel
 import jp.toastkid.lib.Urls
 import jp.toastkid.lib.preference.PreferenceApplier
 import jp.toastkid.setting.R
-import jp.toastkid.setting.presentation.CheckableRow
+import jp.toastkid.ui.parts.CheckableRow
 import jp.toastkid.ui.parts.InsetDivider
 import jp.toastkid.ui.parts.SingleLineText
 import jp.toastkid.web.user_agent.UserAgentDropdown
